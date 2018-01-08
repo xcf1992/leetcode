31f8c698f3b8fc11dd974af5eb5bd0159bedf6d6
Chenfu Xie <chenfu.xie@hulu.com>
2018-01-07 20:14:21 -0800

    implement my calendar III

--- C++/my_calendar_III.cpp
+++ C++/my_calendar_III.cpp
@@ -10,3 +10,27 @@
 #include <stdio.h>
 using namespace std;
 
+class MyCalendarThree {
+private:
+    map<int, int> events;
+public:
+    MyCalendarThree() {}
+    
+    int book(int start, int end) {
+        events[start] += 1;
+        events[end] -= 1;
+        int booked = 0;
+        int result = 0;
+        for (auto &event : events) {
+            booked += event.second;
+            result = max(result, booked);
+        }
+        return result;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(start,end);
+ */
