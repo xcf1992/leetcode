9085f36b328e922769ce1a206b8ffaf3c8a44865
Chenfu Xie <chenfu.xie@hulu.com>
2018-01-11 22:37:00 -0800

    implement find smallest letter greater than target

--- C++/find_smallest_letter_greater_than_target.cpp
+++ C++/find_smallest_letter_greater_than_target.cpp
@@ -8,14 +8,7 @@
 #include <queue>
 #include <stack>
 #include <stdio.h>
-#include <map>
 using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
 
 class Solution {
 public:
@@ -24,26 +17,18 @@ public:
             return letters.front();
         }
         
-        if (letters.front() > target) {
-            return letters.front();
-        }
         int start = 0;
         int end = letters.size() - 1;
+        int mid = start + (end - start) >> 1;
         while (start < end) {
-            int mid = start + (end - start) >> 1;
             if (letters[mid] <= target) {
                 start = mid + 1;
             }
             else {
                 end = mid;
             }
+            mid = start + (end - start) >> 1;
         }
         return letters[end];
     }
 };
-
-int main() {
-    Solution s;
-    vector<char> v{'e','e','e','e','e','e','n','n','n','n'};
-    s.nextGreatestLetter(v, 'e');
-}
