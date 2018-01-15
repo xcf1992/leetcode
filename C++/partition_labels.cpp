add3f2a640e8ba3de4b11d9e00e60fa719910f44
Chenfu Xie <chenfu.xie@hulu.com>
2018-01-14 22:11:08 -0800

    implement partition labels

--- C++/partition_labels.cpp
+++ C++/partition_labels.cpp
@@ -1,22 +1,3 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#include <algorithm>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <stdio.h>
-#include <map>
-using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
 class Solution {
 public:
     vector<int> partitionLabels(string S) {
@@ -46,10 +27,3 @@ public:
         return result;
     }
 };
-
-
-int main() {
-    Solution s;
-    vector<int> v{3,4,5,1,2};
-    s.partitionLabels("ababcbacadefegdehijhklij");
-}
