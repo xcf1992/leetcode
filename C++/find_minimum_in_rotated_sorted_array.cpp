65e26ee582e03ccf22cfde3b182086a87ee08270
Chenfu Xie <chenfu.xie@hulu.com>
2018-01-14 16:40:57 -0800

    implement find minimum in rotated sorted array

--- C++/find_minimum_in_rotated_sorted_array.cpp
+++ C++/find_minimum_in_rotated_sorted_array.cpp
@@ -2,20 +2,8 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <unordered_set>
 #include <algorithm>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <stdio.h>
-#include <map>
 using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
 
 class Solution {
 public:
@@ -23,21 +11,18 @@ public:
         int start = 0;
         int end = nums.size() - 1;
         while (start < end) {
+            if (nums[start] < nums[end]) {
+                return nums[start];
+            }
+            
             int mid = start + (end - start) / 2;
-            if (nums[mid] > nums[start]) {
+            if (nums[mid] >= nums[start]) {
                 start = mid + 1;
             }
-            else if (nums[mid] < nums[end]) {
+            else {
                 end = mid;
             }
         }
         return nums[start];
     }
 };
-
-
-int main() {
-    Solution s;
-    vector<int> v{3,4,5,1,2};
-    s.findMin(v);
-}
