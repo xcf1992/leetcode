3daadbe0451ade105bb397bfda4e75e821ae8097
Chenfu Xie <chenfu.xie@hulu.com>
2018-01-20 17:37:54 -0800

    copied parse lisp expression - may do this in the future

---
+++ C++/parse_lisp_expression.cpp
@@ -0,0 +1,78 @@
+//
+//  parse_lisp_expression.cpp
+//  C++
+//
+//  Created by Chenfu Xie on 1/20/18.
+//  Copyright © 2018 Chenfu Xie. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    int evaluate(string expression) {
+        unordered_map<string,int> myMap;
+        return help(expression,myMap);
+    }
+    
+    int help(string expression,unordered_map<string,int> myMap) {
+        if ((expression[0] == '-') || (expression[0] >= '0' && expression[0] <= '9')) {
+            return stoi(expression);
+        }
+        else if (expression[0] != '(') {
+            return myMap[expression];
+        }
+        //to get rid of the first '(' and the last ')'
+        string s = expression.substr(1,expression.size()-2);
+        int start = 0;
+        string word = parse(s,start);
+        if (word == "let") {
+            while (true) {
+                string variable = parse(s,start);
+                //if there is no more expression, simply evaluate the variable
+                if (start > s.size()) {
+                    return help(variable,myMap);
+                }
+                string temp = parse(s,start);
+                myMap[variable] = help(temp,myMap);
+            }
+        }
+        else if (word == "add"){
+            return help(parse(s,start),myMap) + help(parse(s,start),myMap);
+        }
+        else if (word == "mult") {
+            return help(parse(s,start),myMap) * help(parse(s,start),myMap);
+        }
+    }
+    
+    //function to seperate each expression
+    string parse(string &s,int &start) {
+        int end = start+1, temp = start, count = 1;
+        if (s[start] == '(') {
+            while (count != 0) {
+                if (s[end] == '(')
+                    count++;
+                else if (s[end] == ')')
+                    count--;
+                end++;
+            }
+        }
+        else {
+            while (end < s.size() && s[end] != ' ')
+                end++;
+        }
+        start = end+1;
+        return s.substr(temp,end-temp);
+    }
+};
