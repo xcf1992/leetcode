77e1923ab7da3548bd2402394e7d9e3408588eec
Chenfu Xie <chenfu.xie@hulu.com>
2018-02-28 16:05:07 -0800

    maximum size subarray sum equals k

---
+++ C++/maximum_size_subarray_sum_equals_k.cpp
@@ -0,0 +1,62 @@
+//
+//  maximum_size_subarray_sum_equals_k.cpp
+//  C++
+//
+//  Created by Chenfu Xie on 2/28/18.
+//  Copyright © 2018 Chenfu Xie. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+using namespace std;
+
+/*
+ Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+ 
+ Note:
+ The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
+ 
+ Example 1:
+ Given nums = [1, -1, 5, -2, 3], k = 3,
+ return 4. (because the subarray [1, -1, 5, -2] sums to 3 and is the longest)
+ 
+ Example 2:
+ Given nums = [-2, -1, 2, 1], k = 1,
+ return 2. (because the subarray [-1, 2] sums to 1 and is the longest)
+ 
+ Follow Up:
+ Can you do it in O(n) time?
+ */
+
+class Solution {
+public:
+    int maxSubArrayLen(vector<int>& nums, int k) {
+        unordered_map<int, int> sumIndex;
+        int sum = 0;
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum == k) {
+                result = i + 1;
+            }
+            else if (sumIndex.find(sum - k) != sumIndex.end()) {
+                result = max(result, i - sumIndex[sum - k]);
+            }
+            
+            if (sumIndex.find(sum) == sumIndex.end()) {
+                sumIndex[sum] = i;
+            }
+        }
+        return result;
+    }
+};
+
