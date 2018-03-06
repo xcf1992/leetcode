be908e8c54e762ea8adeae4a95b5b96a0922f818
Chenfu Xie <chenfu.xie@hulu.com>
2018-03-05 21:50:19 -0800

    binary tree longest consecutive sequence II

---
+++ C++/binary_tree_longest_consecutive_sequence_II.cpp
@@ -0,0 +1,67 @@
+/*
+ Given a binary tree, you need to find the length of Longest Consecutive Path in Binary Tree.
+ 
+ Especially, this path can be either increasing or decreasing. For example, [1,2,3,4] and [4,3,2,1] are both considered valid, but the path [1,2,4,3] is not valid. On the other hand, the path can be in the child-Parent-child order, where not necessarily be parent-child order.
+ 
+ Example 1:
+ Input:
+   1
+  / \
+ 2   3
+ Output: 2
+ Explanation: The longest consecutive path is [1, 2] or [2, 1].
+ Example 2:
+ Input:
+   2
+  / \
+ 1   3
+ Output: 3
+ Explanation: The longest consecutive path is [1, 2, 3] or [3, 2, 1].
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+using namespace std;
+
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+class Solution {
+private:
+    int findPath(TreeNode* root, int preVal, int diff) {
+        if (root == nullptr) {
+            return 0;
+        }
+        
+        if (root -> val == preVal + diff) {
+            return max(findPath(root -> left, root -> val, diff), findPath(root -> right, root -> val, diff)) + 1;
+        }
+        return 0;
+    }
+public:
+    int longestConsecutive(TreeNode* root) {
+        if (root == nullptr) {
+            return 0 ;
+        }
+        
+        int l1 = findPath(root -> left, root -> val, -1) + findPath(root -> right, root -> val, 1) + 1;
+        int l2 = findPath(root -> left, root -> val, 1) + findPath(root -> right, root -> val, -1) + 1;
+        int longest = max(longestConsecutive(root -> left), longestConsecutive(root -> right));
+
+        return max(longest, max(l1, l2));
+    }
+};
