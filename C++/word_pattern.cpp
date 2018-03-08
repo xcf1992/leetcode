c985f59b3194e6f2109ddca7838a7c5b93a43fe2
Chenfu Xie <chenfu.xie@hulu.com>
2018-03-08 13:09:47 -0800

    implement word pattern

--- C++/word_pattern.cpp
+++ C++/word_pattern.cpp
@@ -1,3 +1,17 @@
+/*
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Examples:
+pattern = "abba", str = "dog cat cat dog" should return true.
+pattern = "abba", str = "dog cat cat fish" should return false.
+pattern = "aaaa", str = "dog cat cat dog" should return false.
+pattern = "abba", str = "dog dog dog dog" should return false.
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -14,39 +28,37 @@ using namespace std;
 class Solution {
 public:
     bool wordPattern(string pattern, string str) {
-        vector<string> st;
+        vector<string> words;
         string temp;
-        for(auto c:str){
-            if(c!=' ') temp+=c;
+        for (char c : str) {
+            if (c != ' ') {
+                temp += c;
+            }
             else {
-                st.push_back(temp);
-                temp="";
+                words.push_back(temp);
+                temp = "";
             }
         }
-        st.push_back(temp);
+        words.push_back(temp);
         
-        if(pattern.length()!=st.size()) return false;
-        
-        unordered_map<char,string> mp1;
-        unordered_map<string,char> mp2;
-        int i=0;
-        for(auto c:pattern){
-            if(mp1.find(c)==mp1.end()) mp1[c]=st[i];
-            else if(mp1[c]!=st[i]) return false;
-            i++;
+        if (pattern.length()!= words.size()) {
+            return false;
         }
-        i=0;
-        for(auto s:st){
-            if(mp2.find(s)==mp2.end()) mp2[s]=pattern[i];
-            else if(mp2[s]!=pattern[i]) return false;
-            i++;
+        
+        unordered_map<char, string> pat2Str;
+        unordered_map<string, char> str2Pat;
+        for (int i = 0; i < pattern.size(); i++) {
+            if (pat2Str.find(pattern[i]) == pat2Str.end()) {
+                if (str2Pat.find(words[i]) != str2Pat.end()) {
+                    return false;
+                }
+                pat2Str[pattern[i]] = words[i];
+                str2Pat[words[i]] = pattern[i];
+            }
+            if (pat2Str[pattern[i]] != words[i]) {
+                return false;
+            }
         }
         return true;
     }
 };
-
-int main() {
-    Solution s;
-    cout << s.wordPattern("aba", "wo cao wo");
-    return 0;
-}
---
+++ C++/word_pattern_II.cpp
@@ -0,0 +1,69 @@
+/*
+ Given a pattern and a string str, find if str follows the same pattern.
+ 
+ Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty substring in str.
+ 
+ Examples:
+ pattern = "abab", str = "redblueredblue" should return true.
+ pattern = "aaaa", str = "asdasdasdasd" should return true.
+ pattern = "aabb", str = "xyzabcxzyabc" should return false.
+ Notes:
+ You may assume both pattern and str contains only lowercase letters.
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+using namespace std;
+
+class Solution {
+private:
+    unordered_map<char, string> pat2Str;
+    unordered_map<string, char> str2Pat;
+    
+    bool match(string &pattern, int i, string &str, int j) {
+        int m = pattern.size();
+        int n = str.size();
+        if (i == m || j == n) {
+            if (i == m && j == n) {
+                return true;
+            }
+            return false;
+        }
+        
+        bool result = false;
+        for (int k = j; k < n; k++) {
+            string word = str.substr(j, k - j + 1);
+            if (pat2Str.find(pattern[i]) != pat2Str.end() && pat2Str[pattern[i]] != word) {
+                continue;
+            }
+            else if (str2Pat.find(word) != str2Pat.end() && str2Pat[word] != pattern[i]) {
+                continue;
+            }
+            else {
+                pat2Str[pattern[i]] = word;
+                str2Pat[word] = pattern[i];
+                result = true;
+            }
+            if (match(pattern, i + 1, str, k + 1)) {
+                return true;
+            }
+            if (result) {
+                pat2Str.erase(pattern[i]);
+                str2Pat.erase(word);
+            }
+        }
+        return false;
+    }
+public:
+    bool wordPatternMatch(string pattern, string str) {
+        return match(pattern, 0, str, 0);
+    }
+};
