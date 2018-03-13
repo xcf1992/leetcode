9755147916d2327467c3ea728a53a5342fe12a64
Chenfu Xie <chenfu.xie@hulu.com>
2018-03-12 22:35:43 -0700

    contains duplicates III

--- C++/contains_duplicate_III.cpp
+++ C++/contains_duplicate_III.cpp
@@ -1,3 +1,7 @@
+/*
+ Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,22 +12,8 @@
 #include <queue>
 #include <stack>
 #include <stdio.h>
-#include <map>
 #include <set>
 using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
 class Solution {
 public:
@@ -39,8 +29,7 @@ public:
                 left++;
             }
             set<long>::iterator it = windows.lower_bound((long)nums[i] - (long)t);
-            if (it != windows.end()) {
-                int x = *it;
+            if (it != windows.end() && *it <= ((long)nums[i] + (long)t)) {
                 return true;
             }
             windows.insert(nums[i]);
@@ -48,17 +37,3 @@ public:
         return false;
     }
 };
-
-int main() {
-    Solution s;
-    vector<string> v({"ABD","BCE","DEF","FFF"});
-    vector<int> v2({4,2});
-    vector<char> chars({'a','a','a','a','a','b','b','c'});
-    vector<vector<int>> matrix({{1,2},{3},{3},{}});
-    vector<pair<int, int>> fuxk;
-    fuxk.push_back(make_pair(0, 1));
-    fuxk.push_back(make_pair(1, 2));
-    fuxk.push_back(make_pair(3, 4));
-    
-    s.containsNearbyAlmostDuplicate(v2, 2, 1);
-}
