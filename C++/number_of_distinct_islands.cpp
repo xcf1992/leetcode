070e368394dc4378d329acdfa26bd2c7be993d14
Chenfu Xie <chenfu.xie@hulu.com>
2018-03-13 21:35:58 -0700

    number of distinct islands

--- C++/number_of_distinct_islands.cpp
+++ C++/number_of_distinct_islands.cpp
@@ -1,3 +1,31 @@
+/*
+ Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+ 
+ Count the number of distinct islands. An island is considered to be the same as another if and only if one island can be translated (and not rotated or reflected) to equal the other.
+ 
+ Example 1:
+ 11000
+ 11000
+ 00011
+ 00011
+ Given the above grid map, return 1.
+ Example 2:
+ 11011
+ 10000
+ 00001
+ 11011
+ Given the above grid map, return 3.
+ 
+ Notice that:
+ 11
+ 1
+ and
+  1
+ 11
+ are considered different island shapes, because we do not consider reflection / rotation.
+ Note: The length of each dimension in the given grid does not exceed 50.
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,48 +36,38 @@
 #include <queue>
 #include <stack>
 #include <stdio.h>
-#include <map>
 #include <set>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 class Solution {
 private:
-    void dfs(int x, int y, string& island, vector<vector<int>>& grid, vector<vector<bool>>& visited) {
+    void dfs(int x, int y, char index, string& island, vector<vector<int>>& grid, vector<vector<bool>>& visited) {
         int m = grid.size();
         int n = grid[0].size();
         
         if (x + 1 < m && !visited[x + 1][y] && grid[x + 1][y] == 1) {
+            island.push_back(index);
             island.push_back('d');
             visited[x + 1][y] = true;
-            dfs(x + 1, y, island, grid, visited);
+            dfs(x + 1, y, index + 1, island, grid, visited);
         }
         if (x - 1 >= 0 && !visited[x - 1][y] && grid[x - 1][y] == 1) {
+            island.push_back(index);
             island.push_back('u');
             visited[x - 1][y] = true;
-            dfs(x - 1, y, island, grid, visited);
+            dfs(x - 1, y, index + 1, island, grid, visited);
         }
         if (y + 1 < n && !visited[x][y + 1] && grid[x][y + 1] == 1) {
+            island.push_back(index);
             island.push_back('r');
             visited[x][y + 1] = true;
-            dfs(x, y + 1, island, grid, visited);
+            dfs(x, y + 1, index + 1, island, grid, visited);
         }
         if (y - 1 >= 0 && !visited[x][y - 1] && grid[x][y - 1] == 1) {
+            island.push_back(index);
             island.push_back('l');
             visited[x][y - 1] = true;
-            dfs(x, y - 1, island, grid, visited);
+            dfs(x, y - 1, index + 1, island, grid, visited);
         }
     }
 public:
@@ -66,7 +84,7 @@ public:
                 if (grid[i][j] == 1 && !visited[i][j]) {
                     string island = "";
                     visited[i][j] = true;
-                    dfs(i, j, island, grid, visited);
+                    dfs(i, j, '0', island, grid, visited);
                     if (shape.find(island) == shape.end()) {
                         shape.insert(island);
                     }
@@ -76,16 +94,3 @@ public:
         return shape.size();
     }
 };
-int main() {
-    Solution s;
-    vector<string> v({"ABD","BCE","DEF","FFF"});
-    vector<int> v2({4,2});
-    vector<char> chars({'a','a','a','a','a','b','b','c'});
-    vector<vector<int>> matrix({{1,1,0}, {0,1,1}, {0,0,0}, {1,1,1}, {0,1,0}});
-    vector<pair<int, int>> fuxk;
-    fuxk.push_back(make_pair(0, 1));
-    fuxk.push_back(make_pair(1, 2));
-    fuxk.push_back(make_pair(3, 4));
-    
-    s.numDistinctIslands(matrix);
-}
