577368b3378deaa18c6e6f6743aa151ee52f6dcd
Chenfu Xie <chenfu.xie@hulu.com>
2018-05-16 17:58:39 -0700

    flipping an image

---
+++ C++/fippling_an_image.cpp
@@ -0,0 +1,69 @@
+/*
+ Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+ 
+ To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+ 
+ To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+ 
+ Example 1:
+ 
+ Input: [[1,1,0],[1,0,1],[0,0,0]]
+ Output: [[1,0,0],[0,1,0],[1,1,1]]
+ Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+ Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+ Example 2:
+ 
+ Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+ Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+ Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ Notes:
+ 
+ 1 <= A.length = A[0].length <= 20
+ 0 <= A[i][j] <= 1
+
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+using namespace std;
+
+class Solution {
+private:
+    void change(vector<int>& row) {
+        int left = 0;
+        int right = row.size() - 1;
+        while (left <= right) {
+            row[left] = 1 - row[left];
+            if (left != right) {
+                row[right] = 1 - row[right];
+                swap(row[left], row[right]);
+            }
+            left += 1;
+            right -= 1;
+        }
+        return;
+    }
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        int m = A.size();
+        if (m == 0) {
+            return A;
+        }
+        int n = A[0].size();
+        
+        for (vector<int>& row : A) {
+            change(row);
+        }
+        return A;
+    }
+};
