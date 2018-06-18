4d8c49408830d0ed5ad3f684a4145c0b61b0e560
Chenfu Xie <chenfu.xie@hulu.com>
2018-06-17 22:37:30 -0700

    peak index of a mountain array

---
+++ C++/peak_index_in_a_mountain_array.cpp
@@ -0,0 +1,57 @@
+/*
+ Let's call an array A a mountain if the following properties hold:
+ 
+ A.length >= 3
+ There exists some 0 < i < A.length - 1 such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
+ Given an array that is definitely a mountain, return any i such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1].
+ 
+ Example 1:
+ 
+ Input: [0,1,0]
+ Output: 1
+ Example 2:
+ 
+ Input: [0,2,1,0]
+ Output: 1
+ Note:
+ 
+ 3 <= A.length <= 10000
+ 0 <= A[i] <= 10^6
+ A is a mountain, as defined above.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        int left = 0;
+        int right = A.size() - 1;
+        int mid = 0;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) {
+                return mid;
+            }
+            else if (A[mid] > A[mid - 1]) {
+                left = mid;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return 0;
+    }
+};
