471f51b7457636200186c980416ac888cc77f553
Chenfu Xie <chenfu.xie@hulu.com>
2018-07-05 14:18:56 -0700

    Score After Flipping Matrix

--- C++/score_after_flipping_matrix.cpp
+++ C++/score_after_flipping_matrix.cpp
@@ -1,29 +1,45 @@
+/*
+ We have a two dimensional matrix A where each value is 0 or 1.
+ 
+ A move consists of choosing any row or column, and toggling each value in that row or column: changing all 0s to 1s, and all 1s to 0s.
+ 
+ After making any number of moves, every row of this matrix is interpreted as a binary number, and the score of the matrix is the sum of these numbers.
+ 
+ Return the highest possible score.
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: [[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+ Output: 39
+ Explanation:
+ Toggled to [[1,1,1,1],[1,0,0,1],[1,1,1,1]].
+ 0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+ 
+ 
+ Note:
+ 
+ 1 <= A.length <= 20
+ 1 <= A[0].length <= 20
+ A[i][j] is 0 or 1.
+
+ */
+
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <map>
 #include <unordered_set>
 #include <algorithm>
 #include <cmath>
 #include <queue>
 #include <stack>
 #include <stdio.h>
-#include <map>
 #include <set>
 using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
 class Solution {
 private:
@@ -32,7 +48,9 @@ private:
         for (int i = 0; i < m; i++) {
             int row = 0;
             for (int j = 0; j < n; j++) {
-                row += (1 << (n - j - 1));
+                if (A[i][j] == 1) {
+                    row += (1 << (n - j - 1));
+                }
             }
             result += row;
         }
@@ -66,19 +84,3 @@ public:
         return countSum(A, m, n);
     }
 };
-
-int main() {
-    Solution s;
-    vector<string> v({"ahjpjau","ja","ahbwzgqnuk","tnmlanowax"});
-    vector<int> v1({0,2});
-    vector<string> v2({"a","cd"});
-    vector<char> chars({'a','a','a','a','a','b','b','c'});
-    vector<vector<int>> matrix({{0,0,1,1}, {1,0,1,0}, {1,1,0,0}});
-    vector<pair<int, int>> fuxk;
-    fuxk.push_back(make_pair(4,3));
-    fuxk.push_back(make_pair(2,3));
-    fuxk.push_back(make_pair(2,1));
-    fuxk.push_back(make_pair(5,0));
-    
-    s.matrixScore(matrix);
-}
