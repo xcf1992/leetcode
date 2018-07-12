e0d44215f27720f0ef452d42bf3c54c98e142850
Chenfu Xie <chenfu.xie@hulu.com>
2018-07-12 11:13:06 -0700

    709. To Lower Case

---
+++ C++/to_lower_case.cpp
@@ -0,0 +1,32 @@
+/*
+ Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ 
+
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (int i = 0; i < str.size(); i++) {
+            if (str[i] >= 'A' && str[i] <= 'Z') {
+                str[i] = 'a' + (str[i] - 'A');
+            }
+        }
+        return str;
+    }
+};
