dc333fba896d545b0f8b0f1469a83b970dbc4ef5
Chenfu Xie <chenfu.xie@hulu.com>
2018-07-13 16:51:11 -0700

    518. Coin Change 2

---
+++ C++/coin_changes_2.cpp
@@ -0,0 +1,92 @@
+/*
+ You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+ 
+ Note: You can assume that
+ 
+ 0 <= amount <= 5000
+ 1 <= coin <= 5000
+ the number of coins is less than 500
+ the answer is guaranteed to fit into signed 32-bit integer
+ 
+ 
+ Example 1:
+ 
+ Input: amount = 5, coins = [1, 2, 5]
+ Output: 4
+ Explanation: there are four ways to make up the amount:
+ 5=5
+ 5=2+2+1
+ 5=2+1+1+1
+ 5=1+1+1+1+1
+ 
+ 
+ Example 2:
+ 
+ Input: amount = 3, coins = [2]
+ Output: 0
+ Explanation: the amount of 3 cannot be made up just with coins of 2.
+ 
+ 
+ Example 3:
+ 
+ Input: amount = 10, coins = [10]
+ Output: 1
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+/*
+ This is a 2D dp problem,
+ dp[i][j] means combinations using coin 0, 1 ... i to build amount j
+ it can be get by:
+ 1. do not use coin i, which is dp[i - 1][j]
+ 2. use at least one coin i, which is dp[i][j - coins[i]]
+ 
+ later we can find that dp[i][j] only relies on dp[i - 1][j] and dp[i][j - coins[i]]
+ so we can actually only use one array to solve the problem
+ */
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
+
+class Solution1 {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= coins.size(); i++) {
+            dp[i][0] = 1;
+            for (int j = 0; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= coins[i - 1]) {
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+                }
+                
+            }
+        }
+        return dp[coins.size()][amount];
+    }
+};
