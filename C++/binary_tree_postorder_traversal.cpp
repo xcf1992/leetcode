ff74ecf208f557ef90a7b7b9235b4c22645ea2e5
Chenfu Xie <chenfu.xie@hulu.com>
2018-07-12 20:26:09 -0700

    145. Binary Tree Postorder Traversal

---
+++ C++/binary_tree_postorder_traversal.cpp
@@ -0,0 +1,65 @@
+/*
+ Given a binary tree, return the postorder traversal of its nodes' values.
+ 
+ Example:
+ 
+ Input: [1,null,2,3]
+ 1
+   \
+    2
+   /
+  3
+ 
+ Output: [3,2,1]
+ Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ 
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (root == nullptr) {
+            return result;
+        }
+        
+        stack<TreeNode*> stk;
+        stk.push(root);
+        while (!stk.empty()) {
+            TreeNode* cur = stk.top();
+            stk.pop();
+            result.push_back(cur -> val);
+            if (cur -> left) {
+                stk.push(cur -> left);
+            }
+            if (cur -> right) {
+                stk.push(cur -> right);
+            }
+        }
+        
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
