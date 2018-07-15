65e239e6b5235d70bd0b64b52772a52732b34802
Chenfu Xie <chenfu.xie@hulu.com>
2018-07-14 20:25:52 -0700

    868. Binary Gap

---
+++ C++/binary_gap.cpp
@@ -0,0 +1,80 @@
+/*
+ Given a positive integer N, find and return the longest distance between two consecutive 1's in the binary representation of N.
+ 
+ If there aren't two consecutive 1's, return 0.
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: 22
+ Output: 2
+ Explanation:
+ 22 in binary is 0b10110.
+ In the binary representation of 22, there are three ones, and two consecutive pairs of 1's.
+ The first consecutive pair of 1's have distance 2.
+ The second consecutive pair of 1's have distance 1.
+ The answer is the largest of these two distances, which is 2.
+ Example 2:
+ 
+ Input: 5
+ Output: 2
+ Explanation:
+ 5 in binary is 0b101.
+ Example 3:
+ 
+ Input: 6
+ Output: 1
+ Explanation:
+ 6 in binary is 0b110.
+ Example 4:
+ 
+ Input: 8
+ Output: 0
+ Explanation:
+ 8 in binary is 0b1000.
+ There aren't any consecutive pairs of 1's in the binary representation of 8, so we return 0.
+ 
+ 
+ Note:
+ 
+ 1 <= N <= 10^9
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int binaryGap(int N) {
+        int result = 0;
+        int pre = -1;
+        int cur = 0;
+        while (N != 0) {
+            if (N % 2 == 1) {
+                if (pre == -1) {
+                    pre = cur;
+                }
+                else {
+                    result = max(result, cur - pre);
+                    pre = cur;
+                }
+            }
+            cur += 1;
+            N >>= 1;
+        }
+        return result;
+    }
+};
