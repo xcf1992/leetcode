8f06d5fcc0d645f22b06fa90ab3e1a4f638df8ac
Chenfu Xie <chenfu.xie@hulu.com>
2018-07-23 20:24:21 -0700

    873. Length of Longest Fibonacci Subsequence

---
+++ C++/length_of_longest_fibonacci_subsequence.cpp
@@ -0,0 +1,74 @@
+/*
+ A sequence X_1, X_2, ..., X_n is fibonacci-like if:
+ 
+ n >= 3
+ X_i + X_{i+1} = X_{i+2} for all i + 2 <= n
+ Given a strictly increasing array A of positive integers forming a sequence, find the length of the longest fibonacci-like subsequence of A.  If one does not exist, return 0.
+ 
+ (Recall that a subsequence is derived from another sequence A by deleting any number of elements (including none) from A, without changing the order of the remaining elements.  For example, [3, 5, 8] is a subsequence of [3, 4, 5, 6, 7, 8].)
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: [1,2,3,4,5,6,7,8]
+ Output: 5
+ Explanation:
+ The longest subsequence that is fibonacci-like: [1,2,3,5,8].
+ Example 2:
+ 
+ Input: [1,3,7,11,12,14,18]
+ Output: 3
+ Explanation:
+ The longest subsequence that is fibonacci-like:
+ [1,11,12], [3,11,14] or [7,11,18].
+ 
+ 
+ Note:
+ 
+ 3 <= A.length <= 1000
+ 1 <= A[0] < A[1] < ... < A[A.length - 1] <= 10^9
+ (The time limit has been reduced by 50% for submissions in Java, C, and C++.)
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& A) {
+        int n = A.size();
+        if (n < 3) {
+            return 0;
+        }
+        
+        int result = 0;
+        unordered_set<int> num(A.begin(), A.end());
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int first = A[i];
+                int second = A[j];
+                int length = 2;
+                while (num.find(first + second) != num.end()) {
+                    length += 1;
+                    second += first;
+                    first = second - first;
+                }
+                result = max(result, length);
+            }
+        }
+        return result > 2 ? result : 0;
+    }
+};
