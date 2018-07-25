7a6747f09673a2776b005efab7efa59855aa7c67
Chenfu Xie <chenfu.xie@hulu.com>
2018-07-25 13:29:21 -0700

    223. Rectangle Area

---
+++ C++/rectangle_area.cpp
@@ -0,0 +1,41 @@
+/*
+ Find the total area covered by two rectilinear rectangles in a 2D plane.
+ 
+ Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+ 
+ Rectangle Area
+ 
+ Example:
+ 
+ Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
+ Output: 45
+ Note:
+ 
+ Assume that the total area is never beyond the maximum possible value of int.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int a1 = (C - A) * (D - B) + (G - E) * (H - F);
+        if (G > A && E < C && H > B && F < D) {
+            return a1 - (min(C,G) - max(A,E)) * (min(D,H) - max(B,F));
+        }
+        return a1;
+    }
+};
