1724a4b222b61fb96ea1aef9db52194dccd399f7
Chenfu Xie <chenfu.xie@hulu.com>
2018-08-06 13:30:50 -0700

    885. Boats to Save People

---
+++ C++/boats_to_save_people.cpp
@@ -0,0 +1,67 @@
+/*
+ The i-th person has weight people[i], and each boat can carry a maximum weight of limit.
+ 
+ Each boat carries at most 2 people at the same time, provided the sum of the weight of those people is at most limit.
+ 
+ Return the minimum number of boats to carry every given person.  (It is guaranteed each person can be carried by a boat.)
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: people = [1,2], limit = 3
+ Output: 1
+ Explanation: 1 boat (1, 2)
+ Example 2:
+ 
+ Input: people = [3,2,2,1], limit = 3
+ Output: 3
+ Explanation: 3 boats (1, 2), (2) and (3)
+ Example 3:
+ 
+ Input: people = [3,5,3,4], limit = 5
+ Output: 4
+ Explanation: 4 boats (3), (3), (4), (5)
+ Note:
+ 
+ 1 <= people.length <= 50000
+ 1 <= people[i] <= limit <= 30000
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int boats = 0;
+        int left = 0;
+        int right = people.size() - 1;
+        sort(people.begin(), people.end());
+        while (left <= right) {
+            if (left == right) {
+                return boats + 1;
+            }
+            
+            if (people[left] + people[right] <= limit) {
+                left += 1;
+            }
+            boats += 1;
+            right -= 1;
+        }
+        return boats;
+    }
+};
