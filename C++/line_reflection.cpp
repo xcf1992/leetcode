986f76f82cf45bcdd9d802ec6ec2bd65a09c7a7c
Chenfu Xie <chenfu.xie@hulu.com>
2018-08-17 10:34:03 -0700

    356. Line Reflection

---
+++ C++/line_reflection.cpp
@@ -0,0 +1,56 @@
+/*
+ Given n points on a 2D plane, find if there is such a line parallel to y-axis that reflect the given points.
+ 
+ Example 1:
+ 
+ Input: [[1,1],[-1,1]]
+ Output: true
+ Example 2:
+ 
+ Input: [[1,1],[-1,-1]]
+ Output: false
+ Follow up:
+ Could you do better than O(n2) ?
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    bool isReflected(vector<pair<int, int>>& points) {
+        if (points.size() <= 1) {
+            return true;
+        }
+        
+        sort(points.begin(), points.end());
+        
+        int mid = points.front().first + points.back().first;
+        int xXor = 0;
+        int yXor = 0;
+        for (int i = 0; i < points.size(); i++) {
+            if (i != 0 && points[i].first == points[i - 1].first) {
+                continue;
+            }
+            
+            if (2 * points[i].first != mid) {
+                yXor ^= points[i].second;
+                xXor ^= (abs(mid - 2 * points[i].first));
+            }
+        }
+        return xXor == 0 && yXor == 0;
+    }
+};
