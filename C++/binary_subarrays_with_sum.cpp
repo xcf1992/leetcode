608ff0f9e04dbc6817f46da95402e0c6486fb09d
Chenfu Xie <chenfu.xie@hulu.com>
2018-10-28 11:34:08 -0700

    932. Beautiful Array

--- C++/binary_subarrays_with_sum.cpp
+++ C++/binary_subarrays_with_sum.cpp
@@ -37,6 +37,21 @@
 #include <numeric>
 using namespace std;
 
+/*
+ another solution:
+ Intuition
+ 
+ Let P[i] = A[0] + A[1] + ... + A[i-1]. Then P[j+1] - P[i] = A[i] + A[i+1] + ... + A[j], the sum of the subarray [i, j].
+ 
+ Hence, we are looking for the number of i < j with P[j] - P[i] = S.
+ 
+ Algorithm
+ 
+ For each j, let's count the number of i with P[j] = P[i] + S. This is analogous to counting the number of subarrays ending in j with sum S.
+ 
+ It comes down to counting how many P[i] + S we've seen before. We can keep this count on the side to help us find the final answer.
+ */
+
 // the key point is to count the number of one between each 1
 class Solution {
 public:
