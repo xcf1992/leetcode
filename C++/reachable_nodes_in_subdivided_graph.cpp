655d7ad0a8b922a640a624995db512d625be001f
Chenfu Xie <chenfu.xie@hulu.com>
2018-11-08 15:13:05 -0800

    882. Reachable Nodes In Subdivided Graph - should check discuss to find a better algorithm

--- C++/reachable_nodes_in_subdivided_graph.cpp
+++ C++/reachable_nodes_in_subdivided_graph.cpp
@@ -1,3 +1,45 @@
+/*
+ Starting with an undirected graph (the "original graph") with nodes from 0 to N-1, subdivisions are made to some of the edges.
+ 
+ The graph is given as follows: edges[k] is a list of integer pairs (i, j, n) such that (i, j) is an edge of the original graph,
+ 
+ and n is the total number of new nodes on that edge.
+ 
+ Then, the edge (i, j) is deleted from the original graph, n new nodes (x_1, x_2, ..., x_n) are added to the original graph,
+ 
+ and n+1 new edges (i, x_1), (x_1, x_2), (x_2, x_3), ..., (x_{n-1}, x_n), (x_n, j) are added to the original graph.
+ 
+ Now, you start at node 0 from the original graph, and in each move, you travel along one edge.
+ 
+ Return how many nodes you can reach in at most M moves.
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: edges = [[0,1,10],[0,2,1],[1,2,2]], M = 6, N = 3
+ Output: 13
+ Explanation:
+ The nodes that are reachable in the final graph after M = 6 moves are indicated below.
+ 
+ Example 2:
+ 
+ Input: edges = [[0,1,4],[1,2,6],[0,2,8],[1,3,1]], M = 10, N = 4
+ Output: 23
+ 
+ 
+ Note:
+ 
+ 0 <= edges.length <= 10000
+ 0 <= edges[i][0] < edges[i][1] < N
+ There does not exist any i != j for which edges[i][0] == edges[j][0] and edges[i][1] == edges[j][1].
+ The original graph has no parallel edges.
+ 0 <= edges[i][2] <= 10000
+ 0 <= M <= 10^9
+ 1 <= N <= 3000
+ A reachable node is a node that can be travelled to using at most M moves starting from node 0.
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -14,10 +56,9 @@ using namespace std;
 
 class Solution {
 public:
-    int reachableNodes(vector<vector<int>> edges, int M, int N) {
+    int reachableNodes(vector<vector<int>>& edges, int M, int N) {
         unordered_map<int, vector<int>> next;
         unordered_map<int, unordered_map<int, int>> distance;
-        unordered_map<int, unordered_map<int, int>> used;
         for (vector<int>& edge : edges) {
             int start = edge[0];
             int end = edge[1];
@@ -25,11 +66,10 @@ public:
             next[end].push_back(start);
             distance[start][end] = edge[2];
             distance[end][start] = edge[2];
-            used[start][end] = 0;
-            used[end][start] = 0;
         }
         
         unordered_map<int, int> visited;
+        unordered_map<int, unordered_map<int, int>> used;
         queue<pair<int, int>> bfs;
         bfs.push({0, M});
         visited[0] = M;
@@ -68,35 +108,3 @@ public:
         return usedNodes + visited.size();
     }
 };
-
-int main() {
-    Solution s;
-    vector<string> v({"ahjpjau","ja","ahbwzgqnuk","tnmlanowax"});
-    vector<int> va({4,5,8,2});
-    vector<vector<int>> vb({{3,0}});
-    vector<string> v2({"a","cd"});
-    vector<char> chars({'a','a','a','a','a','b','b','c'});
-    
-    vector<vector<int>> matrix1({
-        {1,1,1,0,1,1,1,1},
-        {1,0,0,0,0,1,1,1},
-        {1,1,1,0,0,0,1,1},
-        {1,1,0,0,0,0,0,0},
-        {1,0,0,0,0,0,0,0},
-        {1,0,0,0,0,0,0,0}
-    });
-    vector<vector<char>> matrix2({
-        {'1','1','1','1','1','1','1','1'},
-        {'1','1','1','1','1','1','1','0'},
-        {'1','1','1','1','1','1','1','0'},
-        {'1','1','1','1','1','0','0','0'},
-        {'0','1','1','1','1','0','0','0'}
-    });
-    vector<vector<int>> matrix3({
-        {1,0},
-        {0,1}
-    });
-    
-    int x = s.reachableNodes({{3,4,8},{0,1,3},{1,4,0},{1,2,3},{0,3,2},{0,4,10},{1,3,3},{2,4,3},{2,3,3},{0,2,10}}, 7, 5);
-    return 0;
-}
