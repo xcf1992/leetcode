c7b9c38861a48478c0948687c65bbd38d53f260d
Chenfu Xie <chenfu.xie@hulu.com>
2018-11-15 12:05:01 -0800

    864. Shortest Path to Get All Keys

--- C++/shortest_path_to_get_all_keys.cpp
+++ C++/shortest_path_to_get_all_keys.cpp
@@ -1,3 +1,32 @@
+/*
+ We are given a 2-dimensional grid. "." is an empty cell, "#" is a wall, "@" is the starting point, ("a", "b", ...) are keys, and ("A", "B", ...) are locks.
+ 
+ We start at the starting point, and one move consists of walking one space in one of the 4 cardinal directions.  We cannot walk outside the grid, or walk into a wall.  If we walk over a key, we pick it up.  We can't walk over a lock unless we have the corresponding key.
+ 
+ For some 1 <= K <= 6, there is exactly one lowercase and one uppercase letter of the first K letters of the English alphabet in the grid.  This means that there is exactly one key for each lock, and one lock for each key; and also that the letters used to represent the keys and locks were chosen in the same order as the English alphabet.
+ 
+ Return the lowest number of moves to acquire all keys.  If it's impossible, return -1.
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: ["@.a.#","###.#","b.A.B"]
+ Output: 8
+ Example 2:
+ 
+ Input: ["@..aA","..B#.","....b"]
+ Output: 6
+ 
+ 
+ Note:
+ 
+ 1 <= grid.length <= 30
+ 1 <= grid[0].length <= 30
+ grid[i][j] contains only '.', '#', '@', 'a'-'f' and 'A'-'F'
+ The number of keys is in [1, 6].  Each key has a different letter and opens exactly one lock.
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -17,7 +46,7 @@ private:
     vector<int> rDiff{0, 0, 1, -1};
     vector<int> cDiff{1, -1, 0, 0};
 public:
-    int shortestPathAllKeys(vector<string> grid) {
+    int shortestPathAllKeys(vector<string>& grid) {
         int m = grid.size();
         if (m == 0) {
             return -1;
@@ -81,35 +110,3 @@ public:
         return -1;
     }
 };
-
-int main() {
-    Solution s;
-    vector<string> v({"ahjpjau","ja","ahbwzgqnuk","tnmlanowax"});
-    vector<int> va({4,5,8,2});
-    vector<vector<int>> vb({{3,0}});
-    vector<string> v2({"a","cd"});
-    vector<char> chars({'a','a','a','a','a','b','b','c'});
-    
-    vector<vector<int>> matrix1({
-        {1,1,1,0,1,1,1,1},
-        {1,0,0,0,0,1,1,1},
-        {1,1,1,0,0,0,1,1},
-        {1,1,0,0,0,0,0,0},
-        {1,0,0,0,0,0,0,0},
-        {1,0,0,0,0,0,0,0}
-    });
-    vector<vector<char>> matrix2({
-        {'1','1','1','1','1','1','1','1'},
-        {'1','1','1','1','1','1','1','0'},
-        {'1','1','1','1','1','1','1','0'},
-        {'1','1','1','1','1','0','0','0'},
-        {'0','1','1','1','1','0','0','0'}
-    });
-    vector<vector<int>> matrix3({
-        {1,0},
-        {0,1}
-    });
-    
-    int x = s.shortestPathAllKeys({"@...a",".###A","b.BCc"});
-    return 0;
-}
