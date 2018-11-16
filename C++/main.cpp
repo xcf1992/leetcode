68db61aaa25208d08a9af08f1cf7777c25397a0b
Chenfu Xie <chenfu.xie@hulu.com>
2018-11-15 17:59:43 -0800

    862. Shortest Subarray with Sum at Least K

--- C++/main.cpp
+++ C++/main.cpp
@@ -13,72 +13,35 @@
 using namespace std;
 
 class Solution {
-private:
-    vector<int> rDiff{0, 0, 1, -1};
-    vector<int> cDiff{1, -1, 0, 0};
 public:
-    int shortestPathAllKeys(vector<string> grid) {
-        int m = grid.size();
-        if (m == 0) {
+    int shortestSubarray(vector<int> A, int K) {
+        int n = A.size();
+        if (n == 0) {
             return -1;
         }
-        int n = grid[0].size();
-        
-        unordered_map<int, unordered_set<int>> visited;
-        queue<pair<int, int>> bfs;
-        int target = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '@') {
-                    bfs.push({i * n + j, 0});
-                    visited[i * n + j].insert(0);
-                }
-                else if (grid[i][j] >= 'A' and grid[i][j] <= 'F') {
-                    target |= (1 << (grid[i][j] - 'A'));
+        int result = n + 1;
+        int left = 0;
+        int right = 0;
+        int sum = A[0];
+        while (right < n and left < n) {
+            if (sum < K) {
+                right += 1;
+                if (right < n) {
+                    sum += A[right];
                 }
             }
-        }
-        
-        int step = 0;
-        while (!bfs.empty()) {
-            int curSize = bfs.size();
-            for (int i = 0; i < curSize; i++) {
-                int row = bfs.front().first / n;
-                int col = bfs.front().first % n;
-                int keyMap = bfs.front().second;
-                bfs.pop();
-                
-                if (keyMap == target) {
-                    return step;
-                }
-                
-                for (int d = 0; d < 4; d++) {
-                    int nr = row + rDiff[d];
-                    int nc = col + cDiff[d];
-                    int have = keyMap;
-                    if (nr < 0 or nr >= m or nc < 0 or nc >= n or grid[nr][nc] == '#') {
-                        continue;
-                    }
-                    if (grid[nr][nc] >= 'A' and grid[nr][nc] <= 'F') {
-                        int key = 1 << (grid[nr][nc] - 'A');
-                        if (!(have & key)) {
-                            continue;
-                        }
-                    }
-                    else if (grid[nr][nc] >= 'a' and grid[nr][nc] <= 'f') {
-                        int key = 1 << (grid[nr][nc] - 'a');
-                        have |= key;
-                    }
-                    int pos = nr * n + nc;
-                    if (visited.find(pos) == visited.end() or visited[pos].find(have) == visited[pos].end()) {
-                        visited[pos].insert(have);
-                        bfs.push({pos, have});
-                    }
-                }
+            else if (left <= right){
+                result = min(result, right - left + 1);
+                sum -= A[left];
+                left += 1;
             }
-            step += 1;
         }
-        return -1;
+        while (sum >= K and left < right) {
+            result = min(result, right - left + 1);
+            sum -= A[left];
+            left += 1;
+        }
+        return result > n ? -1 : result;
     }
 };
 
@@ -110,6 +73,6 @@ int main() {
         {0,1}
     });
     
-    int x = s.shortestPathAllKeys({"@...a",".###A","b.BCc"});
+    s.shortestSubarray({84,-37,32,40,95}, 167);
     return 0;
 }
