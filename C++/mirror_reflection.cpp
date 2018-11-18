3223bb78c77a7fdf4cd68b85ba92da19af1ae354
Chenfu Xie <chenfu.xie@hulu.com>
2018-11-17 18:39:58 -0800

    941. Valid Mountain Array

--- C++/mirror_reflection.cpp
+++ C++/mirror_reflection.cpp
@@ -36,6 +36,17 @@
 #include <set>
 using namespace std;
 
+/*
+Intuition and Algorithm
+
+Instead of modelling the ray as a bouncing line, model it as a straight line through reflections of the room.
+
+For example, if p = 2, q = 1, then we can reflect the room horizontally, and draw a straight line from (0, 0) to (4, 2). The ray meets the receptor 2, which was reflected from (0, 2) to (4, 2).
+
+In general, the ray goes to the first integer point (kp, kq) where k is an integer, and kp and kq are multiples of p. Thus, the goal is just to find the smallest k for which kq is a multiple of p.
+
+The mathematical answer is k = p / gcd(p, q).
+*/
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
