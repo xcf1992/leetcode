d753c2f729b1dbe15b1d3ce03b54f5087268ef6a
Chenfu Xie <chenfu.xie@hulu.com>
2018-11-30 18:47:08 -0800

    834. Sum of Distances in Tree

--- C++/image_overlap.cpp
+++ C++/image_overlap.cpp
@@ -37,6 +37,46 @@
 #include <stdio.h>
 using namespace std;
 
+/*
+Intuition:
+If we do brute force, we have 2N horizontal possible sliding, 2N vertical sliding and N^2 to count overlap area.
+We get O(N^4) solution and it may get accepted.
+But we waste out time on case of sparse matrix.
+
+Explanation:
+Assume index in A and B is [0, N * N -1].
+
+Loop on A, if value == 1, save a coordinates i / N * 100 + i % N to LA.
+Loop on B, if value == 1, save a coordinates i / N * 100 + i % N to LB.
+Loop on combination (i, j) of LA and LB, increase count[i - j] by 1.
+If we slide to make A[i] orverlap B[j], we can get 1 point.
+Loop on count and return max values.
+I use a 1 key hashmap. Assume ab for row and cd for col, I make it abcd as coordinate.
+For sure, hashmap with 2 keys will be better for understanding.
+
+Time Complexity:
+As reminded by @specialzsp,
+O(N^2) for preparing, and O(AB) for loop.
+O(AB + N^2)
+
+Update 2018-05-15 about i / N * 100 + i % N
+I find many people discuss it, so I update this explanantion.
+
+1.why 100?
+100 is big enough and very clear.
+For example, If I slide 13 rows and 19 cols, it will be 1319.
+
+why not 30?
+30 is not big enough.
+For example: 409 = 13 * 30 + 19 = 14 * 30 - 11.
+409 can be taken as sliding "14 rows and -11 cols" or "13 rows and 19 cols" at the same time.
+
+How big is enough?
+Bigger than 2N-1. Bigger than 2N-1. Bigger than 2N-1.
+
+Can we replace i / N * 100 + i % N by i?
+No, it's wrong for simple test case [[0,1],[1,1]], [[1,1],[1,0]]
+*/
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
