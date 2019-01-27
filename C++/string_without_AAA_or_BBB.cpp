b14ca90cd1de8321460eb4958e717676c33a8607
Chenfu Xie <chenfu.xie@hulu.com>
2019-01-26 21:01:27 -0800

    984. String Without AAA or BBB

---
+++ C++/string_without_AAA_or_BBB.cpp
@@ -0,0 +1,111 @@
+/*
+ 984. String Without AAA or BBB
+
+ Given two integers A and B, return any string S such that:
+ 
+ S has length A + B and contains exactly A 'a' letters, and exactly B 'b' letters;
+ The substring 'aaa' does not occur in S;
+ The substring 'bbb' does not occur in S.
+ 
+ 
+ Example 1:
+ 
+ Input: A = 1, B = 2
+ Output: "abb"
+ Explanation: "abb", "bab" and "bba" are all correct answers.
+ Example 2:
+ 
+ Input: A = 4, B = 1
+ Output: "aabaa"
+ 
+ 
+ Note:
+ 
+ 0 <= A <= 100
+ 0 <= B <= 100
+ It is guaranteed such an S exists for the given A and B.
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    string strWithout3a3b(int A, int B) {
+        string result = "";
+        while (A > 0 or B > 0) {
+            if (A > B) {
+                if (result.back() == 'a') {
+                    if (B > 0) {
+                        result.push_back('b');
+                        B -= 1;
+                    }
+                    if (A > 0) {
+                        result.push_back('a');
+                        A -= 1;
+                        if (A > 0) {
+                            result.push_back('a');
+                            A -= 1;
+                        }
+                    }
+                }
+                else {
+                    if (A > 0) {
+                        result.push_back('a');
+                        A -= 1;
+                        if (A > 0) {
+                            result.push_back('a');
+                            A -= 1;
+                        }
+                    }
+                    if (B > 0) {
+                        result.push_back('b');
+                        B -= 1;
+                    }
+                }
+            }
+            else {
+                if (result.back() == 'b') {
+                    if (A > 0) {
+                        result.push_back('a');
+                        A -= 1;
+                    }
+                    if (B > 0) {
+                        result.push_back('b');
+                        B -= 1;
+                        if (B > 0) {
+                            result.push_back('b');
+                            B -= 1;
+                        }
+                    }
+                }
+                else {
+                    if (B > 0) {
+                        result.push_back('b');
+                        B -= 1;
+                        if (B > 0) {
+                            result.push_back('b');
+                            B -= 1;
+                        }
+                    }
+                    if (A > 0) {
+                        result.push_back('a');
+                        A -= 1;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
