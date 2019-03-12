29513181422cb3046b9dd5ee40ae9672ebfd255f
Chenfu Xie <chenfu.xie@hulu.com>
2019-03-12 12:52:24 -0700

    639. Decode Ways II

--- C++/decode_ways_II.cpp
+++ C++/decode_ways_II.cpp
@@ -39,32 +39,52 @@
 #include <numeric>
 using namespace std;
 
+/*
+ The idea is DP. One of the hints is that you need mod the answer with a huge prime number.
+
+ For any string s longer than 2, we can decode either the last 2 characters as a whole or the last 1 character. So dp[i] = dp[i-1]* f(s.substr(i,1)) + dp[i-2]* f(s.substr(i-1, 2)). f() is the number of ways to decode a string of length 1 or 2. f() could be 0, for example f("67").
+ There is a lot of cases and corner cases for f(string s). For example, * cannot be '0', so ** has 15 instead of 16 possibilities, because "20" is excluded. But the time complexity is still O(n).
+ */
 class Solution {
+private:
+    int MOD = 1e9 + 7;
+    int helper(string s) {
+        if (s.size() == 1) {
+            if (s[0] == '*') {
+                return 9;
+            }
+            return s[0] == '0'? 0 : 1;
+        }
+        // 11-26, except 20 because '*' is 1-9
+        if (s == "**") {
+            return 15;
+        }
+
+        if (s[1] =='*') {
+            if (s[0] == '1') {
+                return 9;
+            }
+            return s[0] == '2' ? 6 : 0;
+        }
+
+        if (s[0] == '*') {
+            return s[1] <= '6' ? 2 : 1;
+        }
+        // if two digits, it has to be in [10 26]; no leading 0
+        return (stoi(s) >= 10 && stoi(s) <= 26) ? 1 : 0;
+    }
 public:
     int numDecodings(string s) {
-        int MOD = 1e9 + 7;
         int n = s.size();
-        vector<int> dp(n + 1, 0);
-        dp[n] = 1;
-        dp[n - 1] = s[n - 1] == '0' ? 0 : 1;
-        
-        for (int i = n - 2; i >= 0; i--) {
-            if (s[i] == '0') {
-                continue;
-            }
-            if (s[i] != '*') {
-                int num = stoi(s.substr(i, 2));
-                dp[i] = num <= 26 ? dp[i + 1] + dp[i + 2] : dp[i + 1];
-            }
-            else {
-                dp[i] = ((9 * dp[i + 1]) % MOD + dp[i + 2]) % MOD;
-                if (s[i] <= '6') {
-                    dp[i] += + dp[i + 2];
-                }
-            }
-            dp[i] %= MOD;
+        // f2 is the answer to sub string ending at position i; Initially i = 0.
+        long f1 = 1;
+        long f2 = helper(s.substr(0, 1));
+        // DP to get f2 for sub string ending at position n-1;
+        for (int i = 1; i < n; i++) {
+            long f3 = (f2 * helper(s.substr(i, 1))) + (f1 * helper(s.substr(i - 1, 2)));
+            f1 = f2;
+            f2 = f3 % MOD;
         }
-        
-        return dp[0];
+        return f2;
     }
 };
