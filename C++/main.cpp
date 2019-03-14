17f6ce669876c185c9ee0daf1abf29dec38d3dc4
Chenfu Xie <chenfu.xie@hulu.com>
2019-03-13 17:02:58 -0700

    600. Non-negative Integers without Consecutive Ones

--- C++/main.cpp
+++ C++/main.cpp
@@ -14,51 +14,52 @@ using namespace std;
 
 class Solution {
 private:
-    int find(vector<int>& parent, int node) {
-        return parent[node] == 0 ? node : find(parent, parent[node]);
+    string toBinary(int num) {
+        // change num to its binary string, so 6 will be 0011, the most significant bit will be right-most
+        string result = "";
+        while (num != 0) {
+            result.push_back('0' + (num & 1));
+            num >>= 1;
+        }
+        return result;
     }
 public:
-    vector<int> findRedundantDirectedConnection(vector<vector<int>> edges) {
-        int n = edges.size();
-        vector<int> parent(n + 1, 0);
-        vector<vector<int>> result;
-        for (vector<int>& edge : edges) {
-            if (parent[edge[1]] == 0) {
-                parent[edge[1]] = parent[edge[0]];
+    int findIntegers(int num) {
+        string nStr = toBinary(num);
+        int n = nStr.size();
+        vector<int> a(n, 1);
+        vector<int> b(n, 1);
+        for (int i = 1; i < n; i++) {
+            a[i] = a[i - 1] + b[i - 1];
+            b[i] = a[i - 1];
+        }
+
+        int result = a[n - 1] + b[n - 1];
+        /*
+         the count of valid number has n bit, some of them are bigger than num we want and we need to subtract them below
+         then we subtract the solutions which is larger than num, we iterate from the MSB to LSB of num:
+
+         if bit[i] == 1
+         if bit[i + 1] == 0, there's no solutions in res that is larger than num, we go further into smaller bit and subtract.
+         if bit[i + 1] == 1, we know in those res solutions it won't have any consecutive ones. when bit[i + 1] == 1, in one[i + 1], the i-th bit in valid solutions must be 0, which are all smaller than 'num', we don't need to check smaller bits and subtract, so we break form the loop.
+         if bit[i] == 0
+         if bit[i + 1] == 1, there's no solutions in res that is larger than num, we go further into smaller bit and subtract.
+         if bit[i + 1] == 0, we know zero[i + 1] includes solutions of i-th == 0 (00***) and i-th bit == 1 (01***), we know the i-th bit of num is 0, so we need to subtract all the 01*** solutions because it is larger than num. Therefore, one[i] is subtracted from res.
+         */
+        for (int i = n - 2; i >= 0; i--) {
+            if (nStr[i] == '1' and nStr[i + 1] == '1') {
+                break;
             }
-            else {
-                result.push_back({parent[edge[1]], edge[1]});
-                result.push_back(edge);
-                edge[1] = 0;
+            if (nStr[i] == '0' and nStr[i + 1] == '0') {
+                result -= b[i];
             }
         }
-        
-        for (int i = 0; i <= n; i++) {
-            parent[i] = 0;
-        }
-        for (vector<int>& edge : edges) {
-            if (edge[1] == 0) {
-                continue;
-            }
-            
-            int p = edge[0];
-            int c = edge[1];
-            int cur = find(parent, p);
-            if (cur == c) {
-                if (result.empty()) {
-                    return edge;
-                }
-                // cause currently we have removed result[1] already but still has circle
-                return result[0];
-            }
-            parent[c] = cur;
-        }
-        return result[1];
+        return result;
     }
 };
 
 int main() {
     Solution s;
-    s.findRedundantDirectedConnection({{3,1},{2,1},{4,2},{1,4}});
+    s.findIntegers(12);
     return 0;
 }
