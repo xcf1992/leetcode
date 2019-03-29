a3a8eb31a1c7b3afb901c994f122a1ae07b545d2
Chenfu Xie <chenfu.xie@hulu.com>
2019-03-29 16:35:58 -0700

    321. Create Maximum Number

--- C++/create_maximum_number.cpp
+++ C++/create_maximum_number.cpp
@@ -1,3 +1,35 @@
+/*
+ 321. Create Maximum Number
+ Given two arrays of length m and n with digits 0-9 representing two numbers. Create the maximum number of length k <= m + n from digits of the two. The relative order of the digits from the same array must be preserved. Return an array of the k digits.
+
+ Note: You should try to optimize your time and space complexity.
+
+ Example 1:
+
+ Input:
+ nums1 = [3, 4, 6, 5]
+ nums2 = [9, 1, 2, 5, 8, 3]
+ k = 5
+ Output:
+ [9, 8, 6, 5, 3]
+ Example 2:
+
+ Input:
+ nums1 = [6, 7]
+ nums2 = [6, 0, 4]
+ k = 5
+ Output:
+ [6, 7, 6, 0, 4]
+ Example 3:
+
+ Input:
+ nums1 = [3, 9]
+ nums2 = [8, 9]
+ k = 3
+ Output:
+ [9, 8, 9]
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,22 +41,31 @@
 #include <stack>
 #include <stdio.h>
 #include <map>
-#include <numeric>
 using namespace std;
 
+/*
+it is easy to solve problem if nums1.size() + nums2.size() == k
+ so we split the problem into 3 subproblems:
+ 1. get max sub array from nums1 of length i
+ 2. get max sub array from nums2 of length k - i
+ 3. get max merge arrary from maxsub1 and maxsub2
+ 4. get max merge array of length k
+ */
 class Solution {
 private:
     vector<int> mergeArray(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<int> result(k);
-        int idx1 = 0, idx2 = 0;
-        int idx = 0;
-        while(idx < k){
-            if(compareArray(nums1, nums2, idx2, idx1)){
-                result[idx] = nums1[idx1++];
-            }else{
-                result[idx] = nums2[idx2++];
+        int idx1 = 0;
+        int idx2 = 0;
+        for (int i = 0; i < k; ++i) {
+            if (compareArray(nums1, nums2, idx1, idx2)) {
+                result[i] = nums1[idx1];
+                idx1 += 1;
             }
-            idx++;
+            else {
+                result[i] = nums2[idx2];
+                idx2 += 1;
+            }
         }
         return result;
     }
@@ -70,7 +111,7 @@ private:
         return result;
     }
 public:
-    vector<int> maxNumber(vector<int> nums1, vector<int> nums2, int k) {
+    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
         int n1 = nums1.size();
         int n2 = nums2.size();
         if (k == n1 + n2) {
@@ -82,7 +123,7 @@ public:
             if (i <= n1 and k - i <= n2) {
                 vector<int> maxNums1 = maxSubArray(nums1, i);
                 vector<int> maxNums2 = maxSubArray(nums2, k - i);
-                vector<int> temp = mergeArray(nums1, nums2, k);
+                vector<int> temp = mergeArray(maxNums1, maxNums2, k);
                 if (compareArray(temp, result, 0, 0)) {
                     result = temp;
                 }
@@ -91,9 +132,3 @@ public:
         return result;
     }
 };
-
-int main() {
-    Solution s;
-    s.maxNumber({3,4,6,5}, {9,1,2,5,8,3}, 5);
-    return 0;
-}
