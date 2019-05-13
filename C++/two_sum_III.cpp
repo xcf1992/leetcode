ebc81059ce78aa7c9758926fbf7e1e43d1cd3e89
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-13 09:49:13 -0700

    170. Two Sum III - Data structure design

--- C++/two_sum_III.cpp
+++ C++/two_sum_III.cpp
@@ -1,3 +1,23 @@
+/*
+170. Two Sum III - Data structure design
+
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+Example 1:
+
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+Example 2:
+
+add(3); add(1); add(2);
+find(3) -> true
+find(6) -> false
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,33 +26,55 @@
 using namespace std;
 
 class TwoSum {
+    unordered_map<int,int> num_to_count; 
+public:
+    /** Initialize your data structure here. */
+    TwoSum() {}
+    
+    /** Add the number to an internal data structure.. */
+    void add(int number) {
+        num_to_count[number]++;
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    bool find(int value) {
+        for(const auto& el : num_to_count) {
+            if(num_to_count.find(value - el.first) != end(num_to_count) and
+               (value - el.first != el.first or el.second > 1))
+                    return true;
+        }
+        return false;
+    }
+};
+
+class TwoSum1 {
 private:
-    unordered_map<int, bool> table;
-    unordered_map<int, bool> hashtable;
+    unordered_map<int, bool> num;
+    unordered_map<int, bool> sumTable;
     
 public:
-	void add(int number) {
-        if (table.empty()) {
-            table[number] = true;
+    void add(int number) {
+        if (num.empty()) {
+            num[number] = true;
             return;
         }
-        if (table.find(number) != table.end()) {
-            if (hashtable.find(number * 2) == hashtable.end()) {
-                hashtable[number * 2] = true;
+        if (num.find(number) != num.end()) {
+            if (sumTable.find(number * 2) == sumTable.end()) {
+                sumTable[number * 2] = true;
             }
             return;
         }
-        for (auto it = table.begin(); it != table.end(); it++) {
-            if (hashtable.find(number + it->first) == hashtable.end()) {
-                hashtable[number + it->first] = true;
+        for (auto it = num.begin(); it != num.end(); it++) {
+            if (sumTable.find(number + it->first) == sumTable.end()) {
+                sumTable[number + it->first] = true;
             }
         }
-        table[number] = true;
+        num[number] = true;
         return;
 	}
 
 	bool find(int value) {
-	    if (hashtable.find(value) == hashtable.end()) {
+	    if (sumTable.find(value) == sumTable.end()) {
 	        return false;
 	    }
 	    return true;
