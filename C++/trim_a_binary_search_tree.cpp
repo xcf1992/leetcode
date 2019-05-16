52d890c5d19e6613c9c626b0d6e86cd4c5eaff30
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-15 20:15:56 -0700

    669. Trim a Binary Search Tree

--- C++/trim_a_binary_search_tree.cpp
+++ C++/trim_a_binary_search_tree.cpp
@@ -1,3 +1,41 @@
+/*
+669. Trim a Binary Search Tree
+Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+
+Example 1:
+Input:
+    1
+   / \
+  0   2
+
+  L = 1
+  R = 2
+
+Output:
+    1
+      \
+       2
+Example 2:
+Input:
+    3
+   / \
+  0   4
+   \
+    2
+   /
+  1
+
+  L = 1
+  R = 3
+
+Output:
+      3
+     /
+   2
+  /
+ 1
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -27,15 +65,15 @@ public:
         }
 
         if (root -> val > R) {
-            return trimBST(root -> left);
+            return trimBST(root -> left, L, R);
         }
 
         if (root -> val < L) {
-            return trimBST(root -> right);
+            return trimBST(root -> right, L, R);
         }
 
         root -> left = trimBST(root -> left, L, R);
-        root -> left = trimBST(root -> right, L, R);
+        root -> right = trimBST(root -> right, L, R);
         return root;
     }
 };
