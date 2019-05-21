04b43c8d601cfd076760fd37fb951b8a6691c3a3
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-21 16:44:49 -0700

    39. Combination Sum

--- C++/Combination_sum.cpp
+++ C++/Combination_sum.cpp
@@ -5,6 +5,32 @@
 #include <algorithm>
 using namespace std;
 
+class Solution {
+    private:
+    void check(vector<int>& candidates, int target, int start, vector<int> combination, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(combination);
+            return;
+        }
+        
+        for (int i = start; i < candidates.size(); ++i) {
+            if (target < candidates[i]) {
+                return;
+            }
+            combination.push_back(candidates[i]);
+            check(candidates, target - candidates[i], i, combination, result);
+            combination.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> result;
+        check(candidates, target, 0, {}, result);
+        return result;
+    }
+};
+
 void getResults(vector<int> &candidates, int target, int sum,
                     vector<vector<int>> &results, vector<int> &result, int level) {
         if (sum > target ||
