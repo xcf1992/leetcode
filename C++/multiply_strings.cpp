fb7bfa585f41b32b82bb7e8df909624b6db88908
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-22 15:55:22 -0700

    43. Multiply Strings

--- C++/multiply_strings.cpp
+++ C++/multiply_strings.cpp
@@ -5,11 +5,39 @@
 #include <algorithm>
 using namespace std;
 
-string multiply(string num1, string num2) {
+class Solution {
+private:
+    string addString(string num1, string num2) {
+        if (num1.size() < num2.size()) {
+            swap(num1, num2);
+        }
+        
+        int add = 0;
+        int n1 = num1.size() - 1;
+        int n2 = num2.size() - 1;
+        while (n1 >= 0 || n2 >= 0) {
+            int sum = 0;
+            sum += add;
+            if (n1 >= 0) {
+                sum += num1[n1] - '0';
+            }
+            if (n2 >= 0) {
+                sum += num2[n2] - '0';
+            }
+            num1[n1] = sum % 10 + '0';
+            add = sum / 10;
+            n1 -= 1;
+            n2 -= 1;
+        }
+        return add == 0 ? num1 : to_string(add) + num1;
+    }
+public:
+    string multiply(string num1, string num2) {
         if (num1.empty() || num2.empty()) {
             return "0";
         }
-        if (num1.front() == '0' || num2.front() == '0') {
+        
+        if (num1 == "0" || num2 == "0") {
             return "0";
         }
         
@@ -17,56 +45,36 @@ string multiply(string num1, string num2) {
             swap(num1, num2);
         }
         
-        vector<int> n1;
-        for (int i = 0; i < num1.size(); i++) {
-            n1.push_back(num1[i] - '0');
-        }
-        
-        vector<int> n2;
-        for (int i = 0; i != num2.size(); i++) {
-            n2.push_back(num2[i] - '0');
-        } 
-
-        vector<int> r(n1.size(), 0);
-        for (int i = n2.size() - 1; i >= 0; i--) {
-            vector<int> temp(n1.size() + n2.size() - 1 - i, 0);
-            for (int j = n1.size() - 1; j >= 0; j--) {
-               temp[j] = n2[i] * n1[j];
-            }
-                
-            int end1 = temp.size() - 1;
-            int end2 = r.size() - 1;
-            for (int m = 0; m != r.size(); m++) {
-                temp[end1 - m] = temp[end1 - m] + r[end2 - m];
+        vector<string> products;
+        string tail = "";
+        for (int pos = num2.size() - 1; pos >= 0; pos--) {
+            if (num2[pos] == '0') {
+                tail.push_back('0');
+                continue;
             }
             
-            r.resize(temp.size() + 1, 0);
-            end1 = temp.size() - 1;
-            end2 = r.size() - 1;
-            int flag = 0;
-            for (int n = 0; n != temp.size(); n++) {
-                r[end2 - n] = (temp[end1 - n] + flag) % 10;
-                flag = (temp[end1 - n] + flag) / 10;
+            string product = "";
+            int add = 0;
+            int d2 = num2[pos] - '0';
+            for (int idx = num1.size() - 1; idx >= 0; idx--) {
+                int d1 = num1[idx] - '0';
+                int result = d1 * d2 + add;
+                product.push_back('0' + result % 10);
+                add = result / 10;
             }
-            r[0] = flag;
+            reverse(product.begin(), product.end());
+            if (add != 0) {
+                product = to_string(add) + product;
+            }
+            product += tail;
+            products.push_back(product);
+            tail.push_back('0');
         }
         
-        string result;
-        for (int i = 0; i != r.size(); i++) {
-            if (i == 0 && r[i] != 0) {
-                result += '0' + r.at(i);
-            }
-            else {
-                result += '0' + r.at(i);
-            }
+        string addSum = products[0];
+        for (int i = 1; i < products.size(); i++) {
+            addSum = addString(addSum, products[i]);
         }
-        
-        return result;
+        return addSum;
     }
-
-int main() {
-	string s1 = "1";
-	string s2 = "1";
-	multiply(s1, s2);
-	return 0;
-}
+};
