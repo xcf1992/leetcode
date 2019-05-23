4c74402726b98e9a8d04cecf2d1c658bcf41203b
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-23 10:44:54 -0700

    547. Friend Circles - with union find optimization

--- C++/friend_circles.cpp
+++ C++/friend_circles.cpp
@@ -10,7 +10,61 @@
 #include <stdio.h>
 using namespace std;
 
+//https://leetcode.com/problems/friend-circles/discuss/101336/Java-solution-Union-Find
 class Solution {
+private:
+    int find(int num, vector<int>& friends) {
+        while (friends[num] != num) {
+            friends[num] = friends[friends[num]];
+            num = friends[num];
+        }
+        return friends[num];
+    }
+    
+    void unin(int num1, int num2, vector<int>& friends, vector<int>& count) {
+        int f1 = find(num1, friends);
+        int f2 = find(num2, friends);
+        if (f1 == f2) {
+            return;
+        }
+        
+        if (count[f1] > count[f2]) {
+            friends[f2] = f1;
+            count[f1] += count[f2];
+        }
+        else {
+            friends[f1] = f2;
+            count[f2] += count[f1];
+        }
+    }
+public:
+    int findCircleNum(vector<vector<int>>& M) {
+        int n = M.size();
+        vector<int> friends(n, -1);
+        for (int i = 0; i < n; ++i) {
+            friends[i] = i;
+        }
+        vector<int> count(n, 1);
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (M[i][j]) {
+                    unin(i, j, friends, count);
+                }
+            }
+        }
+        
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            if (friends[i] == i) {
+                result += 1;
+            }
+        }
+        return result;
+    }
+};
+
+class Solution1 {
 private:
     void visit(vector<vector<int>>& M, vector<bool>& visited, int start) {
         visited[start] = true;
