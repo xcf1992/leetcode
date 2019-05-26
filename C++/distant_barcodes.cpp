d16c4afd144bdaca4d70e84a511e62c2316bd1b9
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-26 10:38:14 -0700

    1054. Distant Barcodes

--- C++/distant_barcodes.cpp
+++ C++/distant_barcodes.cpp
@@ -1,38 +1,26 @@
 /*
- 1053. Previous Permutation With One Swap
+ 1054. Distant Barcodes
+
+ In a warehouse, there is a row of barcodes, where the i-th barcode is barcodes[i].
  
- Given an array A of positive integers (not necessarily distinct), return the lexicographically largest permutation that is smaller than A, that can be made with one swap (A swap exchanges the positions of two numbers A[i] and A[j]).  If it cannot be done, then return the same array.
+ Rearrange the barcodes so that no two adjacent barcodes are equal.  You may return any answer, and it is guaranteed an answer exists.
  
  
  
  Example 1:
  
- Input: [3,2,1]
- Output: [3,1,2]
- Explanation:
- Swapping 2 and 1.
+ Input: [1,1,1,2,2,2]
+ Output: [2,1,2,1,2,1]
  Example 2:
  
- Input: [1,1,5]
- Output: [1,1,5]
- Explanation:
- This is already the smallest permutation.
- Example 3:
- 
- Input: [1,9,4,6,7]
- Output: [1,7,4,6,9]
- Explanation:
- Swapping 9 and 7.
- Example 4:
- 
- Input: [3,1,1,3]
- Output: [1,1,3,3]
+ Input: [1,1,1,1,2,2,3,3]
+ Output: [1,3,1,3,2,1,2,1]
  
  
  Note:
  
- 1 <= A.length <= 10000
- 1 <= A[i] <= 10000
+ 1 <= barcodes.length <= 10000
+ 1 <= barcodes[i] <= 10000
  */
 
 #include <iostream>
@@ -49,3 +37,46 @@
 #include <set>
 #include <numeric>
 using namespace std;
+
+struct myComp {
+    bool operator()(pair<int, int>& a, pair<int, int>& b) {
+        return a.second < b.second or (a.second == b.second and a < b);
+    }
+};
+class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+        unordered_map<int, int> count;
+        for (int code : barcodes) {
+            count[code] += 1;
+        }
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, myComp> pq;
+        for (auto& it : count) {
+            pq.push({it.first, it.second});
+        }
+        
+        vector<int> result;
+        while (!pq.empty()) {
+            int code1 = pq.top().first;
+            int count1 = pq.top().second;
+            pq.pop();
+            
+            result.push_back(code1);
+            if (!pq.empty()) {
+                int code2 = pq.top().first;
+                int count2 = pq.top().second;
+                pq.pop();
+                
+                result.push_back(code2);
+                if (count2 - 1 > 0) {
+                    pq.push({code2, count2 - 1});
+                }
+            }
+            if (count1 - 1 > 0) {
+                pq.push({code1, count1 - 1});
+            }
+        }
+        return result;
+    }
+};
