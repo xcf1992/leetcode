7923f352d5c0328ad080c16cb5db427fc37bdeb8
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-25 17:23:59 -0700

    324. Wiggle Sort II

--- C++/wiggle_sort_II.cpp
+++ C++/wiggle_sort_II.cpp
@@ -31,6 +31,7 @@
 #include <set>
 using namespace std;
 
+// wrong for [4,5,5,6]
 class Solution1 {
 public:
     void wiggleSort(vector<int>& nums) {
@@ -76,6 +77,43 @@ public:
  Accessing A(8) actually accesses nums[6].
  Accessing A(9) actually accesses nums[8].
  */
+
+/*
+This is to explain why mapped index formula is (1 + 2*index) % (n | 1)
+
+Notice that by placing the median in it's place in the array we divided the array in 3 chunks: all numbers less than median are in one side, all numbers larger than median are on the other side, median is in the dead center of the array.
+
+We want to place any a group of numbers (larger than median) in odd slots, and another group of numbers (smaller than median) in even slots. So all numbers on left of the median < n / 2 should be in odd slots, all numbers on right of the median > n / 2 should go into even slots (remember that median is its correct place at n / 2)
+
+PS: I'm ignoring the discussion of odd/even array length for simplicity.
+
+So let's think about the first group in the odd slots, all numbers is the left side of the array should go into these odd slots. What's the formula for it? Naturally it would be:
+(1 + 2 x index) % n
+
+All these indexes are less than n / 2 so multiplying by 2 and add 1 (to make them go to odd place) and then mod by n will always guarantee that they are less than n.
+
+Original Index => Mapped Index
+0 => (1 + 2 x 0) % 6 = 1 % 6 = 1
+1 => (1 + 2 x 1) % 6 = 3 % 6 = 3
+2 => (1 + 2 x 2) % 6 = 5 % 6 = 5
+
+These are what's less than median, if we continue this with indexes 3, 4, 5 we will cycle again:
+3 => (1 + 2 x 3) % 6 = 7 % 6 = 1
+4 => (1 + 2 x 4) % 6 = 9 % 6 = 3
+5 => (1 + 2 x 5) % 6 = 11 % 6 = 5
+
+and we don't want that, so for indexes larger than n/2 we want them to be even, (n|1) does that exactly. What n|1 does it that it gets the next odd number to n if it was even
+if n = 6 for example 110 | 1 = 111 = 7
+if n = 7 for example 111 | 1 = 111 = 7
+
+and this is what we want, instead of cycling the odd numbers again we want them to be even, and odd % odd number is even so updating the formula to :
+(1 + 2*index) % (n | 1)
+
+Then we have:
+3 => (1 + 2 x 3) % 7 = 7 % 7 = 0
+4 => (1 + 2 x 4) % 7 = 9 % 7 = 2
+5 => (1 + 2 x 5) % 7 = 11 % 7 = 4
+*/
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
@@ -86,18 +124,18 @@ public:
         
         #define A(i) nums[(1+2*(i)) % (n|1)]
         
-        int i = 0;
-        int j = 0;
-        int k = n - 1;
-        while (j <= k) {
-            if (A(j) > midNum) {
-                swap(A(i++), A(j++));
+        int left = 0;
+        int cur = 0;
+        int right = n - 1;
+        while (cur <= right) {
+            if (A(cur) > midNum) {
+                swap(A(left++), A(cur++));
             }
-            else if (A(j) < midNum) {
-                swap(A(j), A(k--));
+            else if (A(cur) < midNum) {
+                swap(A(cur), A(right--));
             }
             else {
-                j++;
+                cur++;
             }
         }
     }
