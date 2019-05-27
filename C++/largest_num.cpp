b9e62fd017297d4e9a89bc32925525926206a41d
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-27 16:36:53 -0700

    179. Largest Number

--- C++/largest_num.cpp
+++ C++/largest_num.cpp
@@ -1,18 +1,54 @@
 #include <iostream>
 #include <string>
+/*
+179. Largest Number
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+Example 1:
+
+Input: [10,2]
+Output: "210"
+Example 2:
+
+Input: [3,30,34,5,9]
+Output: "9534330"
+Note: The result may be very large, so you need to return a string instead of an integer.
+*/
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
 using namespace std;
 
-int main() {
-	string s1 = "09999";
-	string s2 = "1";
-	if (s1 > s2) {
-		cout << "s1 > s2" << endl;
-	}
-	else {
-		cout << "s1 < s2" << endl;
-	}
-	return 0;
-}
+class Compare {
+public:
+    bool operator() (string s1, string s2) {
+        return s1 + s2 < s2 + s1;
+    }
+};
+
+class Solution {
+public:
+    string largestNumber(vector<int> &num) {
+        if (num.empty()) {
+            return "";
+        }
+        
+        vector<string> allNum;
+        for (int i = 0; i < num.size(); i++) {
+            allNum.push_back(to_string(num[i]));
+        }
+        
+        sort(allNum.begin(), allNum.end(), Compare());
+        
+        if (allNum.back()[0] == '0') {
+            return "0";
+        }
+        
+        string result = "";
+        for (int i = allNum.size() - 1; i >= 0; i--) {
+            result += allNum[i];
+        }
+        
+        return result;
+    }
+};
