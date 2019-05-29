f66a656f426102463dd9cc824832a6ac57339a88
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-28 21:00:26 -0700

    1008. Construct Binary Search Tree from Preorder Traversal

--- C++/construct_binary_search_tree_from_preorder_traversal.cpp
+++ C++/construct_binary_search_tree_from_preorder_traversal.cpp
@@ -41,7 +41,72 @@ using namespace std;
       TreeNode *right;
       TreeNode(int x) : val(x), left(NULL), right(NULL) {}
   };
-class Solution {
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution { // iterative same process as 255. Verify Preorder Sequence in Binary Search Tree
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        if (preorder.empty()) {
+            return nullptr;
+        }
+        
+        TreeNode* root = new TreeNode(preorder[0]);
+        stack<TreeNode*> stk;
+        stk.push(root);
+        
+        for (int i = 1; i < preorder.size(); ++i) {
+            TreeNode* node = new TreeNode(preorder[i]);
+            if (!stk.empty() and stk.top() -> val > node -> val) {
+                stk.top() -> left = node;
+            }
+            else {
+                TreeNode* parent = nullptr;
+                while (!stk.empty() and stk.top() -> val < node -> val) {
+                    parent = stk.top();
+                    stk.pop();
+                }
+                parent -> right = node;
+            }
+            stk.push(node);
+        }
+        return root;
+    }
+};
+
+class Solution2 { // recursive only loop once
+private:
+    TreeNode* construct(vector<int>& preorder, int& pos, int minVal, int maxVal) {
+        if (pos >= preorder.size()) {
+            return nullptr;
+        }
+        
+        int value = preorder[pos];
+        if (value < minVal or value > maxVal) {
+            return nullptr;
+        }
+        
+        TreeNode* root = new TreeNode(value);
+        pos += 1;
+        root -> left = construct(preorder, pos, minVal, value);
+        root -> right = construct(preorder, pos, value, maxVal);
+        return root;
+    }
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int pos = 0;
+        return construct(preorder, pos, INT_MIN, INT_MAX);
+    }
+};
+
+class Solution1 { // simple recursive
 private:
     TreeNode* construct(int start, int end, vector<int> num) {
         if (start >= end) {
--- C++/verify_preorder_sequence_in_binary_search_tree.cpp
+++ C++/verify_preorder_sequence_in_binary_search_tree.cpp
@@ -1,11 +1,27 @@
-//
-//  verify_preorder_sequence_in_binary_search_tree.cpp
-//  C++
-//
-//  Created by Chenfu Xie on 3/2/18.
-//  Copyright © 2018 Chenfu Xie. All rights reserved.
-//
+/*
+255. Verify Preorder Sequence in Binary Search Tree
 
+Given an array of numbers, verify whether it is the correct preorder traversal sequence of a binary search tree.
+
+You may assume each number in the sequence is unique.
+
+Consider the following binary search tree: 
+
+     5
+    / \
+   2   6
+  / \
+ 1   3
+Example 1:
+
+Input: [5,2,6,1,3]
+Output: false
+Example 2:
+
+Input: [5,2,1,3,6]
+Output: true
+Follow up:
+Could you do it using only constant space complexity?*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -35,19 +51,20 @@ public:
     bool verifyPreorder(vector<int>& preorder) {
         stack<int> stk;
         int lower_bound = INT_MIN;
-        for(int i = 0; i < preorder.size(); i++){
-            if(stk.empty() || preorder[i] < preorder[i - 1]){
-                if(preorder[i] <= lower_bound) return false;
-                stk.push(preorder[i]);
-            }else{
-                while(!stk.empty() && stk.top() < preorder[i]){
+        for (int i = 0; i < preorder.size(); i++) {
+            if (stk.empty() || preorder[i] < preorder[i - 1]) {
+                if (preorder[i] <= lower_bound) {
+                    return false;
+                }
+            }
+            else{
+                while (!stk.empty() && stk.top() < preorder[i]) {
                     lower_bound = stk.top();
                     stk.pop();
                 }
-                stk.push(preorder[i]);
             }
+            stk.push(preorder[i]);
         }
-        
         return true;
     }
 };
