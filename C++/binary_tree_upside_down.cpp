269219bd39b47bffa37e8f61b037cf8f80e62529
Chenfu Xie <chenfu.xie@hulu.com>
2019-05-31 11:49:23 -0700

    156. Binary Tree Upside Down

--- C++/binary_tree_upside_down.cpp
+++ C++/binary_tree_upside_down.cpp
@@ -1,3 +1,42 @@
+/*
+156. Binary Tree Upside Down
+
+Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+
+Example:
+
+Input: [1,2,3,4,5]
+
+    1
+   / \
+  2   3
+ / \
+4   5
+
+Output: return the root of the binary tree [4,5,2,#,#,3,1]
+
+   4
+  / \
+ 5   2
+    / \
+   3   1  
+Clarification:
+
+Confused what [4,5,2,#,#,3,1] means? Read more below on how binary tree is serialized on OJ.
+
+The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+
+Here's an example:
+
+   1
+  / \
+ 2   3
+    /
+   4
+    \
+     5
+The above binary tree is serialized as [1,2,3,#,#,4,#,#,5].
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -16,67 +55,22 @@ using namespace std;
 class Solution {
 public:
     TreeNode *upsideDownBinaryTree(TreeNode *root) {
-        if (root == nullptr) {
+        if (root == nullptr || root -> left == nullptr) {
             return root;
         }
 
-        TreeNode *left = root->left;
-        TreeNode *right = root->right;
-        TreeNode *node = upsideDown(left);
-        if (node == nullptr) {
-            return root;
+        TreeNode *lc = root -> left;
+        TreeNode *rc = root -> right;
+        root -> right = nullptr;
+        root -> left = nullptr;
+        
+        TreeNode *newRoot = upsideDownBinaryTree(lc);
+        TreeNode* cur = newRoot;
+        while (cur -> right != nullptr) {
+            cur = cur -> right;
         }
-
-        node->left = right;
-        node->right = root;
-        root->right = nullptr;
-        return left;
-    }
-
-    TreeNode *upsideDown(TreeNode *root) {
-        if (root == nullptr) {
-            return nullptr;
-        }
-
-        TreeNode *left = root->left;
-        TreeNode *right = root->right;
-
-        TreeNode *newLeft = upsideDown(left);
-        if (newLeft == nullptr) {
-            return root;
-        }
-
-        newLeft->left = right;
-        newLeft->right = root;
-        root->right = nullptr;
-
-        return root;
+        cur -> right = root;
+        cur -> left = rc;
+        return newRoot;
     }
 };
-
-
-int numSeqHelper(int ones, int twos, int threes, int fours, int last){
-	if(ones ==0 && twos ==0 && threes ==0 && fours ==0)
-		return 1;
-
-	int sum = 0;
-
-	if(last != 1 && ones != 0)
-		sum += numSeqHelper(ones-1, twos, threes, fours, 1);
-	if(last != 2 && twos != 0)
-		sum += numSeqHelper(ones, twos-1, threes, fours, 2);
-	if(last != 3 && threes != 0)
-		sum += numSeqHelper(ones, twos, threes-1, fours, 3);
-	if(last != 4 && fours != 0)
-		sum += numSeqHelper(ones, twos, threes, fours-1, 4);
-
-	return sum;
-}
-
-int numSeq(int ones, int twos, int threes, int fours){
-	 return numSeqHelper(ones, twos, threes, fours, 0) ;
-}
-int main() {
-	int i = numSeq(1,1,1,4);
-	return 0;
-}
