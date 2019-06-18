eaaeb65ee2a5e496b8c1672d3adbf8f8143a1bd0
Chenfu Xie <chenfu.xie@hulu.com>
2019-06-18 16:42:34 -0700

    41. First Missing Positive

--- C++/first_missing_positive.cpp
+++ C++/first_missing_positive.cpp
@@ -33,17 +33,17 @@ using namespace std;
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int smallest = -1;
-        int largest = 0;
-        for (int k : nums) {
-            if (k > 0) {
-                if (smallest == -1 or k < smallest) {
-                    smallest = k;
-                }
-                if (k > largest) {
-                    largest = k;
-                }
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 and nums[i] <= n and nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
             }
         }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
 };
