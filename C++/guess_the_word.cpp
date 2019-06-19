09cabbe19705d95851f6abb31f4cf169cfdc82d7
Chenfu Xie <chenfu.xie@hulu.com>
2019-06-19 11:37:22 -0700

    25. Reverse Nodes in k-Group

--- C++/guess_the_word.cpp
+++ C++/guess_the_word.cpp
@@ -1,4 +1,5 @@
 /*
+ 843. Guess the Word
  This problem is an interactive problem new to the LeetCode platform.
  
  We are given a word list of unique words, each word is 6 letters long, and one word in this list is chosen as secret.
@@ -25,7 +26,6 @@
  We made 5 calls to master.guess and one of them was the secret, so we pass the test case.
  Note:  Any solutions that attempt to circumvent the judge will result in disqualification.
  */
-
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -45,14 +45,86 @@ using namespace std;
 /**
  * // This is the Master's API interface.
  * // You should not implement it, or speculate about its implementation
- * class Master {
- *   public:
- *     int guess(string word);
- * };
  */
+class Master {
+   public:
+     int guess(string word);
+};
+
+/*
+Now we want to find a solution that improve this rate.
+
+Generally, we will get 0 matches from the master.guess.
+
+As a result, the size of wordlist reduces slowly.
+
+Here we're going to assume that, we will always run into the worst case.
+
+We need to guess a word that can minimum our worst outcome.
+
+So we compare each two words and count their matches.
+
+For each word, we note how many word of 0 matches it gets.
+
+Then we guess the word with minimum words of 0 matches.
+
+In this solution, we apply a minimax idea.
+
+We minimize our worst case,
+
+where the worst case is max(the number of words with i matches)
+*/
 class Solution {
 private:
-    int match(string&a,string&b){
+    int match(string& a, string& b){
+        int res = 0;
+        for(int i = 0; i < a.size(); i++)
+            res += a[i] == b[i] ? 1 : 0;
+        return res;
+    }
+public:
+/*
+Anyone who doesn't know why checking 0 match instead of 1,2,3...6 matches, please take a look at this comment. 
+The probability of two words with 0 match is (25/26)^6 = 80%. 
+That is to say, for a candidate word, we have 80% chance to see 0 match with the secret word. 
+In this case, we had 80% chance to eliminate the candidate word and its "family" words which have at least 1 match. 
+Additionally, in order to delete a max part of words, we select a candidate who has a big "family" (fewest 0 match with other words).
+
+Since the words are generated randomly. 
+So a word most probably has 0 matches with the secret word(80% possibility). 
+That's why we minimize the 0 matches rather than minimize the maximum matches.
+
+randomly generate in the above solution
+minimax: 因为你会保留其中一个group（和自己share 0,1,2,3,4,5,6这7个group中的一个），
+但是你不知道会保留哪一个group，所以使得所有group中的峰值最小的词，这样每一步可以保证删除比较多的词，而不至于落入极端情况，一步中删除很多，或者完全没怎么删除
+这个post解释得比较好：https://leetcode.com/problems/guess-the-word/discuss/134251/Optimal-MinMax-Solution-(+-extra-challenging-test-cases)
+
+https://leetcode.com/problems/guess-the-word/discuss/134087/C++-elimination-histogram-beats-Minimax
+*/
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        for (int i = 0, x = 0; i < 10 && x < 6; ++i) {
+            unordered_map<string, int> count;
+            for (string w1 : wordlist)
+                for (string w2 : wordlist)
+                    if (match(w1, w2) == 0)
+                        count[w1]++;
+            pair<string, int> minimax = make_pair(wordlist[0], 1000);
+            for (string w : wordlist)
+                if (count[w] <= minimax.second)
+                    minimax = make_pair(w, count[w]);
+            x = master.guess(minimax.first);
+            vector<string> wordlist2;
+            for (string w : wordlist)
+                if (match(minimax.first, w) == x)
+                    wordlist2.push_back(w);
+            wordlist = wordlist2;
+        }
+    }
+};
+
+class Solution2 {
+private:
+    int match(string& a, string& b){
         int res = 0;
         for(int i = 0; i < a.size(); i++)
             res += a[i] == b[i] ? 1 : 0;
---
+++ C++/reverse_nodes_in_k_group.cpp
@@ -0,0 +1,81 @@
+/*
+25. Reverse Nodes in k-Group
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+Example:
+
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+
+Note:
+
+Only constant extra memory is allowed.
+You may not alter the values in the list's nodes, only nodes itself may be changed.
+ */
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+#include <numeric>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+private:
+    void reverse(ListNode* head, ListNode* tail) {
+        if (head == tail) {
+            return;
+        }
+
+        ListNode* suc = head -> next;
+        reverse(suc, tail);
+        suc -> next = head;
+    }
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (head == nullptr or head -> next == nullptr or k == 1) {
+            return head;
+        }
+
+        ListNode* preHead = new ListNode(-1);
+        preHead -> next = head;
+        ListNode* tail = head;
+        ListNode* pre = preHead;
+        int len = 1;
+        while (tail != nullptr) {
+            tail = tail -> next;
+            len += 1;
+            if (len == k and tail != nullptr) {
+                ListNode* curHead = pre -> next;
+                ListNode* cur = tail -> next;
+                reverse(curHead, tail);
+                curHead -> next = cur;
+                pre -> next = tail;
+
+                pre = curHead;
+                tail = cur;
+                len = 1;
+            }
+        }
+        return preHead -> next;
+    }
+};
