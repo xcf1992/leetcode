4e042fd8e4a13c7e8c52675ca99d11a3b90d5fd7
Chenfu Xie <chenfu.xie@hulu.com>
2019-06-20 15:20:24 -0700

    44. Wildcard Matching

--- C++/employee_free_time.cpp
+++ C++/employee_free_time.cpp
@@ -1,3 +1,38 @@
+/*
+759. Employee Free Time
+We are given a list schedule of employees, which represents the working time for each employee.
+
+Each employee has a list of non-overlapping Intervals, and these intervals are in sorted order.
+
+Return the list of finite intervals representing common, positive-length free time for all employees, also in sorted order.
+
+Example 1:
+
+Input: schedule = [[[1,2],[5,6]],[[1,3]],[[4,10]]]
+Output: [[3,4]]
+Explanation:
+There are a total of three employees, and all common
+free time intervals would be [-inf, 1], [3, 4], [10, inf].
+We discard any intervals that contain inf as they aren't finite.
+
+
+Example 2:
+
+Input: schedule = [[[1,3],[6,7]],[[2,4]],[[2,5],[9,12]]]
+Output: [[5,6],[7,9]]
+
+
+(Even though we are representing Intervals in the form [x, y], the objects inside are Intervals, not lists or arrays.
+For example, schedule[0][0].start = 1, schedule[0][0].end = 2, and schedule[0][0][0] is not defined.)
+
+Also, we wouldn't include intervals like [5, 5] in our answer, as they have zero length.
+
+Note:
+
+schedule and schedule[i] are lists with lengths in range [1, 50].
+0 <= schedule[i].start < schedule[i].end <= 10^8.
+NOTE: input types have been changed on June 17, 2019. Please reset to default code definition to get new method signature.
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,42 +43,67 @@
 #include <queue>
 #include <stack>
 #include <stdio.h>
+#include <map>
 using namespace std;
 
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
+struct Interval {
+      int start;
+      int end;
+      Interval() : start(0), end(0) {}
+      Interval(int s, int e) : start(s), end(e) {}
+  };
+
 class Solution {
 public:
-    vector<Interval> employeeFreeTime(vector<vector<Interval>>& schedules) {
+    vector<Interval*> employeeFreeTime(vector<vector<Interval*>>& schedules) {
         map<int, int> timeline;
-        for (auto schedule : schedules) {
-            for (Interval interval : schedule) {
-                timeline[interval.start] += 1;
-                timeline[interval.end] -= 1;
+        for (auto& schedule : schedules) {
+            for (Interval* interval : schedule) {
+                timeline[interval -> start] += 1;
+                timeline[interval -> end] -= 1;
             }
         }
-        
-        vector<Interval> result;
+
+        vector<Interval*> result;
         int workers = 0;
         for (pair<int, int> busy : timeline) {
             workers += busy.second;
             if (workers == 0) {
-                result.push_back(Interval(busy.first, 0));
+                result.push_back(new Interval(busy.first, 0));
             }
-            else if (!result.empty() && result.back().end == 0) {
-                result.back().end = busy.first;
+            else if (!result.empty() && result.back() -> end == 0) {
+                result.back() -> end = busy.first;
             }
         }
-        
-        if (result.back().end == 0) {
+
+        if (result.back() -> end == 0) {
             result.pop_back();
+        }
+        return result;
+    }
+};
+
+class Solution { //5.27%
+public:
+    vector<Interval*> employeeFreeTime(vector<vector<Interval*>>& schedules) {
+        vector<Interval*> intervals;
+        for (vector<Interval*> schedule : schedules) {
+            for (Interval* it : schedule) {
+                intervals.push_back(it);
+            }
+        }
+
+        sort(intervals.begin(), intervals.end(), [](Interval* a, Interval* b) {
+            return a -> start < b -> start or (a -> start == b -> start and a -> end < b -> end);
+        });
+
+        vector<Interval*> result;
+        int end = intervals[0] -> end;
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i] -> start > end) {
+                result.push_back(new Interval(end, intervals[i] -> start));
+            }
+            end = max(end, intervals[i] -> end);
         }
         return result;
     }
---
+++ C++/wildcard_matching.cpp
@@ -0,0 +1,96 @@
+/*
+44. Wildcard Matching
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Note:
+
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+Example 1:
+
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input:
+s = "aa"
+p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+Example 3:
+
+Input:
+s = "cb"
+p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+Example 4:
+
+Input:
+s = "adceb"
+p = "*a*b"
+Output: true
+Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+Example 5:
+
+Input:
+s = "acdcb"
+p = "a*c?b"
+Output: false
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+private:
+    /*
+     * 0: reach the end of s but unmatched
+     * 1: unmatched without reaching the end of s
+     * 2: matched
+     */
+    int check(string& s, string& p, int si, int pi) {
+        if (si == s.size() and pi == p.size()) {
+            return 2;
+        }
+
+        if (si == s.size() and p[pi] != '*') {
+            return 0;
+        }
+
+        if (pi == p.size()) {
+            return 1;
+        }
+
+        if (p[pi] == '*') {
+            if (pi + 1 < p.size() and p[pi + 1] == '*') {
+                return check(s, p, si, pi + 1);
+            }
+            for (int i = 0; i + si <= s.size(); ++i) {
+                int result = check(s, p, si + i, pi + 1);
+                if (result == 2 or result == 0) {
+                    return result;
+                }
+            }
+        }
+
+        if (p[pi] == '?' or s[si] == p[pi]) {
+            return check(s, p, si + 1, pi + 1);
+        }
+        return 1;
+    }
+public:
+    bool isMatch(string s, string p) {
+        return check(s, p, 0, 0) == 2;
+    }
+};
