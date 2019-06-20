5e26541fa3b58af52184f66a14841052a6694ae6
Chenfu Xie <chenfu.xie@hulu.com>
2019-06-19 17:19:35 -0700

    edit distance

--- C++/Edit_distance.cpp
+++ C++/Edit_distance.cpp
@@ -1,9 +1,41 @@
+/*
+72. Edit Distance
+Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation:
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+Example 2:
+
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation:
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
 class Solution {
 public:
-    int diff(char a, char b) {
-        return a == b ? 0 : 1;
-    }
-
     int minDistance(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
@@ -12,8 +44,6 @@ public:
         }
 
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        dp[0][0] = 0;
-
         for (int i = 1; i < m + 1; i++) {
             dp[i][0] = i;
         }
@@ -22,11 +52,10 @@ public:
         }
         for (int i = 1; i < m + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
-                dp[i][j] = min(dp[i - 1][j - 1] + diff(word1[i - 1], word2[j - 1]),
-                    min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
+                dp[i][j] = min(dp[i - 1][j - 1] + (word1[i - 1] == word2[j - 1] ? 0 : 1),
+                               min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
             }
         }
-
         return dp[m][n];
     }
 };
