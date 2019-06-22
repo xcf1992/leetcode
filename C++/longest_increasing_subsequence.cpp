77cde5dfda4266dbe1b10cc70257f00a3f8ab6a3
Chenfu Xie <chenfu.xie@hulu.com>
2019-06-22 14:34:05 -0700

    300. Longest Increasing Subsequence

--- C++/longest_increasing_subsequence.cpp
+++ C++/longest_increasing_subsequence.cpp
@@ -1,3 +1,18 @@
+/*
+300. Longest Increasing Subsequence
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+Example:
+
+Input: [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Note:
+
+There may be more than one LIS combination, it is only necessary for you to return the length.
+Your algorithm should run in O(n2) complexity.
+Follow up: Could you improve it to O(n log n) time complexity?
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,18 +25,46 @@
 #include <stdio.h>
 using namespace std;
 
-
+/*
+https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
+Basically we are trying to log longest increasing subsequence in lis
+if current num is bigger then current sequence we push it to the back
+if any number in current lis is bigger than current num we replace it with current num
+cause the size of lis is non-shrinking, which means it will always be the length of longest increasing subsequnce we ever seen
+so by replacing internal element with current num(which is smaller), we hope we can longer sequence after that.
+*/
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
+        int n = nums.size();
+        if (n <= 1) {
+            return n;
         }
 
+        vector<int> lis;
+        for (int num : nums) {
+            auto it = lower_bound(lis.begin(), lis.end(), num);
+            if (it == lis.end()) {
+                lis.push_back(num);
+            }
+            else {
+                *it = num;
+            }
+        }
+        return lis.size();
+    }
+};
+
+class Solution1 {
+public:
+    int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+        if (n <= 1) {
+            return n;
+        }
+
         vector<int> dp(n, 1);
         int result = 1;
-
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
@@ -30,12 +73,6 @@ public:
             }
             result = max(result, dp[i]);
         }
-
         return result;
     }
 };
-
-int main() {
-    Solution s;
-    return 0;
-}
--- C++/russian_doll_envelopes.cpp
+++ C++/russian_doll_envelopes.cpp
@@ -1,7 +1,9 @@
 /*
  354. Russian Doll Envelopes
 
- You have a number of envelopes with widths and heights given as a pair of integers (w, h). One envelope can fit into another if and only if both the width and height of one envelope is greater than the width and height of the other envelope.
+ You have a number of envelopes with widths and heights given as a pair of integers (w, h).
+ One envelope can fit into another if and only if both the width and height of one envelope
+ is greater than the width and height of the other envelope.
 
  What is the maximum number of envelopes can you Russian doll? (put one inside other)
 
@@ -14,7 +16,6 @@
  Output: 3
  Explanation: The maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
  */
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -31,20 +32,21 @@ using namespace std;
 
 class Solution {
 public:
-    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
         int n = envelopes.size();
         if (n <= 1) {
             return n;
         }
-        sort(envelopes.begin(), envelopes.end(), [](pair<int, int>& a, pair<int, int>& b) {
-            return a.first < b.first or (a.first == b.first and a.second < b.second);
+
+        sort(envelopes.begin(), envelopes.end(), [](vector<int>& a, vector<int>& b) {
+            return a[0] < b[0] or (a[0] == b[0] and a[1] < b[1]);
         });
 
         vector<int> dp(n, 1);
         int result = 1;
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (envelopes[i].first > envelopes[j].first and envelopes[i].second > envelopes[j].second) {
+                if (envelopes[i][0] > envelopes[j][0] and envelopes[i][1] > envelopes[j][1]) {
                     dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
