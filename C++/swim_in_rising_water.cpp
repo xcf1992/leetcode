1d371dc1b9f298cbb65d48f72e59a8b01a733fff
Chenfu Xie <chenfu.xie@hulu.com>
2019-06-26 20:06:05 -0700

    778. Swim in Rising Water

--- C++/swim_in_rising_water.cpp
+++ C++/swim_in_rising_water.cpp
@@ -48,19 +48,78 @@ grid[i][j] is a permutation of [0, ..., N*N - 1].
 using namespace std;
 
 /*
-Imagine the grid as 
-So what the problem ask is to find a
+Let's keep a priority queue of which square we can walk in next.
+We always walk in the smallest one that is 4-directionally adjacent to ones we've visited.
+
+When we reach the target, the largest number we've visited so far is the answer.
 */
+struct T { // {height, row, col}
+    int t, x, y;
+    T(int a, int b, int c) : t (a), x (b), y (c){}
+    bool operator< (const T &d) const {
+        return t > d.t;
+    }
+};
 class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int N = grid.size();
+        int res = 0;
+        priority_queue<T> pq;
+        pq.push(T(grid[0][0], 0, 0));
+        vector<vector<int>> seen(N, vector<int>(N, 0));
+        seen[0][0] = 1;
+        static int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, { -1, 0}};
+
+        while (true) {
+            auto p = pq.top();
+            pq.pop ();
+
+            res = max(res, p.t);
+            if (p.x == N - 1 and p.y == N - 1) {
+                return res;
+            }
+
+            for (auto& d : dir) {
+                int i = p.x + d[0];
+                int j = p.y + d[1];
+                if (i >= 0 and i < N and j >= 0 and j < N and !seen[i][j]) {
+                    seen[i][j] = 1;
+                    pq.push(T(grid[i][j], i, j));
+                }
+            }
+        }
+    }
+};
+
+/*
+Imagine the grid as a place with different elevations at different spot
+and the elevation of each spot will be changed to max(t, original_elevation)
+which means a spot with elevation 5 at beginning, the elevation when change in first 5 seconds
+but change to 6 at 6 time 6 and so on so forth.
+
+So what the problem ask is to find a path from (0,0) to (n - 1, n - 1)
+and you can walk through the path unless the elevation of each spot along the path is the same,
+which will be the evevation of highest spot at time 0
+
+there are different paths from start to end, and the time takes will be the same as max elevation spot along each path
+we need to find the path with minimum highest elevation.
+*/
+class Solution1 { // 5.02%
 private:
     int n = 0;
     vector<int> diff = {0, 1, 0, -1, 0};
 
+    // so dp[i][j] stores the best elevation path we have seen so far from start to current spot
     void dfs(vector<vector<int>> &grid, int row, int col, int elevation, vector<vector<int>> &dp) {
         if (row < 0 or col < 0 or row >= n or col >= n) {
             return;
         }
         if (elevation >= dp[row][col]) {
+            /*
+            * if the new elevation is larger than current dp value
+            * which means the path is worse than previous one we have seen before
+            */
             return;
         }
 
