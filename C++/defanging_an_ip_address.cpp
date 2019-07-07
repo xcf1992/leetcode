f181993fe99d300103bea2cc244a33461c462fbe
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-06 21:11:13 -0700

    1108. Defanging an IP Address

---
+++ C++/defanging_an_ip_address.cpp
@@ -0,0 +1,52 @@
+/*
+1108. Defanging an IP Address
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+
+
+Constraints:
+
+The given address is a valid IPv4 address.
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string result = "";
+        for (char c : address) {
+            if (c == '.') {
+                result += "[.]";
+            }
+            else {
+                result.push_back(c);
+            }
+        }
+        return result;
+    }
+};
