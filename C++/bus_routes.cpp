55ece554b51d2cdacd51e1ebbde21798785366bb
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-08 09:12:49 -0700

    add desc

--- C++/bus_routes.cpp
+++ C++/bus_routes.cpp
@@ -1,8 +1,11 @@
 /*
- We have a list of bus routes. Each routes[i] is a bus route that the i-th bus repeats forever. For example if routes[0] = [1, 5, 7], this means that the first bus (0-th indexed) travels in the sequence 1->5->7->1->5->7->1->... forever.
- 
- We start at bus stop S (initially not on a bus), and we want to go to bus stop T. Travelling by buses only, what is the least number of buses we must take to reach our destination? Return -1 if it is not possible.
- 
+ 815. Bus Routes
+ We have a list of bus routes. Each routes[i] is a bus route that the i-th bus repeats forever.
+ For example if routes[0] = [1, 5, 7], this means that the first bus (0-th indexed) travels in the sequence 1->5->7->1->5->7->1->... forever.
+
+ We start at bus stop S (initially not on a bus), and we want to go to bus stop T.
+ Travelling by buses only, what is the least number of buses we must take to reach our destination? Return -1 if it is not possible.
+
  Example:
  Input:
  routes = [[1, 2, 7], [3, 6, 7]]
@@ -12,13 +15,11 @@
  Explanation:
  The best strategy is take the first bus to the bus stop 7, then take the second bus to the bus stop 6.
  Note:
- 
+
  1 <= routes.length <= 500.
  1 <= routes[i].length <= 500.
  0 <= routes[i][j] < 10 ^ 6.
-
  */
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -43,7 +44,7 @@ public:
                 busLine[stop].insert(i);
             }
         }
-        
+
         unordered_set<int> visited;
         queue<vector<int>> bfs; // {busStop, distance}
         visited.insert(S);
@@ -52,10 +53,11 @@ public:
             int curStop = bfs.front()[0];
             int distance = bfs.front()[1];
             bfs.pop();
+
             if (curStop == T) {
                 return distance;
             }
-            
+
             for (int line : busLine[curStop]) {
                 for (int next : routes[line]) {
                     if (visited.find(next) == visited.end()) {
@@ -63,7 +65,6 @@ public:
                         bfs.push({next, distance + 1});
                     }
                 }
-                routes[line].clear();
             }
         }
         return -1;
--- C++/kth_smallest_prime_fraction.cpp
+++ C++/kth_smallest_prime_fraction.cpp
@@ -1,8 +1,9 @@
 /*
+ 786. K-th Smallest Prime Fraction
  A sorted list A contains 1, plus some number of primes.  Then, for every p < q in the list, we consider the fraction p/q.
- 
+
  What is the K-th smallest fraction considered?  Return your answer as an array of ints, where answer[0] = p and answer[1] = q.
- 
+
  Examples:
  Input: A = [1, 2, 3, 5], K = 3
  Output: [2, 5]
@@ -10,16 +11,15 @@
  The fractions to be considered in sorted order are:
  1/5, 1/3, 2/5, 1/2, 3/5, 2/3.
  The third fraction is 2/5.
- 
+
  Input: A = [1, 7], K = 1
  Output: [1, 7]
  Note:
- 
+
  A will have length between 2 and 2000.
  Each A[i] will be between 1 and 30000.
  K will be between 1 and A.length * (A.length - 1) / 2.
  */
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -34,28 +34,36 @@
 #include <numeric>
 using namespace std;
 
+class Solution { // binary search
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
+
+    }
+};
+
 /*
  when we put element into the pq, we just need to make sure
  1. we first put all the smallest elments, with A[0] / A[i]
  2. then for each poped item x, we put x.first + 1/ x.second, and we sure x.first / x.second or x.first / x.second + 1 already in or will be in the pq
  */
-class Solution {
+class Solution1 { // TLE
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
         auto cmp = [&A](auto& a, auto& b) {
             return A[a.first] * A[b.second] > A[a.second] * A[b.first];
         };
         priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
-        
+
         for (int i = 1; i < A.size(); i++) {
             pq.push({0, i});
         }
+
         int count = 1;
         while (count < K) {
             count += 1;
             pair<int, int> cur = pq.top();
             pq.pop();
-            
+
             if (cur.first + 1 < cur.second) {
                 pq.push({cur.first + 1, cur.second});
             }
