99d35312ca27cc9e6e88b7d8616f77e95c87ae70
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-09 15:33:16 -0700

    664. Strange Printer

--- C++/strange_printer.cpp
+++ C++/strange_printer.cpp
@@ -1,4 +1,5 @@
 /*
+ 664. Strange Printer
  There is a strange printer with the following two special requirements:
 
  The printer can only print a sequence of the same character each time.
@@ -15,7 +16,6 @@
  Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
  Hint: Length of the given string will not exceed 100.
  */
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -31,14 +31,13 @@
 using namespace std;
 
 /*
-
- The problem wants us to find the number of ways to do something without giving specific steps like how to achieve it. This can be a typical signal that dynamic programming may come to help.
+ The problem wants us to find the number of ways to do something without giving specific steps like how to achieve it.
+ This can be a typical signal that dynamic programming may come to help.
 
  dp[i][j] stands for the minimal turns we need for string from index i to index j.
- So we have
 
+ So we have
  dp[i][i] = 1: we need 1 turn to paint a single character.
- dp[i][i + 1]
  dp[i][i + 1] = 1 if s.chartAt(i) == s.charAt(i + 1)
  dp[i][i + 1] = 2 if s.chartAt(i) != s.charAt(i + 1)
  Then we can iteration len from 2 to possibly n. For each iteration, we iteration start index from 0 to the farthest possible.
@@ -51,6 +50,22 @@ using namespace std;
  This case we can reduce our turns to dp[start][start + k] + dp[start + k + 1][start + len] - 1
  */
 class Solution {
+    /*
+    * I suggest to do this treatment, before go directly DP.
+    * Shorten the original string, like reduce aaabbb to ab.
+    */
+    string polish(string s) {
+        int pos = 0;
+        string result = "";
+        result.push_back(s[0]);
+        for (int i = 1; i < s.size(); ++i) {
+            if (s[i] == result.back()) {
+                continue;
+            }
+            result.push_back(s[i]);
+        }
+        return result;
+    }
 public:
     int strangePrinter(string s) {
         int n = s.size();
@@ -58,6 +73,8 @@ public:
             return n;
         }
 
+        s = polish(s);
+        n = s.size();
         vector<vector<int>> dp(n, vector<int>(n, 0));
         for (int i = 0; i < n; i++) {
             dp[i][i] = 1;
