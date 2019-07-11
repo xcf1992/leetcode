a2143ae0d5d503b1d22ef763be071b623737d939
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-10 17:04:23 -0700

    629. K Inverse Pairs Array

--- C++/guess_number_higher_or_lower_II.cpp
+++ C++/guess_number_higher_or_lower_II.cpp
@@ -1,23 +1,23 @@
 /*
  375. Guess Number Higher or Lower II
  We are playing the Guess Game. The game is as follows:
- 
+
  I pick a number from 1 to n. You have to guess which number I picked.
- 
+
  Every time you guess wrong, I'll tell you whether the number I picked is higher or lower.
- 
+
  However, when you guess a particular number x, and you guess wrong, you pay $x. You win the game when you guess the number I picked.
- 
+
  Example:
- 
+
  n = 10, I pick 8.
- 
+
  First round:  You guess 5, I tell you that it's higher. You pay $5.
  Second round: You guess 7, I tell you that it's higher. You pay $7.
  Third round:  You guess 9, I tell you that it's lower. You pay $9.
- 
+
  Game over. 8 is the number I picked.
- 
+
  You end up paying $5 + $7 + $9 = $21.
  Given a particular n ≥ 1, find out how much money you need to have to guarantee a win.
  */
@@ -40,11 +40,12 @@ using namespace std;
  –> // the max means whenever you choose a number, the feedback is always bad and therefore leads you to a worse branch.
  then we get DP([i~j]) = min{xi, … ,xj}
  –> // this min makes sure that you are minimizing your cost.
- 
+
  It takes me some time to understand the simple question “why it’s using max(dp[i][x-1], dp[x+1][j])”, so I want to share my understanding here to help people like me.
- 
+
  dp[i][j] is the minimal cost to guess from range(i…j).
- When you choose an x where i <= x <= j, you may find the target number from left i…x-1, or you may find the target number from the x+1…j, because you don’t know which way should go, so to guarantee you have enough money to find the target, you need to prepare the more, which is max(dp[i][x-1], dp[x+1][j]).
+ When you choose an x where i <= x <= j, you may find the target number from left i…x-1, or you may find the target number from the x+1…j,
+ because you don’t know which way should go, so to guarantee you have enough money to find the target, you need to prepare the more, which is max(dp[i][x-1], dp[x+1][j]).
  */
 class Solution {
 private:
@@ -52,11 +53,11 @@ private:
         if (start >= end) {
             return 0;
         }
-        
+
         if (dp[start][end] != 0) {
             return dp[start][end];
         }
-        
+
         int result = INT_MAX;
         for (int guess = start; guess <= end; guess++) {
             result = min(result, guess + max(calculate(start, guess - 1, dp), calculate(guess + 1, end, dp)));
--- C++/k_inverse_pairs_array.cpp
+++ C++/k_inverse_pairs_array.cpp
@@ -27,7 +27,6 @@
 
  The integer n is in the range [1, 1000] and k is in the range [0, 1000].
  */
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -51,35 +50,38 @@ using namespace std;
  ...
  if we put n as the first number then there's n-1 inverse pairs involve n so the rest k-(n-1) comes from the first n-1 numbers
 
- dp[n][k] = dp[n-1][k]+dp[n-1][k-1]+dp[n-1][k-2]+...+dp[n-1][k+1-n+1]+dp[n-1][k-n+1]
+ dp[n][k] = dp[n-1][k] + dp[n-1][k-1] + dp[n-1][k-2] + ... + dp[n-1][k+1-n+1] + dp[n-1][k-n+1]
 
- It's possible that some where in the right hand side the second array index become negative, since we cannot generate negative inverse pairs we just treat them as 0, but still leave the item there as a place holder.
+ It's possible that some where in the right hand side the second array index become negative,
+ since we cannot generate negative inverse pairs we just treat them as 0, but still leave the item there as a place holder.
 
- dp[n][k] = dp[n-1][k]+dp[n-1][k-1]+dp[n-1][k-2]+...+dp[n-1][k+1-n+1]+dp[n-1][k-n+1]
- dp[n][k+1] = dp[n-1][k+1]+dp[n-1][k]+dp[n-1][k-1]+dp[n-1][k-2]+...+dp[n-1][k+1-n+1]
+ dp[n][k] = dp[n-1][k] + dp[n-1][k-1] + dp[n-1][k-2] + ... + dp[n-1][k+1-n+1] + dp[n-1][k-n+1]
+ dp[n][k+1] = dp[n-1][k+1] + dp[n-1][k] + dp[n-1][k-1] + dp[n-1][k-2] + ... + dp[n-1][k+1-n+1]
 
  so by deducting the first line from the second line, we have
 
- dp[n][k+1] = dp[n][k]+dp[n-1][k+1]-dp[n-1][k+1-n]
+ dp[n][k+1] = dp[n][k] + dp[n-1][k+1] - dp[n-1][k+1-n]
  */
 class Solution {
+private:
+    int MOD = 1e9 + 7;
 public:
     int kInversePairs(int n, int k) {
-        int MOD = 1e9 + 7;
         if (k > n * (n - 1) / 2 or k < 0) {
             return 0;
         }
-        if (k == 0 || k == n * (n - 1) / 2) {
+
+        if (k == 0 or k == n * (n - 1) / 2) {
             return 1;
         }
 
         vector<vector<long>> dp(n + 1, vector<long>(k + 1, 0));
         dp[2][0] = 1;
         dp[2][1] = 1;
-        for (int i = 3; i <= n; i++) {
+        for (int i = 3; i <= n; ++i) {
             dp[i][0] = 1;
-            for (int j = 1; j <= min(k, i * (i - 1) / 2); j++) {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            for (int j = 1; j <= min(k, i * (i - 1) / 2); ++j) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
                 if (j >= i) {
                     dp[i][j] -= dp[i - 1][j - i];
                 }
