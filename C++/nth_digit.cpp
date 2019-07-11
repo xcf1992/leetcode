bc09010028788bfa2057984952850f1dd6baee27
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-11 14:25:51 -0700

    400. Nth Digit

--- C++/nth_digit.cpp
+++ C++/nth_digit.cpp
@@ -1,3 +1,28 @@
+/*
+400. Nth Digit
+Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
+
+Note:
+n is positive and will fit within the range of a 32-bit signed integer (n < 231).
+
+Example 1:
+
+Input:
+3
+
+Output:
+3
+Example 2:
+
+Input:
+11
+
+Output:
+0
+
+Explanation:
+The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,33 +35,21 @@
 #include <stdio.h>
 using namespace std;
 
-
 class Solution {
 public:
-    int findNthDigit(int n) {int findNthDigit(int n) {
-        if (n < 10) {
-            return n;
-        }
-        int i = 0;
-
-        while ( n > 0) {
-            i += 1;
-            int length = i * (pow(10, i) - pow(10, i - 1));
-            if ( n - length > 0) {
-                n -= length;
-            }
-            else {
-                break;
-            }
+    int findNthDigit(int n) {
+        int len = 1;
+        int start = 1;
+        long count = 9;
+        while (n > len * count) {
+            n -= len * count;
+            len += 1;
+            count *= 10;
+            start *= 10;
         }
 
-        int res = pow(10, i - 1) + (n - 1) / i;
-        string result = to_string(res);
-        return result[(n - 1) % i] - '0';
+        start += (n - 1) / len;
+        string result = to_string(start);
+        return result[(n - 1) % len] - '0';
     }
 };
-
-int main() {
-    Solution s;
-    return 0;
-}
