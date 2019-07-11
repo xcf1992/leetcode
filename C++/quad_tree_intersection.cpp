192f5ab191879e4753108c5d4291e7310efcb811
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-11 13:11:35 -0700

    572. Subtree of Another Tree

--- C++/quad_tree_intersection.cpp
+++ C++/quad_tree_intersection.cpp
@@ -1,10 +1,11 @@
 /*
+ 558. Quad Tree Intersection
  A quadtree is a tree data in which each internal node has exactly four children: topLeft, topRight, bottomLeft and bottomRight. Quad trees are often used to partition a two-dimensional space by recursively subdividing it into four quadrants or regions.
- 
+
  We want to store True/False information in our quad tree. The quad tree is used to represent a N * N boolean grid. For each node, it will be subdivided into four children nodes until the values in the region it represents are all the same. Each node has another two boolean attributes : isLeaf and val. isLeaf is true if and only if the node is a leaf node. The val attribute for a leaf node contains the value of the region it represents.
- 
+
  For example, below are two quad trees A and B:
- 
+
  A:
  +-------+-------+   T: true
  |       |       |   F: false
@@ -19,7 +20,7 @@
  topRight: T
  bottomLeft: F
  bottomRight: F
- 
+
  B:
  +-------+---+---+
  |       | F | F |
@@ -38,10 +39,10 @@
  bottomRight: T
  bottomLeft: T
  bottomRight: F
- 
- 
+
+
  Your task is to implement a function that will take two quadtrees and return a quadtree that represents the logical OR (or union) of the two trees.
- 
+
  A:                 B:                 C (A or B):
  +-------+-------+  +-------+---+---+  +-------+-------+
  |       |       |  |       | F | F |  |       |       |
@@ -53,12 +54,11 @@
  |       |       |  |       |       |  |       |       |
  +-------+-------+  +-------+-------+  +-------+-------+
  Note:
- 
+
  N is guaranteened to be a power of 2.
  If you want to know more about the quad tree, you can refer to its wiki.
  The logic OR operation is define as this: "A or B" is true if A is true, or if B is true, or if both A and B are true.
  */
-
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -75,24 +75,24 @@
 using namespace std;
 
  class Node {
- public:
- bool val;
- bool isLeaf;
- Node* topLeft;
- Node* topRight;
- Node* bottomLeft;
- Node* bottomRight;
- 
- Node() {}
- 
- Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
- val = _val;
- isLeaf = _isLeaf;
- topLeft = _topLeft;
- topRight = _topRight;
- bottomLeft = _bottomLeft;
- bottomRight = _bottomRight;
- }
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+
+    Node() {}
+
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
  };
 
 class Solution {
@@ -102,12 +102,12 @@ private:
         Node* topRight = node -> topRight;
         Node* botLeft = node -> bottomLeft;
         Node* botRight = node -> bottomRight;
-        
+
         if (topLeft == nullptr && topRight == nullptr
             && botLeft == nullptr && botRight == nullptr) {
             return true;
         }
-        
+
         if (topLeft -> isLeaf && topRight -> isLeaf && botRight -> isLeaf && botLeft -> isLeaf && (
             (topLeft -> val && topRight -> val && botLeft -> val && botRight -> val) ||
              !(topLeft -> val || topRight -> val || botLeft -> val || botRight -> val))) {
@@ -117,13 +117,10 @@ private:
     }
 public:
     Node* intersect(Node* quadTree1, Node* quadTree2) {
-        if (quadTree1 == nullptr && quadTree2 == nullptr) {
-            return nullptr;
-        }
-        if (quadTree2 == nullptr || quadTree1 == nullptr) {
+        if (quadTree1 == nullptr or quadTree1 == nullptr) {
             return quadTree1 == nullptr ? quadTree2 : quadTree1;
         }
-        
+
         if (quadTree1 -> isLeaf) {
             return quadTree1 -> val ? quadTree1 : quadTree2;
         }
@@ -136,12 +133,11 @@ public:
         quadTree1 -> topRight = intersect(quadTree1 -> topRight, quadTree2 -> topRight);
         quadTree1 -> bottomLeft = intersect(quadTree1 -> bottomLeft, quadTree2 -> bottomLeft);
         quadTree1 -> bottomRight = intersect(quadTree1 -> bottomRight, quadTree2 -> bottomRight);
-        
+
         if (isLeaf(quadTree1)) {
             quadTree1 -> isLeaf = true;
             quadTree1 -> val = quadTree1 -> topLeft -> val;
         }
-        
         return quadTree1;
     }
 };
--- C++/subtree_of_another_tree.cpp
+++ C++/subtree_of_another_tree.cpp
@@ -1,3 +1,39 @@
+/*
+572. Subtree of Another Tree
+Given two non-empty binary trees s and t,
+check whether tree t has exactly the same structure and node values with a subtree of s.
+A subtree of s is a tree consists of a node in s and all of this node's descendants.
+The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+    4
+  /  \
+ 1    2
+Return true, because t has the same structure and node values with a subtree of s.
+Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+Return false.
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,29 +46,26 @@
 #include <stdio.h>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 private:
     bool sameTree(TreeNode* s, TreeNode* t) {
-        if (s == nullptr && t == nullptr) {
+        if (s == nullptr and t == nullptr) {
             return true;
         }
-        if (s == nullptr || t == nullptr) {
+        if (s == nullptr or t == nullptr) {
             return false;
         }
 
         if (s -> val != t -> val) {
             return false;
         }
-
         return sameTree(s -> left, t -> left) && sameTree(s -> right, t -> right);
     }
 public:
@@ -40,11 +73,9 @@ public:
         if (t == nullptr) {
             return true;
         }
-
         if (s == nullptr) {
             return false;
         }
-
-        return sameTree(s, t) || isSubtree(s -> left, t) || isSubtree(s -> right, t);
+        return sameTree(s, t) or isSubtree(s -> left, t) or isSubtree(s -> right, t);
     }
 };
