6364f0fb0686acfe1f354974b09bfd220e93190d
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-12 10:05:42 -0700

    974. Subarray Sums Divisible by K

--- C++/subarray_sum_equals_K.cpp
+++ C++/subarray_sum_equals_K.cpp
@@ -1,5 +1,7 @@
 /*
-Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+560. Subarray Sum Equals K
+Given an array of integers and an integer k,
+you need to find the total number of continuous subarrays whose sum equals to k.
 
 Example 1:
 Input:nums = [1,1,1], k = 2
--- C++/subarray_sums_divisible_by_k.cpp
+++ C++/subarray_sums_divisible_by_k.cpp
@@ -1,23 +1,22 @@
 /*
- Given an array A of integers, return the number of (contiguous, non-empty) subarrays that have a sum divisible by K.
- 
- 
- 
+ 974. Subarray Sums Divisible by K
+ Given an array A of integers,
+ return the number of (contiguous, non-empty) subarrays that have a sum divisible by K.
+
  Example 1:
- 
+
  Input: A = [4,5,0,-2,-3,1], K = 5
  Output: 7
  Explanation: There are 7 subarrays with a sum divisible by K = 5:
  [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
- 
- 
+
+
  Note:
- 
+
  1 <= A.length <= 30000
  -10000 <= A[i] <= 10000
  2 <= K <= 10000
  */
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -54,16 +53,12 @@ public:
         int n = A.size();
         vector<int> mod(K, 0);
         int curSum = 0;
+        int result = 0;
         for (int i = 0; i < n; i++) {
             curSum += A[i];
-            mod[((curSum % K) + K) % K] += 1;
-        }
-        
-        int result = 0;
-        for (int i = 0; i < K; i++) {
-            if (mod[i] > 1) {
-                result += (mod[i] * (mod[i] - 1)) / 2;
-            }
+            int left = ((curSum % K) + K) % K;
+            result += mod[left];
+            mod[left] += 1;
         }
         return result + mod[0];
     }
