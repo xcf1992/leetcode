558b1c38acce090108e34471602ea41e48472ef4
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-13 10:03:14 -0700

    2. Add Two Numbers

--- C++/add_two_numbers.cpp
+++ C++/add_two_numbers.cpp
@@ -1,5 +1,8 @@
 /*
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+2. Add Two Numbers
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -25,56 +28,36 @@ struct ListNode {
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        if (l1 == nullptr) {
-            return l2;
+        if (l1 == nullptr or l2 == nullptr) {
+            return l1 == nullptr ? l2 : l1;
         }
-        if (l2 == nullptr) {
-            return l1;
-        }
-        
-        int flag = 0;
-        ListNode *cur1 = l1;
-        ListNode *cur2 = l2;
-        ListNode *head = new ListNode(-1);
-        head -> next = l1;
-        ListNode *cur = head;
-        while (cur1 != nullptr && cur2 != nullptr) {
-            int a = cur1 -> val;
-            int b = cur2 -> val;
 
-            int sum = a + b + flag;
-            int digit = sum % 10;
-            flag = sum / 10;
-            
-            cur -> next -> val = digit;
-            cur = cur -> next;
-            
-            cur1 = cur1 -> next;
-            cur2 = cur2 -> next;
+        int carry = 0;
+        ListNode* cur1 = l1;
+        ListNode* cur2 = l2;
+        ListNode* head = nullptr;
+        ListNode* cur = head;
+        while (cur1 != nullptr or cur2 != nullptr or carry != 0) {
+            int sum = carry;
+            if (cur1 != nullptr) {
+                sum += cur1 -> val;
+                cur1 = cur1 -> next;
+            }
+            if (cur2 != nullptr) {
+                sum += cur2 -> val;
+                cur2 = cur2 -> next;
+            }
+
+            ListNode* node = new ListNode(sum % 10);
+            if (head == nullptr) {
+                head = node;
+            }
+            else {
+                cur -> next = node;
+            }
+            cur = node;
+            carry = sum / 10;
         }
-        
-        while (cur1 != nullptr) {
-            int a = cur1 -> val;
-            int sum = a + flag;
-            cur1 -> val = sum % 10;
-            flag = sum / 10;
-            cur -> next = cur1;
-            cur = cur -> next;
-            cur1 = cur1 -> next;
-        }
-        while (cur2 != nullptr) {
-            int b = cur2 -> val;
-            int sum = b + flag;
-            cur2 -> val = sum % 10;
-            flag = sum / 10;
-            cur -> next = cur2;
-            cur = cur -> next;
-            cur2 = cur2 -> next;
-        }
-        if (flag == 1) {
-            cur -> next = new ListNode(flag);
-        }
-        
-        return head->next;
+        return head;
     }
 };
