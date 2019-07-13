24a4af7e393b587df0fe4253b928deb25da53eb0
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-12 21:00:38 -0700

    48. Rotate Image

--- C++/basic_calculator_II.cpp
+++ C++/basic_calculator_II.cpp
@@ -1,26 +1,26 @@
 /*
+ 227. Basic Calculator II
  Implement a basic calculator to evaluate a simple expression string.
- 
+
  The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . The integer division should truncate toward zero.
- 
+
  Example 1:
- 
+
  Input: "3+2*2"
  Output: 7
  Example 2:
- 
+
  Input: " 3/2 "
  Output: 1
  Example 3:
- 
+
  Input: " 3+5 / 2 "
  Output: 5
  Note:
- 
+
  You may assume that the given expression is always valid.
  Do not use the eval built-in library function.
  */
-
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -39,10 +39,10 @@ using namespace std;
 class Solution {
 private:
     bool IsOpt(char ch) {
-        return  ch == '+' ||ch == '-' ||ch == '*' ||ch == '/';
+        return  ch == '+' or ch == '-' or ch == '*' or ch == '/';
     }
-    
-    int Calc(int num1, int num2, char opt) {
+
+    long Calc(long num1, long num2, char opt) {
         if (opt == '+') {
             return num1 + num2;
         }
@@ -54,24 +54,24 @@ private:
         }
         return num1 / num2;
     }
-    
-    void CalcTop(stack<int> &nums, stack<char> &opts) {
-        int num2 = nums.top();
+
+    void CalcTop(stack<long> &nums, stack<char> &opts) {
+        long num2 = nums.top();
         nums.pop();
-        
-        int num1 = nums.top();
+
+        long num1 = nums.top();
         nums.pop();
-        
+
         char opt = opts.top();
         opts.pop();
-        
+
         nums.push(Calc(num1, num2, opt));
     }
 public:
     int calculate(string s) {
-        stack<int> nums;
+        stack<long> nums;
         stack<char> opts;
-        int num = 0;
+        long num = 0;
         for (int i = 0; i < s.size(); i++) {
             if (isdigit(s[i])) {
                 num = num * 10 + s[i] - '0';
@@ -85,14 +85,14 @@ public:
                     }
                 }
                 else { // s[i] == '*' || s[i] == '/'
-                    while (!opts.empty() && (opts.top() == '*'|| opts.top() == '/')) {
+                    while (!opts.empty() and (opts.top() == '*' or opts.top() == '/')) {
                         CalcTop(nums, opts);
                     }
                 }
                 opts.push(s[i]);
             }
-            
         }
+
         nums.push(num);
         while (!opts.empty()) {
             CalcTop(nums, opts);
@@ -100,3 +100,39 @@ public:
         return nums.top();
     }
 };
+/*
+public int calculate(String s) {
+    int len;
+    if(s==null || (len = s.length())==0) return 0;
+    Stack<Integer> stack = new Stack<Integer>();
+    int num = 0;
+    char sign = '+';
+    for(int i=0;i<len;i++){
+        if(Character.isDigit(s.charAt(i))){
+            num = num*10+s.charAt(i)-'0';
+        }
+        if((!Character.isDigit(s.charAt(i)) &&' '!=s.charAt(i)) || i==len-1){
+            if(sign=='-'){
+                stack.push(-num);
+            }
+            if(sign=='+'){
+                stack.push(num);
+            }
+            if(sign=='*'){
+                stack.push(stack.pop()*num);
+            }
+            if(sign=='/'){
+                stack.push(stack.pop()/num);
+            }
+            sign = s.charAt(i);
+            num = 0;
+        }
+    }
+
+    int re = 0;
+    for(int i:stack){
+        re += i;
+    }
+    return re;
+}
+*/
