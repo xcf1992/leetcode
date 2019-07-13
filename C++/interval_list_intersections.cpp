19aacce512763c49972768e5aebd90502a877fbb
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-12 20:21:21 -0700

    34. Find First and Last Position of Element in Sorted Array

--- C++/interval_list_intersections.cpp
+++ C++/interval_list_intersections.cpp
@@ -1,30 +1,27 @@
 /*
- 986. Interval List Intersections
+ 986. vector<int> List Intersections
 
  Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
 
  Return the intersection of these two interval lists.
 
- (Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
-
-
+ (Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+ The intersection of two closed intervals is a set of real numbers that is either empty,
+ or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
 
  Example 1:
 
-
-
  Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
  Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
- Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
+ Reminder: The inputs and the desired output are lists of vector<int> objects, and not arrays or lists.
 
 
  Note:
 
  0 <= A.length < 1000
  0 <= B.length < 1000
- 0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+ 0 <= A[i][0], A[i][1], B[i][0], B[i][1] < 10^9
  */
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -39,26 +36,20 @@
 #include <numeric>
 using namespace std;
 
-struct Interval {
-      int start;
-      int end;
-      Interval() : start(0), end(0) {}
-      Interval(int s, int e) : start(s), end(e) {}
-  };
-
 class Solution {
 public:
-    vector<Interval> intervalIntersection(vector<Interval>& A, vector<Interval>& B) {
-        vector<Interval> result;
-        int m = A.size(), a = 0;
-        int n = B.size(), b = 0;
-        while (a < m and b < n) {
-            int left = max(A[a].start, B[b].start);
-            int right = min(A[a].end, B[b].end);
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int m = A.size();
+        int n = B.size();
+
+        vector<vector<int>> result;
+        for (int a = 0, b = 0; a < m and b < n;) {
+            int left = max(A[a][0], B[b][0]);
+            int right = min(A[a][1], B[b][1]);
             if (left <= right) {
-                result.push_back(Interval(left, right));
+                result.push_back({left, right});
             }
-            if (A[a].end < B[b].end) {
+            if (A[a][1] < B[b][1]) {
                 a += 1;
             }
             else {
