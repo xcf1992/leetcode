ebf6caf396bd37cbf98ed1e262cac9864e5f8560
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-15 13:24:49 -0700

    311. Sparse Matrix Multiplication

--- C++/encode_and_decode_strings.cpp
+++ C++/encode_and_decode_strings.cpp
@@ -1,8 +1,9 @@
 /*
- Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
- 
+ Design an algorithm to encode a list of strings to a string.
+ The encoded string is then sent over the network and is decoded back to the original list of strings.
+
  Machine 1 (sender) has the function:
- 
+
  string encode(vector<string> strs) {
  // ... your code
  return encoded_string;
@@ -13,21 +14,21 @@
  return strs;
  }
  So Machine 1 does:
- 
+
  string encoded_string = encode(strs);
  and Machine 2 does:
- 
+
  vector<string> strs2 = decode(encoded_string);
  strs2 in Machine 2 should be the same as strs in Machine 1.
- 
+
  Implement the encode and decode methods.
- 
+
  Note:
- The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough to work on any possible characters.
+ The string may contain any possible characters out of 256 valid ascii characters.
+ Your algorithm should be generalized enough to work on any possible characters.
  Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
  Do not rely on any library method such as eval or serialize methods. You should implement your own encode/decode algorithm.
  */
-
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -43,15 +44,15 @@
 #include <set>
 #include <numeric>
 using namespace std;
+
 class Codec {
 public:
-
     // Encodes a list of strings to a single string.
     string encode(vector<string>& strs) {
         if (strs.empty()) {
             return "";
         }
-        
+
         string result = "";
         for (string s : strs) {
             for (char c : s) {
@@ -75,7 +76,7 @@ public:
         if (s == "") {
             return {};
         }
-        
+
         vector<string> result;
         string cur = "";
         int index = 0;
@@ -86,7 +87,7 @@ public:
                 index += 1;
                 continue;
             }
-            
+
             if (s[index] == '$') {
                 index += 1;
             }
@@ -106,7 +107,7 @@ public:
 // then we cannot test if there are two string or one start letter
 class Codec1 {
 public:
-    
+
     // Encodes a list of strings to a single string.
     string encode(vector<string>& strs) {
         string result = "$";
@@ -127,7 +128,7 @@ public:
         }
         return result;
     }
-    
+
     // Decodes a single string to a list of strings.
     vector<string> decode(string s) {
         vector<string> result;
--- C++/next_closest_time.cpp
+++ C++/next_closest_time.cpp
@@ -1,3 +1,23 @@
+/*
+681. Next Closest Time
+Given a time represented in the format "HH:MM", form the next closest time by reusing the current digits.
+There is no limit on how many times a digit can be reused.
+
+You may assume the given input string is always valid. For example, "01:34", "12:09" are all valid. "1:34", "12:9" are all invalid.
+
+Example 1:
+
+Input: "19:34"
+Output: "19:39"
+Explanation: The next closest time choosing from digits 1, 9, 3, 4, is 19:39, which occurs 5 minutes later.
+It is not 19:33, because this occurs 23 hours and 59 minutes later.
+Example 2:
+
+Input: "23:59"
+Output: "22:22"
+Explanation: The next closest time choosing from digits 2, 3, 5, 9, is 22:22.
+It may be assumed that the returned time is next day's time since it is smaller than the input time numerically.
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -27,7 +47,6 @@ public:
                 return time;
             }
         }
-
         time[4] = digits[0];
 
         for (int i = 0; i < 4; i++) {
@@ -39,7 +58,6 @@ public:
                 return time;
             }
         }
-
         time[3] = digits[0];
 
         for (int i = 0; i < 4; i++) {
@@ -51,7 +69,6 @@ public:
                 return time;
             }
         }
-
         time[1] = digits[0];
 
         for (int i = 0; i < 4; i++) {
@@ -63,7 +80,6 @@ public:
                 return time;
             }
         }
-
         time[0] = digits[0];
         return time;
     }
