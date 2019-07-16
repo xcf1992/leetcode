a91d7b9f4dfdc940517ed0c7baa6d667f9ee1401
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-15 21:20:40 -0700

    1122. Relative Sort Array

---
+++ C++/relative_sort_array.cpp
@@ -0,0 +1,58 @@
+/*
+1122. Relative Sort Array
+Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.
+Elements that don't appear in arr2 should be placed at the end of arr1 in ascending order.
+
+Example 1:
+
+Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+Output: [2,2,2,1,4,3,3,9,6,7,19]
+
+
+Constraints:
+
+arr1.length, arr2.length <= 1000
+0 <= arr1[i], arr2[i] <= 1000
+Each arr2[i] is distinct.
+Each arr2[i] is in arr1.
+ */
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        map<int, int> count;
+        for (int num : arr1) {
+            count[num] += 1;
+        }
+
+        vector<int> result;
+        for (int num : arr2) {
+            for (int i = 0; i < count[num]; ++i) {
+                result.push_back(num);
+            }
+            count[num] = 0;
+        }
+        for (auto& it : count) {
+            for (int i = 0; i < it.second; ++i) {
+                result.push_back(it.first);
+            }
+        }
+        return result;
+    }
+};
