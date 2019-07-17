16dea2ac5637395e057e5dedc077574e156a5cea
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-16 20:14:46 -0700

    add desc

--- C++/house_robber_III.cpp
+++ C++/house_robber_III.cpp
@@ -1,7 +1,11 @@
 /*
 337. House Robber III
 
-The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+The thief has found himself a new place for his thievery again.
+There is only one entrance to this area, called the "root."
+Besides the root, each house has one and only one parent house.
+After a tour, the smart thief realized that "all houses in this place forms a binary tree".
+It will automatically contact the police if two directly-linked houses were broken into on the same night.
 
 Determine the maximum amount of money the thief can rob tonight without alerting the police.
 
@@ -69,7 +73,6 @@ public:
         if (root -> right != nullptr) {
             maxMoney += rob(root -> right -> left) + rob(root -> right -> right);
         }
-
         cache[root] = max(maxMoney, rob(root -> left) + rob(root -> right));
         return cache[root];
     }
