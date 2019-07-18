962f26ff3747c363008b5bc6f1124c56a20f2da1
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-17 20:51:11 -0700

    1120. Maximum Average Subtree

--- C++/maximum_averge_subtree.cpp
+++ C++/maximum_averge_subtree.cpp
@@ -1,3 +1,30 @@
+/*
+1120. Maximum Average Subtree
+Given the root of a binary tree, find the maximum average value of any subtree of that tree.
+
+(A subtree of a tree is any node of that tree plus all its descendants.
+The average value of a tree is the sum of its values,
+divided by the number of nodes.)
+
+Example 1:
+        5
+    /       \
+    6       1
+Input: [5,6,1]
+Output: 6.00000
+Explanation:
+For the node with value = 5 we have and average of (5 + 6 + 1) / 3 = 4.
+For the node with value = 6 we have and average of 6 / 1 = 6.
+For the node with value = 1 we have and average of 1 / 1 = 1.
+So the answer is 6 which is the maximum.
+
+
+Note:
+
+The number of nodes in the tree is between 1 and 5000.
+Each node will have a value between 0 and 100000.
+Answers will be accepted as correct if they are within 10^-5 of the correct answer.
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,9 +35,6 @@
 #include <queue>
 #include <stack>
 #include <stdio.h>
-#include <map>
-#include <set>
-#include <numeric>
 #include "extra_data_types.hpp"
 using namespace std;
 
@@ -20,14 +44,14 @@ private:
         if (root == nullptr) {
             return {0.0, 0.0};
         }
-        
+
         if (root -> left == nullptr and root -> right == nullptr) {
             result = max(result, (double)(root -> val));
             return {root -> val, 1.0};
         }
-        
+
         pair<double, double> lft = traverse(root -> left, result);
-        pair<double, double> rgt = traverse(root -> left, result);
+        pair<double, double> rgt = traverse(root -> right, result);
         double sum = root -> val + lft.first + rgt.first;
         double count = 1 + lft.second + rgt.second;
         double avg = sum / count;
@@ -41,31 +65,3 @@ public:
         return result;
     }
 };
-
-int main() {
-    Solution s;
-    vector<int> temp({1,3,2,4});
-    vector<int> temp1({1,3,3,3,2});
-    vector<vector<int>> matrix({
-        {0,1},
-        {1,2},
-        {2,1},
-        {1,0},
-        {0,2},
-        {0,0},
-        {1,1}
-    });
-    vector<vector<int>> matrix2({
-        {1,2,2,3,5},
-        {3,2,3,4,4},
-        {2,4,5,3,1},
-        {6,7,1,4,5},
-        {5,1,1,2,4}
-    });
-    vector<string> words({"cat","cats","and","sand","dog"});
-    TreeNode* r1 = new TreeNode(0);
-    TreeNode* r2 = new TreeNode(1);
-    TreeNode* r3 = new TreeNode(3);
-    r1 -> left = r2;
-    s.maximumAverageSubtree(r1);
-}
