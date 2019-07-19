97dd14c4884a5d42ceccf389320d1e8afb6f18bd
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-19 16:10:37 -0700

    1125. Smallest Sufficient Team

--- C++/number_of_subarrays_with_bounded_maximum.cpp
+++ C++/number_of_subarrays_with_bounded_maximum.cpp
@@ -38,6 +38,27 @@ public:
         int dp = 0;
         int start = -1;
         for (int i = 0; i < A.size(); i++) {
+            if (A[i] > R) {
+                dp = 0;
+                start = i;
+                continue;
+            }
+            if (A[i] >= L) {
+                dp = i - start;
+            }
+            result += dp;
+        }
+        return result;
+    }
+};
+
+class Solution1 {
+public:
+    int numSubarrayBoundedMax(vector<int>& A, int L, int R) {
+        int result = 0;
+        int dp = 0;
+        int start = -1;
+        for (int i = 0; i < A.size(); i++) {
             if (A[i] < L) {
                 result += dp;
             }
