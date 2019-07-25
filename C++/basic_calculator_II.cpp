1d84ec8b2f12d1732d14948f1713fa55841130d5
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-24 18:08:16 -0700

    basic calculator

--- C++/basic_calculator_II.cpp
+++ C++/basic_calculator_II.cpp
@@ -2,7 +2,8 @@
  227. Basic Calculator II
  Implement a basic calculator to evaluate a simple expression string.
 
- The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . The integer division should truncate toward zero.
+ The expression string contains only non-negative integers, +, -, *, / operators and empty spaces.
+ The integer division should truncate toward zero.
 
  Example 1:
 
