84e0a140d0be0e71495aae6e9d4a031e162fed63
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-26 16:13:35 -0700

    352 - unfinished yet

--- C++/data_stream_as_disjoint_intervals.cpp
+++ C++/data_stream_as_disjoint_intervals.cpp
@@ -1,6 +1,7 @@
 /*
 352. Data Stream as Disjoint Intervals
-Given a data stream input of non-negative integers a1, a2, ..., an, ..., summarize the numbers seen so far as a list of disjoint intervals.
+Given a data stream input of non-negative integers a1, a2, ..., an, ...,
+summarize the numbers seen so far as a list of disjoint intervals.
 
 For example, suppose the integers from the data stream are 1, 3, 7, 2, 6, ..., then the summary will be:
 
@@ -12,7 +13,8 @@ For example, suppose the integers from the data stream are 1, 3, 7, 2, 6, ..., t
 Follow up:
 What if there are lots of merges and the number of disjoint intervals are small compared to the data stream's size?
 
-NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+NOTE: input types have been changed on April 15, 2019.
+Please reset to default code definition to get new method signature.
 */
 #include <iostream>
 #include <string>
@@ -26,27 +28,16 @@ NOTE: input types have been changed on April 15, 2019. Please reset to default c
 #include <stdio.h>
 using namespace std;
 
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class SummaryRanges {
 private:
     vector<vector<int>> intervals;
 public:
     /** Initialize your data structure here. */
-    SummaryRanges() {
-
-    }
+    SummaryRanges() {}
 
     void addNum(int val) {
-        auto it = lower_bound(intervals.begin(), intervals.end(), {val, val}, [](vector<int>& a, vector<int>& b) {
-            return a[0] > b[0];
+        auto it = lower_bound(intervals.begin(), intervals.end(), val, [](vector<int>& a, int v) {
+            return a[0] < v;
         });
 
         if (it == intervals.end()) {
@@ -56,16 +47,16 @@ public:
             }
             intervals.insert(intervals.begin(), {val, val});
         }
-        else if (val - 1 <= it -> end) {
-            it -> end = max(it -> end, val);
+        else if (val - 1 <= (*it)[1]) {
+            (*it)[1] = max((*it)[1], val);
         }
         else {
             it += 1;
             intervals.insert(it, {val, val});
         }
 
-        if (it + 1 != intervals.end() && it -> end + 1 >= (it + 1) -> start) {
-            (it + 1) -> start = it -> start;
+        if (it + 1 != intervals.end() && (*it)[1] + 1 >= (*next(it))[0]) {
+            (*next(it))[0] = (*it)[0];
             intervals.erase(it);
         }
     }
