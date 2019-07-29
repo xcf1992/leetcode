4f3326148ae394ed09149a1d1b2d1c63e2d63ee2
Chenfu Xie <chenfu.xie@hulu.com>
2019-07-29 11:00:58 -0700

    964. Least Operators to Express Number

--- C++/least_operators_to_express_number.cpp
+++ C++/least_operators_to_express_number.cpp
@@ -1,16 +1,21 @@
 /*
 964. Least Operators to Express Number
-Given a single positive integer x, we will write an expression of the form x (op1) x (op2) x (op3) x ... where each operator op1, op2, etc. is either addition, subtraction, multiplication, or division (+, -, *, or /).  For example, with x = 3, we might write 3 * 3 / 3 + 3 - 3 which is a value of 3.
+Given a single positive integer x,
+we will write an expression of the form x (op1) x (op2) x (op3) x ...
+where each operator op1, op2, etc. is either addition, subtraction, multiplication, or division (+, -, *, or /).
+For example, with x = 3, we might write 3 * 3 / 3 + 3 - 3 which is a value of 3.
 
 When writing such an expression, we adhere to the following conventions:
 
 The division operator (/) returns rational numbers.
 There are no parentheses placed anywhere.
 We use the usual order of operations: multiplication and division happens before addition and subtraction.
-It's not allowed to use the unary negation operator (-).  For example, "x - x" is a valid expression as it only uses subtraction, but "-x + x" is not because it uses negation.
-We would like to write an expression with the least number of operators such that the expression equals the given target.  Return the least number of expressions used.
+It's not allowed to use the unary negation operator (-).
+For example, "x - x" is a valid expression as it only uses subtraction,
+but "-x + x" is not because it uses negation.
 
-
+We would like to write an expression with the least number of operators such that the expression equals the given target.
+Return the least number of expressions used.
 
 Example 1:
 
@@ -51,103 +56,48 @@ using namespace std;
 /*
  * Intuition
 
-First, we notice that we can consider blocks of multiplication and division separately. Each block is a power of x: either x / x, x, x * x, x * x * x, x * x * x * x and so on. (There is no point to write expressions like x * x / x because it uses strictly more operators.)
+First, we notice that we can consider blocks of multiplication and division separately.
+Each block is a power of x: either x / x, x, x * x, x * x * x, x * x * x * x and so on.
+(There is no point to write expressions like x * x / x because it uses strictly more operators.)
 
-Let's think of the cost of a block as all the operators needed to express it, including the addition or subtraction operator in front of it. For example, we can think of x * x + x + x / x as (+ x * x) (+ x) (+ x / x) for a cost of 2 + 1 + 2, minus 1 the leading + (so the total cost is 4).
+Let's think of the cost of a block as all the operators needed to express it,
+including the addition or subtraction operator in front of it.
+For example, we can think of x * x + x + x / x as (+ x * x) (+ x) (+ x / x)
+for a cost of 2 + 1 + 2, minus 1 the leading + (so the total cost is 4).
 
-We can write the cost of writing a block that has value x^ex
-e
- : it is ee, except when e = 0e=0 it is 2. We want the sum of the costs of all blocks minus 1.
+We can write the cost of writing a block that has value x^e it is e, except when e = 0 it is 2.
+We want the sum of the costs of all blocks minus 1.
 
-Second, we have the reduced problem: we have the costs of writing all x^ex
-e
-  or -x^e−x
-e
- , and we want to find the least cost to express the target.
+Second, we have the reduced problem: we have the costs of writing all x^e or -x^e,
+and we want to find the least cost to express the target.
 
-Notice that modulo xx, the only blocks that change the expression are x^0x
-0
- . Let r_1 = \text{target} \pmod xr
-1
-​
- =target(modx). So we must either subtract r_1r
-1
-​
-  x^0x
-0
- 's, or add x-r_1x−r
-1
-​
-  x^0x
-0
- 's. This will form a new "remaining" target, \text{target}_2target
-2
-​
- , that is divisible by xx.
+Notice that modulo x, the only blocks that change the expression are x^0.
+Let r_1 = target(mod x). So we must either subtract r_1x^0's, or add x - r_1x^0's.
+This will form a new "remaining" target, target2, that is divisible by x.
 
-Then, modulo x^2x
-2
- , the only blocks that change the expression are x^1x
-1
-  and x^0x
-0
- . However, since the new target is divisible by xx, there is no point to use x^0x
-0
- , as we would have to use at least xx of them to do the same work as one use of x^1x
-1
- , which is a strictly higher cost.
+Then, modulo x^2, the only blocks that change the expression are x^1 and x^0.
+However, since the new target is divisible by x, there is no point to use x^0,
+as we would have to use at least x of them to do the same work as one use of x^1,
+which is a strictly higher cost.
 
-Again, in a similar way, we have r_2 = \text{target}_2 \pmod {x^2}r
-2
-​
- =target
-2
-​
- (modx
-2
- ), and we must either subtract r_2 / xr
-2
-​
- /x x^1x
-1
- 's, or add x - r_2 / xx−r
-2
-​
- /x x^1x
-1
- 's. This will form a new remaining target \text{target}_3target
-3
-​
- , and so on.
+Again, in a similar way, we have r_2 = target2(mod x^2), and we must either subtract r_2 /(x x^1's), or add x - r_2 /(x x^1's).
+This will form a new remaining target3, and so on.
 
-As a concrete example, say x = 5, target = 123. We either add 2 or subtract 3. This leaves us with a target of 120 or 125. If the target is 120, we can either add 5 or subtract 20, leaving us with a target of 100 or 125. If the target is 100, we can either add 25 or subtract 100, leaving us with a target of 125 or 0. If the target is 125, we subtract 125.
-
-In the above example, the numbers considered were 123, 120, 125, 100, 0. Written in base 5, this is \text{443}_5, \text{440}_5, \text{1000}_5, \text{400}_5, \text{0}_5443
-5
-​
- ,440
-5
-​
- ,1000
-5
-​
- ,400
-5
-​
- ,0
-5
-​
- .
+As a concrete example, say x = 5, target = 123. We either add 2 or subtract 3.
+This leaves us with a target of 120 or 125.
+If the target is 120, we can either add 5 or subtract 20,
+leaving us with a target of 100 or 125.
+If the target is 100, we can either add 25 or subtract 100,
+leaving us with a target of 125 or 0.
+If the target is 125, we subtract 125.
 
 Algorithm
 
-Let's calculate dp(i, target) using a top down dp. Here, i will be the exponent of the block x^ix
-i
-  being considered, and target will be the remaining target, already divided by x^ix
-i
- .
+Let's calculate dp(i, target) using a top down dp. Here, i will be the exponent of the block x^i being considered,
+and target will be the remaining target, already divided by x^i
 
-From here, the recursion is straightforward: r = \text{target} \pmod xr=target(modx), and we either subtract rr blocks or add (x-r)(x−r) of them. The base cases are easily deduced - see the code for more details.
+From here, the recursion is straightforward: r = target(mod x),
+and we either subtract rr blocks or add (x-r) of them. The base cases are easily deduced - see the code for more details.
 
 
 pos the number of operations needed to get y % (x ^ (k+1))
@@ -172,5 +122,45 @@ public:
             }
         }
         return min(pos, k + neg) - 1;
+    }
+};
+
+class Solution { // very basic dfs, we can add a memo[target] to make it top-down dp
+private:
+    unordered_map<int, int> memo;
+public:
+    int leastOpsExpressTarget(int x, int target) {
+        if (memo.find(target) != memo.end()) {
+            return memo[target];
+        }
+        // At this time, you can get target either by add target times x/x or subtract (x - target) times x/x to x
+        // For example, x = 3, target = 2. Then, 3/3 + 3/3 or 3 - 3/3 is possible result
+        if (x > target) {
+            return min(target * 2 - 1, (x - target) * 2);
+        }
+        if (x == target) {  // just push x at the end
+            return 0;
+        }
+
+        long long sums = x;
+        int times = 0;
+        while (sums < target) {  // this is gready, put as much as possible 'x'
+            times++;
+            sums *= x;
+        }
+
+        if (sums == target) {  // one more 'x' you put, one more operator
+            return times;
+        }
+
+        // when you have remainder, you have two choices, one is add, the other is subtract
+        // for example, x = 3, target = 5. Then, 5 = 3 + 2 or 5 = 9 - 4
+        int l = INT_MAX, r = INT_MAX;
+        if (sums - target < target) {
+            l = leastOpsExpressTarget(x, sums - target) + times;  // using subtract
+        }
+        r = leastOpsExpressTarget(x, target - (sums / x)) + times - 1;  // using add, cause we only need sum/x, so times - 1
+        memo[target] = min(l, r) + 1;  // No matter +/- used, one more operator is add
+        return memo[target];
     }
 };
