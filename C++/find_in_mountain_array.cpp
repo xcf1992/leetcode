6f9c6f570810aa5491f51795efb5e4c0ffdf766a
Chenfu Xie <chenfu.xie@hulu.com>
2019-08-02 14:10:45 -0700

    add desc

--- C++/find_in_mountain_array.cpp
+++ C++/find_in_mountain_array.cpp
@@ -8,15 +8,17 @@ A.length >= 3
 There exists some i with 0 < i < A.length - 1 such that:
 A[0] < A[1] < ... A[i-1] < A[i]
 A[i] > A[i+1] > ... > A[A.length - 1]
-Given a mountain array mountainArr, return the minimum index such that mountainArr.get(index) == target.  If such an index doesn't exist, return -1.
+Given a mountain array mountainArr,
+return the minimum index such that mountainArr.get(index) == target.
+If such an index doesn't exist, return -1.
 
-You can't access the mountain array directly.  You may only access the array using a MountainArray interface:
+You can't access the mountain array directly.
+You may only access the array using a MountainArray interface:
 
 MountainArray.get(k) returns the element of the array at index k (0-indexed).
 MountainArray.length() returns the length of the array.
-Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer.  Also, any solutions that attempt to circumvent the judge will result in disqualification.
-
-
+Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer.
+Also, any solutions that attempt to circumvent the judge will result in disqualification.
 
 Example 1:
 
