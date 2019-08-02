2e090b4297f37a66cc10e4ad06de94076cc0c274
Chenfu Xie <chenfu.xie@hulu.com>
2019-08-02 11:23:34 -0700

    996. Number of Squareful Arrays

--- C++/main.cpp
+++ C++/main.cpp
@@ -14,46 +14,50 @@
 #include "extra_data_types.hpp"
 using namespace std;
 
-class Solution { // binary search
+class Solution { // dp
 private:
-    vector<int> under(double limit, vector<int>& A) {
-        double numer = 0.0;
-        double denom = 1.0;
-        int count = 0;
-        int i = -1;
-        int n = A.size();
-        for (int j = 1; j < n; ++j) {
-            // For each j, find the largest i so that primes[i] / primes[j] < x
-            // It has to be at least as big as the previous i, so reuse it ("two pointer")
-            while ((double)A[i + 1] < (double)A[j] * limit) {
-                i += 1;
-            }
-            count += i + 1;
-            // There are i+1 fractions: (primes[0], primes[j]),
-            // (primes[1], primes[j]), ..., (primes[i], primes[j])
-            if (i > 0 and numer * (double)A[j] < denom * (double)A[i]) {
-                numer = A[i];
-                denom = A[j];
-            }
-        }
-        return {count, (int)numer, (int)denom};
+    bool isPerfectSquare(int n1, int n2) {
+        int root = sqrt(n1 + n2);
+        return root * root == n1 + n2;
     }
 public:
-    vector<int> kthSmallestPrimeFraction(vector<int> A, int K) {
-        double left = 0;
-        double right = 1;
-        vector<int> result({0, 1});
-        while (right - left > 1e-9) {
-            double mid = left + (right - left) / 2;
-            vector<int> smaller = under(mid, A);
-            if (smaller[0] < K) {
-                left = mid;
+    int numSquarefulPerms(vector<int>& A) {
+        int n = A.size();
+        vector<vector<int>> adj;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) if (isPerfectSquare(A[i], A[j])) {
+                adj[i].push_back(j);
+                adj[j].push_back(i);
             }
-            else {
-                right = mid;
-                result[0] = smaller[1];
-                result[1] = smaller[2];
+        }
+
+        vector<vector<int>> dp(1 << n, vector<int>(n, 0));
+        for (int i = 0; i < n; ++i) {
+            dp[1 << i][i] = 1;
+        }
+
+        for (int state = 3; state < (1 << n); ++state) {
+            for (int i = 0; i < n; ++i) if (state & (1 << i)) {
+                for (int j : adj[i]) if (state & (1 << j)) {
+                    dp[state][i] += dp[state ^ (1 << i)][j];
+                }
             }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            result += dp[(1 << n) - 1][i];
+        }
+
+        /** Divide out permutations of equal elements. */
+        for (int i = 0; i < n; i++) {
+            int k = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (A[i] == A[j]) {
+                    k++;
+                }
+            }
+            result /= k;
         }
         return result;
     }
@@ -61,7 +65,7 @@ public:
 
 int main() {
     Solution s;
-    vector<int> temp({2,7,9,4,4});
+    vector<int> temp({1,17,8});
     vector<int> temp1({1,3,3,3,2});
     vector<vector<int>> matrix({
         {0,1},
@@ -84,5 +88,5 @@ int main() {
     TreeNode* r2 = new TreeNode(1);
     TreeNode* r3 = new TreeNode(3);
     r1 -> left = r2;
-    s.kthSmallestPrimeFraction({1, 7}, 1);
+    s.numSquarefulPerms(temp);
 }
--- C++/number_of_squareful_arrays.cpp
+++ C++/number_of_squareful_arrays.cpp
@@ -1,11 +1,12 @@
 /*
  996. Number of Squareful Arrays
 
- Given an array A of non-negative integers, the array is squareful if for every pair of adjacent elements, their sum is a perfect square.
+ Given an array A of non-negative integers,
+ the array is squareful if for every pair of adjacent elements,
+ their sum is a perfect square.
 
- Return the number of permutations of A that are squareful.  Two permutations A1 and A2 differ if and only if there is some index i such that A1[i] != A2[i].
-
-
+ Return the number of permutations of A that are squareful.
+ Two permutations A1 and A2 differ if and only if there is some index i such that A1[i] != A2[i].
 
  Example 1:
 
@@ -39,25 +40,24 @@
 #include <numeric>
 using namespace std;
 
-class Solution {
+class Solution { // dfs O(n^n)
 private:
-    void dfs(int num, int length, unordered_map<int, int>& count, unordered_map<int, unordered_set<int>>& adj, int& result) {
-        count[num] -= 1;
+    void dfs(int num, int length, map<int, int>& count, unordered_map<int, unordered_set<int>>& adj, int& result) {
         if (length == 0) {
             result += 1;
+            return;
         }
 
         // cause we use set to store adjacent element, so every permutation will be count only once
-        for (int next : adj[num]) {
-            if (count[next] != 0) {
-                dfs(next, length - 1, count, adj, result);
-            }
+        for (int next : adj[num]) if (count[next] != 0) {
+            count[next] -= 1;
+            dfs(next, length - 1, count, adj, result);
+            count[next] += 1;
         }
-        count[num] += 1;
     }
 public:
     int numSquarefulPerms(vector<int>& A) {
-        unordered_map<int, int> count;
+        map<int, int> count;
         for (int a : A) {
             count[a] += 1;
         }
@@ -70,13 +70,122 @@ public:
                 int root = sqrt(n1 + n2);
                 if (root * root == n1 + n2) {
                     adj[n1].insert(n2);
+                    adj[n2].insert(n1);
                 }
             }
         }
 
         int result = 0;
         for (auto& it : count) {
+            it.second -= 1;
             dfs(it.first, A.size() - 1, count, adj, result);
+            it.second += 1;
+        }
+        return result;
+    }
+};
+
+class Solution1 { // dfs with memo
+    int n = 0;
+
+    bool isPerfectSquare(int n1, int n2) {
+        int root = sqrt(n1 + n2);
+        return root * root == n1 + n2;
+    }
+
+    int dfs(int cur, int state, vector<vector<int>>& adj, vector<vector<int>>& dp) {
+        if (state == (1 << n) - 1) {
+            return 1;
+        }
+
+        if (dp[state][cur] != -1) {
+            return dp[state][cur];
+        }
+
+        dp[state][cur] = 0;
+        for (int nxt : adj[cur]) if (((1 << nxt) & state) == 0) {
+            dp[state][cur] += dfs(nxt, state | (1 << nxt), adj, dp);
+        }
+        return dp[state][cur];
+    }
+public:
+    int numSquarefulPerms(vector<int>& A) {
+        unordered_map<int, int> count;
+        for (int a : A) {
+            count[a] += 1;
+        }
+
+        n = A.size();
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) if (isPerfectSquare(A[i], A[j])) {
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+        }
+
+        // dp[state][cur] means the result we have state with current visiting node cur
+        vector<vector<int>> dp(1 << n, vector<int>(n, -1));
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            result += dfs(i, 1 << i, adj, dp);
+        }
+
+        vector<long> factorial(20);
+        factorial[0] = 1;
+        for (int i = 1; i < 20; ++i)
+            factorial[i] = i * factorial[i-1];
+        for (auto& it : count) if (it.second > 1) {
+            result /= factorial[it.second];
+        }
+        return result;
+    }
+};
+
+class Solution2 { // dp O(n^2 * 2^n)
+private:
+    bool isPerfectSquare(int n1, int n2) {
+        int root = sqrt(n1 + n2);
+        return root * root == n1 + n2;
+    }
+public:
+    int numSquarefulPerms(vector<int>& A) {
+        int n = A.size();
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) if (isPerfectSquare(A[i], A[j])) {
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+        }
+
+        vector<vector<int>> dp(1 << n, vector<int>(n, 0));
+        for (int i = 0; i < n; ++i) {
+            dp[1 << i][i] = 1;
+        }
+
+        for (int state = 3; state < (1 << n); ++state) {
+            for (int i = 0; i < n; ++i) if (state & (1 << i)) {
+                for (int j : adj[i]) if (state & (1 << j)) {
+                    dp[state][i] += dp[state ^ (1 << i)][j];
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            result += dp[(1 << n) - 1][i];
+        }
+
+        /** Divide out permutations of equal elements. */
+        for (int i = 0; i < n; i++) {
+            int k = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (A[i] == A[j]) {
+                    k++;
+                }
+            }
+            result /= k;
         }
         return result;
     }
