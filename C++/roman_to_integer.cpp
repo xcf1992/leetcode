3dcd07a3663809d6cfce9bd71f86d9a78d5871e6
Chenfu Xie <chenfu.xie@hulu.com>
2019-08-08 20:51:42 -0700

    add desc

--- C++/roman_to_integer.cpp
+++ C++/roman_to_integer.cpp
@@ -10,9 +10,17 @@ L             50
 C             100
 D             500
 M             1000
-For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+For example, two is written as II in Roman numeral,
+just two one's added together.
+Twelve is written as, XII, which is simply X + II.
+The number twenty seven is written as XXVII, which is XX + V + II.
 
-Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+Roman numerals are usually written largest to smallest from left to right.
+However, the numeral for four is not IIII.
+Instead, the number four is written as IV.
+Because the one is before the five we subtract it making four.
+The same principle applies to the number nine,
+which is written as IX. There are six instances where subtraction is used:
 
 I can be placed before V (5) and X (10) to make 4 and 9.
 X can be placed before L (50) and C (100) to make 40 and 90.
@@ -59,21 +67,26 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 using namespace std;
 
 class Solution {
+private:
+    unordered_map<char, int> value = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
 public:
     int romanToInt(string s) {
         int n = s.size();
         if (n == 0) {
             return -1;
         }
-        unordered_map<char, int> value({{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}});
+
         int result = value[s[n - 1]];
         for (int i = n - 2; i >= 0; --i) {
-            if (value[s[i]] >= value[s[i + 1]]) {
-                result += value[s[i]];
-            }
-            else {
-                result -= value[s[i]];
-            }
+            result += (value[s[i]] >= value[s[i + 1]] ? 1 : -1) * value[s[i]];
         }
         return result;
     }
