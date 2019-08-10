7e64ba75bfb7ce6879e97adcf6e71ceb94840380
Chenfu Xie <chenfu.xie@hulu.com>
2019-08-10 16:40:22 -0700

    657. Robot Return to Origin

--- C++/rotting_oranges.cpp
+++ C++/rotting_oranges.cpp
@@ -2,36 +2,35 @@
  994. Rotting Oranges
 
  In a given grid, each cell can have one of three values:
- 
+
  the value 0 representing an empty cell;
  the value 1 representing a fresh orange;
  the value 2 representing a rotten orange.
  Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
- 
- Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead.
- 
- 
- 
+
+ Return the minimum number of minutes that must elapse until no cell has a fresh orange.
+ If this is impossible, return -1 instead.
+
  Example 1:
- 
- 
- 
+
+
+
  Input: [[2,1,1],[1,1,0],[0,1,1]]
  Output: 4
  Example 2:
- 
+
  Input: [[2,1,1],[0,1,1],[1,0,1]]
  Output: -1
  Explanation:  The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
  Example 3:
- 
+
  Input: [[0,2]]
  Output: 0
  Explanation:  Since there are already no fresh oranges at minute 0, the answer is just 0.
- 
- 
+
+
  Note:
- 
+
  1 <= grid.length <= 10
  1 <= grid[0].length <= 10
  grid[i][j] is only 0, 1, or 2.
@@ -70,38 +69,25 @@ public:
                 }
             }
         }
-        
+
         int time = 0;
+        vector<int> diff({0, 1, 0, -1, 0});
         while (!bfs.empty() and fresh != 0) {
             int curSize = bfs.size();
             for (int i = 0; i < curSize; i++) {
                 int row = bfs.front() / n;
                 int col = bfs.front() % n;
                 bfs.pop();
-                
-                if (row + 1 < m and grid[row + 1][col] == 1 and !visited[(row + 1) * n + col]) {
-                    grid[row + 1][col] = 2;
-                    visited[(row + 1) * n + col] = true;
-                    fresh -= 1;
-                    bfs.push((row + 1) * n + col);
-                }
-                if (row - 1 >= 0 and grid[row - 1][col] == 1 and !visited[(row - 1) * n + col]) {
-                    grid[row - 1][col] = 2;
-                    visited[(row - 1) * n + col] = true;
-                    fresh -= 1;
-                    bfs.push((row - 1) * n + col);
-                }
-                if (col + 1 < n and grid[row][col + 1] == 1 and !visited[row * n + col + 1]) {
-                    grid[row][col + 1] = 2;
-                    visited[row * n + col + 1] = true;
-                    fresh -= 1;
-                    bfs.push(row * n + col + 1);
-                }
-                if (col - 1 >= 0 and grid[row][col - 1] == 1 and !visited[row * n + col - 1]) {
-                    grid[row][col - 1] = 2;
-                    visited[row * n + col - 1] = true;
-                    fresh -= 1;
-                    bfs.push(row * n + col - 1);
+
+                for (int i = 1; i < diff.size(); ++i) {
+                    int nRow = row + diff[i];
+                    int nCol = col + diff[i - 1];
+                    if (nRow < m and nRow >= 0 and nCol < n and nCol >= 0 and grid[nRow][nCol] == 1 and !visited[nRow * n + nCol]) {
+                        grid[nRow][nCol] = 2;
+                        visited[nRow * n + nCol] = true;
+                        fresh -= 1;
+                        bfs.push(nRow * n + nCol);
+                    }
                 }
             }
             time += 1;
