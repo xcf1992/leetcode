1956937a8a9d186728328ce050b246330fc6b04d
Chenfu Xie <chenfu.xie@hulu.com>
2019-08-11 18:32:53 -0700

    191. Number of 1 Bits

--- C++/employee_importance.cpp
+++ C++/employee_importance.cpp
@@ -1,3 +1,35 @@
+/*
+690. Employee Importance
+You are given a data structure of employee information,
+which includes the employee's unique id,
+his importance value and his direct subordinates' id.
+
+For example, employee 1 is the leader of employee 2,
+and employee 2 is the leader of employee 3.
+They have importance value 15, 10 and 5, respectively.
+Then employee 1 has a data structure like [1, 15, [2]],
+and employee 2 has [2, 10, [3]],
+and employee 3 has [3, 5, []].
+Note that although employee 3 is also a subordinate of employee 1,
+the relationship is not direct.
+
+Now given the employee information of a company,
+and an employee id, you need to return the total importance value of this employee and all his subordinates.
+
+Example 1:
+
+Input: [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
+Output: 11
+Explanation:
+Employee 1 has importance value 5, and he has two direct subordinates: employee 2 and employee 3.
+They both have importance value 3. So the total importance value of employee 1 is 5 + 3 + 3 = 11.
+
+
+Note:
+
+One employee has at most one direct leader and may have several subordinates.
+The maximum number of employees won't exceed 2000.
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,7 +41,6 @@
 #include <stack>
 #include <stdio.h>
 using namespace std;
-
 
 // Employee info
 class Employee {
@@ -25,10 +56,10 @@ public:
 
 class Solution {
 private:
-    int sum(vector<Employee*> employees, unordered_map<int, int> index, int id) {
+    int dfs(vector<Employee*> employees, unordered_map<int, int> index, int id) {
         int result = employees[index[id]] -> importance;
         for (int sub : employees[index[id]] -> subordinates) {
-            result += sum(employees, index, sub);
+            result += dfs(employees, index, sub);
         }
         return result;
     }
@@ -38,7 +69,6 @@ public:
         for (int i = 0; i < employees.size(); i++) {
             index[employees[i] -> id] = i;
         }
-
-        return sum(employees, index, id);
+        return dfs(employees, index, id);
     }
 };
