fddbf4e28cca1bfee932cf5323f535c5e4dd631b
Chenfu Xie <chenfu.xie@hulu.com>
2019-08-13 14:06:43 -0700

    11. Container With Most Water

--- C++/generate_parenthese.cpp
+++ C++/generate_parenthese.cpp
@@ -1,3 +1,17 @@
+/*
+22. Generate Parentheses
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,65 +21,33 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        result.clear();
-        
-        if (n == 0) {
-            return result;
-        }
-        
-        if (n == 1) {
-            string s = "()";
-            result.push_back(s);
-            return result;
-        }
-        
-        string s;
-        generate(result, n, n, s);
-        
-        return result;
-    }
-    
-    void generate(vector<string> &result, int left, int right, string &s) {
-        if (left < 0 || left > right) {
+private:
+    void generate(vector<string>& result, string& parenthesis, int size, int left, int right) {
+        if (parenthesis.size() == size * 2) {
+            result.push_back(parenthesis);
             return;
         }
-        
-        if (left == 0 && right == 0) {
-            result.push_back(s);
-            return ;
+
+        if (left < size) {
+            parenthesis += '(';
+            generate(result, parenthesis, size, left + 1, right);
+            parenthesis.pop_back();
         }
-        
-        if (left > 0) {
-            string temp = s + "(";
-            generate(result, left - 1, right, temp);
-        }
-        if (right > left) {
-            string temp = s + ")";
-            generate(result, left, right - 1, temp);
+        if (left > right) {
+            parenthesis += ')';
+            generate(result, parenthesis, size, left, right + 1);
+            parenthesis.pop_back();
         }
     }
-};
-
-class cmp {
 public:
-	bool operator() (const int &l, const int &r) {
-		return l > r;
-	}
-};
+    vector<string> generateParenthesis(int n) {
+        if (n <= 0) {
+            return {};
+        }
 
-int main() {
-	string s1 = "a";
-	string s2 = "aa";
-	cout << s2.compare(s1) << endl;
-	priority_queue<int, vector<int>, cmp> q;
-	q.push(10);
-	q.push(100);
-	q.push(102);
-	q.push(30);
-	q.push(3);
-	cout << q.top() << endl;
-	return 0;
-}
+        vector<string> result;
+        string parenthesis = "";
+        generate(result, parenthesis, n, 0, 0);
+        return result;
+    }
+};
