aa9f01ff7e8821f48b895c08cfd8ff3c936be962
Chenfu Xie <chenfu.xie@hulu.com>
2019-09-03 09:46:59 -0700

    1175. Prime Arrangements

--- C++/count_primes.cpp
+++ C++/count_primes.cpp
@@ -7,7 +7,7 @@ Example:
 Input: 10
 Output: 4
 Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
- */
+*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -37,7 +37,7 @@ public:
 
         int result = 1;
         for (int i = 3; i < n; i += 2) if (isPrime[i]) {
-                result += 1;
+            result += 1;
         }
         return result;
     }
