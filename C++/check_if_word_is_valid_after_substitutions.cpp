3fec17ee798cbf097c7ba229fcd847fa3aa60b42
Chenfu Xie <chenfu.xie@hulu.com>
2019-09-07 14:45:03 -0700

    1180. Count Substrings with Only One Distinct Letter

--- C++/check_if_word_is_valid_after_substitutions.cpp
+++ C++/check_if_word_is_valid_after_substitutions.cpp
@@ -1,44 +1,45 @@
 /*
- 1003. Check If Word Is Valid After Substitutions
+1003. Check If Word Is Valid After Substitutions
 
- We are given that the string "abc" is valid.
+We are given that the string "abc" is valid.
 
- From any valid string V, we may split V into two pieces X and Y such that X + Y (X concatenated with Y) is equal to V.  (X or Y may be empty.)  Then, X + "abc" + Y is also valid.
+From any valid string V,
+we may split V into two pieces X and Y such that X + Y (X concatenated with Y) is equal to V.
+(X or Y may be empty.)
+Then, X + "abc" + Y is also valid.
 
- If for example S = "abc", then examples of valid strings are: "abc", "aabcbc", "abcabc", "abcabcababcc".  Examples of invalid strings are: "abccba", "ab", "cababc", "bac".
+If for example S = "abc",
+then examples of valid strings are: "abc", "aabcbc", "abcabc", "abcabcababcc".
+Examples of invalid strings are: "abccba", "ab", "cababc", "bac".
 
- Return true if and only if the given string S is valid.
+Return true if and only if the given string S is valid.
 
+Example 1:
+Input: "aabcbc"
+Output: true
+Explanation:
+We start with the valid string "abc".
+Then we can insert another "abc" between "a" and "bc", resulting in "a" + "abc" + "bc" which is "aabcbc".
 
+Example 2:
+Input: "abcabcababcc"
+Output: true
+Explanation:
+"abcabcabc" is valid after consecutive insertings of "abc".
+Then we can insert "abc" before the last letter, resulting in "abcabcab" + "abc" + "c" which is "abcabcababcc".
 
- Example 1:
+Example 3:
+Input: "abccba"
+Output: false
 
- Input: "aabcbc"
- Output: true
- Explanation:
- We start with the valid string "abc".
- Then we can insert another "abc" between "a" and "bc", resulting in "a" + "abc" + "bc" which is "aabcbc".
- Example 2:
+Example 4:
+Input: "cababc"
+Output: false
 
- Input: "abcabcababcc"
- Output: true
- Explanation:
- "abcabcabc" is valid after consecutive insertings of "abc".
- Then we can insert "abc" before the last letter, resulting in "abcabcab" + "abc" + "c" which is "abcabcababcc".
- Example 3:
+Note:
 
- Input: "abccba"
- Output: false
- Example 4:
-
- Input: "cababc"
- Output: false
-
-
- Note:
-
- 1 <= S.length <= 20000
- S[i] is 'a', 'b', or 'c'
+1 <= S.length <= 20000
+S[i] is 'a', 'b', or 'c'
 */
 #include <iostream>
 #include <string>
---
+++ C++/count_substrings_with_only_one_distinct_letter.cpp
@@ -0,0 +1,58 @@
+/*
+1180. Count Substrings with Only One Distinct Letter
+Given a string S, return the number of substrings that have only one distinct letter.
+
+Example 1:
+
+Input: S = "aaaba"
+Output: 8
+Explanation: The substrings with one distinct letter are "aaa", "aa", "a", "b".
+"aaa" occurs 1 time.
+"aa" occurs 2 times.
+"a" occurs 4 times.
+"b" occurs 1 time.
+So the answer is 1 + 2 + 4 + 1 = 8.
+Example 2:
+
+Input: S = "aaaaaaaaaa"
+Output: 55
+
+
+Constraints:
+
+1 <= S.length <= 1000
+S[i] consists of only lowercase English letters.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int countLetters(string S) {
+        int result = 0;
+        int left = 0;
+        int right = 0;
+        int n = S.size();
+        while (left < n and right < n) {
+            while (right < n and S[right] == S[left]) {
+                right += 1;
+            }
+            int len = right - left;
+            result += len * (len + 1) / 2;
+            left = right;
+        }
+        return result;
+    }
+};
