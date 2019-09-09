cc6d04aef703e17135d5a92925a02c050fa3586a
Chenfu Xie <chenfu.xie@hulu.com>
2019-09-09 10:58:54 -0700

    1186. Maximum Subarray Sum with One Deletion

--- C++/maximum_subarray.cpp
+++ C++/maximum_subarray.cpp
@@ -1,16 +1,19 @@
 /*
- Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+53. Maximum Subarray
+https://leetcode.com/problems/maximum-subarray/
 
- Example:
+Given an integer array nums,
+find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
- Input: [-2,1,-3,4,-1,2,1,-5,4],
- Output: 6
- Explanation: [4,-1,2,1] has the largest sum = 6.
- Follow up:
+Example:
 
- If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 */
-
 #include <iostream>
 #include <string>
 #include <vector>
