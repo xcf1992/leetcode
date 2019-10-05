0ac7753eb4c00ccedbd53dd5745fe7f231e10cec
Chenfu Xie <chenfu.xie@hulu.com>
2019-10-05 09:40:24 -0700

    ms oa

--- C++/decode_ways.cpp
+++ C++/decode_ways.cpp
@@ -1,5 +1,6 @@
 /*
 91. Decode Ways
+
 A message containing letters from A-Z is being encoded to numbers using the following mapping:
 'A' -> 1
 'B' -> 2
--- C++/kth_smallest_element_in_a_sorted_matrix.cpp
+++ C++/kth_smallest_element_in_a_sorted_matrix.cpp
@@ -1,20 +1,21 @@
 /*
 378. Kth Smallest Element in a Sorted Matrix
+
 Given a n x n matrix where each of the rows and columns are sorted in ascending order,
 find the kth smallest element in the matrix.
 
-Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+Note that it is the kth smallest element in the sorted order,
+not the kth distinct element.
 
 Example:
-
 matrix = [
    [ 1,  5,  9],
    [10, 11, 13],
    [12, 13, 15]
 ],
 k = 8,
-
 return 13.
+
 Note:
 You may assume k is always valid, 1 ≤ k ≤ n2.
 */
@@ -44,7 +45,11 @@ Single Kth smallest element in matrix.
 k = 11
 Result = 7
 
-This ensures count equals to k at a point, in which it includes the kth smallest element in the matrix. At the moment, binary search shrinks high boundary to mid, instead of returning mid in the regular binary search. You can imagine it as "hi" is waiting at the right spot for "lo" to meet him. (Like dating!)
+This ensures count equals to k at a point,
+in which it includes the kth smallest element in the matrix.
+At the moment, binary search shrinks high boundary to mid,
+instead of returning mid in the regular binary search.
+You can imagine it as "hi" is waiting at the right spot for "lo" to meet him. (Like dating!)
 
 Multiple Kth smallest element in matrix.
 [1,2,3,5]
@@ -58,7 +63,8 @@ Count won't be equal to k, but "hi" is still guaranteed to stop at right spot.
 In the above example, the count is 11 when "mid" is 7.
 After "hi" shrinks to mid, it will not move until "lo" comes to him.
 
-To sum up, "lo" is ensured to reach an authentic element in the matrix, because "hi" will approach and sit at the right spot anyway.
+To sum up, "lo" is ensured to reach an authentic element in the matrix,
+because "hi" will approach and sit at the right spot anyway.
 */
 class Solution {
 public:
