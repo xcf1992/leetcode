0ea428c4320dbe1d076007c6ce2da71cd444aeed
Chenfu Xie <chenfu.xie@hulu.com>
2019-10-04 17:00:58 -0700

    get ms oa

--- C++/count_substrings_with_only_one_distinct_letter.cpp
+++ C++/count_substrings_with_only_one_distinct_letter.cpp
@@ -1,9 +1,9 @@
 /*
 1180. Count Substrings with Only One Distinct Letter
+
 Given a string S, return the number of substrings that have only one distinct letter.
 
 Example 1:
-
 Input: S = "aaaba"
 Output: 8
 Explanation: The substrings with one distinct letter are "aaa", "aa", "a", "b".
@@ -12,14 +12,12 @@ Explanation: The substrings with one distinct letter are "aaa", "aa", "a", "b".
 "a" occurs 4 times.
 "b" occurs 1 time.
 So the answer is 1 + 2 + 4 + 1 = 8.
-Example 2:
 
+Example 2:
 Input: S = "aaaaaaaaaa"
 Output: 55
 
-
 Constraints:
-
 1 <= S.length <= 1000
 S[i] consists of only lowercase English letters.
 */
