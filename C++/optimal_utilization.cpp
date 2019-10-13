3fd84287539d6dc6b39d554d24986420a038c008
Chenfu Xie <chenfu.xie@hulu.com>
2019-10-12 21:09:08 -0700

    add desc

--- C++/optimal_utilization.cpp
+++ C++/optimal_utilization.cpp
@@ -1,47 +1,64 @@
+/*
+https://leetcode.com/discuss/interview-question/344650/Amazon-Online-Assessment-Questions
+https://leetcode.com/discuss/interview-question/373202
+*/
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 #include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+#include <numeric>
+using namespace std;
 
-vector<pair<int, int> > optimalUtilization(int maxTravelDist,
-							        vector<pair<int, int> > forwardRouteList,
-							        vector<pair<int, int> > returnRouteList)
-{
-    int n1 = forwardRouteList.size();
-    int n2 = returnRouteList.size();
-    if (n1 == 0 or n2 == 0) {
-        return {};
-    }
-
-    sort(forwardRouteList.begin(), forwardRouteList.end(), [](pair<int, int>& a, pair<int, int>& b) {
-       return a.second < b.second;
-    });
-    sort(returnRouteList.begin(), returnRouteList.end(), [](pair<int, int>& a, pair<int, int>& b) {
-       return a.second < b.second;
-    });
-
-    int left = 0;
-    int right = n2 - 1;
-    int curMax = -1;
-    unordered_map<int, vector<pair<int, int> > > memo;
-    while (left < n1 and right >= 0) {
-        int curVal = forwardRouteList[left].second + returnRouteList[right].second;
-        if (curVal > maxTravelDist) {
-            right -= 1;
-            continue;
+class Solution {
+public:
+    vector<pair<int, int> > optimalUtilization(int maxTravelDist, vector<pair<int, int> > forwardRouteList, vector<pair<int, int> > returnRouteList) {
+        int n1 = forwardRouteList.size();
+        int n2 = returnRouteList.size();
+        if (n1 == 0 or n2 == 0) {
+            return {};
         }
 
-        if (curVal > curMax) {
-            curMax = curVal;
-        }
-        int r = right;
-        while (r >= 0 and returnRouteList[r].second == returnRouteList[right].second) {
-            memo[curVal].push_back({forwardRouteList[left].first, returnRouteList[r].first});
-            r -= 1;
-        }
-        left += 1;
-    }
+        sort(forwardRouteList.begin(), forwardRouteList.end(), [](pair<int, int>& a, pair<int, int>& b) {
+        return a.second < b.second;
+        });
+        sort(returnRouteList.begin(), returnRouteList.end(), [](pair<int, int>& a, pair<int, int>& b) {
+        return a.second < b.second;
+        });
 
-    if (curMax == -1) {
-        return {};
+        int left = 0;
+        int right = n2 - 1;
+        int curMax = -1;
+        unordered_map<int, vector<pair<int, int> > > memo;
+        while (left < n1 and right >= 0) {
+            int curVal = forwardRouteList[left].second + returnRouteList[right].second;
+            if (curVal > maxTravelDist) {
+                right -= 1;
+                continue;
+            }
+
+            if (curVal > curMax) {
+                curMax = curVal;
+            }
+            int r = right;
+            while (r >= 0 and returnRouteList[r].second == returnRouteList[right].second) {
+                memo[curVal].push_back({forwardRouteList[left].first, returnRouteList[r].first});
+                r -= 1;
+            }
+            left += 1;
+        }
+
+        if (curMax == -1) {
+            return {};
+        }
+        return memo[curMax];
     }
-    return memo[curMax];
-}
-// FUNCTION SIGNATURE ENDS
+};
