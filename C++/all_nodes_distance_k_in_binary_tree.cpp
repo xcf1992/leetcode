14f824bdca222d02f63ec5d5c938569f3c212b52
Chenfu Xie <chenfu.xie@hulu.com>
2019-10-15 22:31:19 -0700

    oct 15 evening

--- C++/all_nodes_distance_k_in_binary_tree.cpp
+++ C++/all_nodes_distance_k_in_binary_tree.cpp
@@ -1,14 +1,15 @@
 /*
- 863. All Nodes Distance K in Binary Tree
- We are given a binary tree (with root node root),
- a target node, and an integer value K.
+863. All Nodes Distance K in Binary Tree
+https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
 
- Return a list of the values of all nodes that have a distance K from the target node.
- The answer can be returned in any order.
+We are given a binary tree (with root node root),
+a target node, and an integer value K.
 
- Example 1:
+Return a list of the values of all nodes that have a distance K from the target node.
+The answer can be returned in any order.
 
- Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
        3
     /     \
    5       1
@@ -16,24 +17,19 @@
  6   2   0   8
     / \
    7   4
- Output: [7,4,1]
+Output: [7,4,1]
+Explanation:
+The nodes that are a distance 2 from the target node (with value 5)
+have values 7, 4, and 1.
 
- Explanation:
- The nodes that are a distance 2 from the target node (with value 5)
- have values 7, 4, and 1.
+Note that the inputs "root" and "target" are actually TreeNodes.
+The descriptions of the inputs above are just serializations of these objects.
 
-
-
- Note that the inputs "root" and "target" are actually TreeNodes.
- The descriptions of the inputs above are just serializations of these objects.
-
-
- Note:
-
- The given tree is non-empty.
- Each node in the tree has unique values 0 <= node.val <= 500.
- The target node is a node in the tree.
- 0 <= K <= 1000.
+Note:
+The given tree is non-empty.
+Each node in the tree has unique values 0 <= node.val <= 500.
+The target node is a node in the tree.
+0 <= K <= 1000.
 */
 #include <iostream>
 #include <sstream>
--- C++/design_twitter.cpp
+++ C++/design_twitter.cpp
@@ -84,9 +84,9 @@ public:
     }
 
     /*
-     * Retrieve the 10 most recent tweet ids in the user's news feed.
-     * Each item in the news feed must be posted by users who the user followed or by the user herself.
-     * Tweets must be ordered from most recent to least recent.
+    * Retrieve the 10 most recent tweet ids in the user's news feed.
+    * Each item in the news feed must be posted by users who the user followed or by the user herself.
+    * Tweets must be ordered from most recent to least recent.
     */
     vector<int> getNewsFeed(int userId) {
         auto comp = [](Tweet& a, Tweet& b) {
