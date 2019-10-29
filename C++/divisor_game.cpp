2018852b064c94474b9cb86c76ed998b17523ebd
Chenfu Xie <chenfu.xie@hulu.com>
2019-10-29 11:54:36 -0700

    1237. Find Positive Integer Solution for a Given Equation

--- C++/divisor_game.cpp
+++ C++/divisor_game.cpp
@@ -1,31 +1,29 @@
 /*
- 1025. Divisor Game
- Alice and Bob take turns playing a game, with Alice starting first.
+1025. Divisor Game
 
- Initially, there is a number N on the chalkboard.
- On each player's turn, that player makes a move consisting of:
+Alice and Bob take turns playing a game, with Alice starting first.
 
- Choosing any x with 0 < x < N and N % x == 0.
- Replacing the number N on the chalkboard with N - x.
- Also, if a player cannot make a move, they lose the game.
+Initially, there is a number N on the chalkboard.
+On each player's turn, that player makes a move consisting of:
 
- Return True if and only if Alice wins the game, assuming both players play optimally.
+Choosing any x with 0 < x < N and N % x == 0.
+Replacing the number N on the chalkboard with N - x.
+Also, if a player cannot make a move, they lose the game.
 
- Example 1:
+Return True if and only if Alice wins the game, assuming both players play optimally.
 
- Input: 2
- Output: true
- Explanation: Alice chooses 1, and Bob has no more moves.
- Example 2:
+Example 1:
+Input: 2
+Output: true
+Explanation: Alice chooses 1, and Bob has no more moves.
 
- Input: 3
- Output: false
- Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
+Example 2:
+Input: 3
+Output: false
+Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
 
-
- Note:
-
- 1 <= N <= 1000
+Note:
+1 <= N <= 1000
 */
 #include <iostream>
 #include <string>
