1f49eafa5bcb9426599e95d912f835a5b50c8ff8
chenfu.xie <chenfu.xie@bytedance.com>
2020-03-23 19:30:39 -0700

    1391. Check if There is a Valid Path in a Grid

--- C++/check_if_there_is_a_valid_path_in_a_grid.cpp
+++ C++/check_if_there_is_a_valid_path_in_a_grid.cpp
@@ -64,6 +64,42 @@ using namespace std;
 class Solution {
 public:
     bool hasValidPath(vector<vector<int>>& grid) {
+        vector<vector<vector<int>>> directs({
+            {{0, -1}, {0, 1}},
+            {{-1, 0}, {1, 0}},
+            {{0, -1}, {1, 0}},
+            {{0, 1}, {1, 0}},
+            {{0, -1}, {-1, 0}},
+            {{0, 1}, {-1, 0}}
+        });
 
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        queue<vector<int>> bfs;
+        bfs.push({0, 0});
+        visited[0][0] = true;
+        while (!bfs.empty()) {
+            vector<int> cur = bfs.front();
+            bfs.pop();
+
+            int row = cur[0];
+            int col = cur[1];
+            int type = grid[row][col] - 1;
+            for (auto& dir : directs[type]) {
+                int nr = row + dir[0];
+                int nc = col + dir[1];
+                if (nr < 0 or nr >= m or nc < 0 or nc >= n or visited[nr][nc]) {
+                    continue;
+                }
+
+                for (auto& backDir : directs[grid[nr][nc] - 1])
+                    if (nr + backDir[0] == row and nc + backDir[1] == col) {
+                        visited[nr][nc] = true;
+                        bfs.push({nr, nc});
+                    }
+            }
+        }
+        return visited[m - 1][n - 1];
     }
 };
