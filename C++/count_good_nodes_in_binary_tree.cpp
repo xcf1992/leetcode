919c1bbceca1958c08f07732ea3ba38ac4fafef6
chenfu.xie <chenfu.xie@bytedance.com>
2020-05-17 10:30:44 -0700

    1448. Count Good Nodes in Binary Tree

---
+++ C++/count_good_nodes_in_binary_tree.cpp
@@ -0,0 +1,73 @@
+/*
+1448. Count Good Nodes in Binary Tree
+https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+Given a binary tree root, a node X in the tree is named good
+if in the path from root to X there are no nodes with a value greater than X.
+Return the number of good nodes in the binary tree.
+
+Example 1:
+                    3
+        1                       4
+3                           1       5
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+
+Example 2:
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+Example 3:
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+
+Constraints:
+The number of nodes in the binary tree is in the range [1, 10^5].
+Each node's value is between [-10^4, 10^4].
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+#include <set>
+#include <numeric>
+#include "extra_data_types.hpp"
+using namespace std;
+
+class Solution {
+private:
+    void traverse(TreeNode* root, int curMax, int& result) {
+        if (root == nullptr) {
+            return;
+        }
+
+        if (root -> val >= curMax) {
+            curMax = root -> val;
+            result += 1;
+        }
+
+        traverse(root -> left, curMax, result);
+        traverse(root -> right, curMax, result);
+    }
+public:
+    int goodNodes(TreeNode* root) {
+        int curMax = INT_MIN;
+        int result = 0;
+        traverse(root, curMax, result);
+        return result;
+    }
+};
