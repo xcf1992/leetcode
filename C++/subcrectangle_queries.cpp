f78b72c9d0959ef74555bacec0b68c8f711cbe51
chenfu.xie <chenfu.xie@bytedance.com>
2020-06-13 17:03:31 -0700

    1476. Subrectangle Queries

--- C++/subcrectangle_queries.cpp
+++ C++/subcrectangle_queries.cpp
@@ -83,16 +83,24 @@ cols == rectangle[i].length
 using namespace std;
 
 class SubrectangleQueries {
+private:
+    vector<vector<int>> rect;
+    vector<vector<int>> subs;
 public:
     SubrectangleQueries(vector<vector<int>>& rectangle) {
-
+        swap(rect, rectangle);
     }
 
     void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-
+        subs.push_back({row1, col1, row2, col2, newValue});
     }
 
     int getValue(int row, int col) {
-
+        for (int i = subs.size() - 1; i >= 0; --i) {
+            if (subs[i][0] <= row && subs[i][1] <= col && row <= subs[i][2] && col <= subs[i][3]) {
+                return subs[i][4];
+            }
+        }
+        return rect[row][col];
     }
 };
