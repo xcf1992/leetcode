7cc6ecfbc341a1eb7b86b389ab42cb3947995219
chenfu.xie <chenfu.xie@bytedance.com>
2020-07-12 15:51:34 -0700

    1512. Number of Good Pairs

--- C++/range_sum_of_sorted_subarray_sums.cpp
+++ C++/range_sum_of_sorted_subarray_sums.cpp
@@ -2,30 +2,28 @@
 1508. Range Sum of Sorted Subarray Sums
 https://leetcode.com/problems/range-sum-of-sorted-subarray-sums/
 
-Given the array nums consisting of n positive integers. You computed the sum of all non-empty continous subarrays from the array and then sort them in non-decreasing order, creating a new array of n * (n + 1) / 2 numbers.
+Given the array nums consisting of n positive integers.
+You computed the sum of all non-empty continous subarrays from the array and then sort them in non-decreasing order,
+creating a new array of n * (n + 1) / 2 numbers.
 
-Return the sum of the numbers from index left to index right (indexed from 1), inclusive, in the new array. Since the answer can be a huge number return it modulo 10^9 + 7.
-
-
+Return the sum of the numbers from index left to index right (indexed from 1),
+inclusive, in the new array. Since the answer can be a huge number return it modulo 10^9 + 7.
 
 Example 1:
-
 Input: nums = [1,2,3,4], n = 4, left = 1, right = 5
 Output: 13
 Explanation: All subarray sums are 1, 3, 6, 10, 2, 5, 9, 3, 7, 4. After sorting them in non-decreasing order we have the new array [1, 2, 3, 3, 4, 5, 6, 7, 9, 10]. The sum of the numbers from index le = 1 to ri = 5 is 1 + 2 + 3 + 3 + 4 = 13.
-Example 2:
 
+Example 2:
 Input: nums = [1,2,3,4], n = 4, left = 3, right = 4
 Output: 6
 Explanation: The given array is the same as example 1. We have the new array [1, 2, 3, 3, 4, 5, 6, 7, 9, 10]. The sum of the numbers from index le = 3 to ri = 4 is 3 + 3 = 6.
-Example 3:
 
+Example 3:
 Input: nums = [1,2,3,4], n = 4, left = 1, right = 10
 Output: 50
 
-
 Constraints:
-
 1 <= nums.length <= 10^3
 nums.length == n
 1 <= nums[i] <= 100
@@ -45,16 +43,35 @@ nums.length == n
 #include <stdio.h>
 #include "extra_data_types.hpp"
 using namespace std;
+/*
+The element in priority queue is a pair of integers. The first integer is the nums[i] + ... + nums[j], and the second integer is j+1.
 
+The running time is O(right * lgn) worst case. The real benefit of using priority_queue is that memory usage is O(n).
+*/
 class Solution {
 public:
-    string reformatDate(string date) {
-        unordered_map<string, string> mm{{"Jan", "01"}, {"Feb", "02"}, {"Mar", "03"}, {"Apr", "04"}, {"May", "05"}, {"Jun", "06"}, {"Jul", "07"}, {"Aug", "08"}, {"Sep", "09"}, {"Oct", "10"}, {"Nov", "11"}, {"Dec", "12"}};
-        stringstream ss{date};
-        string d, m, y, dd{"0"};
-        ss >> d >> m >> y;
-        if(d.size() == 3) dd += d[0];
-        else dd = d.substr(0,2);
-        return y+"-"+mm[m]+"-"+dd;
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> mqueue;
+        for (int i = 0; i < n; i++) {
+            mqueue.push({nums[i], i+1});
+        }
+
+        int result = 0;
+        int mod = 1e9 + 7;
+        for (int i = 1; i <= right; i++) {
+            auto p = mqueue.top();
+            mqueue.pop();
+
+            if (i >= left) {
+                result = (result + p.first) % mod;
+            }
+            if (p.second < n) {
+                p.first += nums[p.second];
+                p.second += 1;
+                mqueue.push(p);
+            }
+        }
+        return result;
     }
 };
+
