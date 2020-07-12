76eeed71b01d7ef2c1dc4bbb9b468a0e4d34f629
chenfu.xie <chenfu.xie@bytedance.com>
2020-07-12 15:35:33 -0700

    1507. Reformat Date

--- C++/minimum_number_difference_between_largest_and_smallest_value_in_three_moves.cpp
+++ C++/minimum_number_difference_between_largest_and_smallest_value_in_three_moves.cpp
@@ -64,3 +64,12 @@ Solution 1: Quick Sort
 I used quick sort to find out the biggest and smallest
 So time and space are O(quick sort)
 */
+class Solution {
+public:
+    int minDifference(vector<int>& A) {
+        int n = A.size();
+        if (n < 5) return 0;
+        sort(A.begin(), A.end());
+        return min({A[n - 1] - A[3], A[n - 2] - A[2], A[n - 3] - A[1], A[n - 4] - A[0]});
+    }
+};
