b7dcb744ad94895ad3979023e5e426379edfd9c3
chenfu.xie <chenfu.xie@bytedance.com>
2020-07-25 17:48:44 -0700

    1483. Kth Ancestor of a Tree Node

---
+++ C++/kth_ancestor_of_a_tree_node.cpp
@@ -0,0 +1,83 @@
+/*
+1483. Kth Ancestor of a Tree Node
+https://leetcode.com/problems/kth-ancestor-of-a-tree-node/
+
+You are given a tree with n nodes numbered from 0 to n-1 in the form of a parent array
+where parent[i] is the parent of node i.
+The root of the tree is node 0.
+
+Implement the function getKthAncestor(int node, int k) to return the k-th ancestor of the given node.
+If there is no such ancestor, return -1.
+
+The k-th ancestor of a tree node is the k-th node in the path from that node to the root.
+
+Example:
+                    0
+            1               2
+        3       4         5     6
+Input:
+["TreeAncestor","getKthAncestor","getKthAncestor","getKthAncestor"]
+[[7,[-1,0,0,1,1,2,2]],[3,1],[5,2],[6,3]]
+
+Output:
+[null,1,0,-1]
+
+Explanation:
+TreeAncestor treeAncestor = new TreeAncestor(7, [-1, 0, 0, 1, 1, 2, 2]);
+treeAncestor.getKthAncestor(3, 1);  // returns 1 which is the parent of 3
+treeAncestor.getKthAncestor(5, 2);  // returns 0 which is the grandparent of 5
+treeAncestor.getKthAncestor(6, 3);  // returns -1 because there is no such ancestor
+
+Constraints:
+1 <= k <= n <= 5*10^4
+parent[0] == -1 indicating that 0 is the root node.
+0 <= parent[i] < n for all 0 < i < n
+0 <= node < n
+There will be at most 5*10^4 queries.
+*/
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+#include "extra_data_types.hpp"
+using namespace std;
+
+class TreeAncestor {
+public:
+    vector<vector<int>>v;
+    TreeAncestor(int n, vector<int>& parent) {
+        vector<vector<int>> par(n, vector<int>(20));
+        for (int i = 0; i < n; i++) par[i][0] = parent[i];
+        for (int j = 1; j < 20; j++) {
+            for (int i = 0; i < n; i++) {
+                if (par[i][j - 1] == -1) par[i][j] = -1;
+                else par[i][j] = par[par[i][j - 1]][j - 1];
+            }
+        }
+        swap(v, par);
+    }
+    int getKthAncestor(int node, int k) {
+        for (int i = 0; i < 20; i++) {
+            if ((k >> i) & 1) {
+                node = v[node][i];
+                if (node == -1) return -1;
+            }
+        }
+        return node;
+    }
+};
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor* obj = new TreeAncestor(n, parent);
+ * int param_1 = obj->getKthAncestor(node,k);
+ */
