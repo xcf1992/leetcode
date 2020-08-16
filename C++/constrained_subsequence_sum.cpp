c15c856550a17b8187501655f86a900fc054dc81
chenfu.xie <chenfu.xie@bytedance.com>
2020-08-15 21:16:48 -0700

    1425. Constrained Subsequence Sum

--- C++/constrained_subsequence_sum.cpp
+++ C++/constrained_subsequence_sum.cpp
@@ -41,10 +41,79 @@ Constraints:
 #include <stdio.h>
 #include <set>
 using namespace std;
+/*
+Intuition
+We need to know the maximum in the window of size k.
+Use heqp will be O(NlogN)
+Use TreeMap will be O(NlogK)
+Use deque will be O(N)
+Done. (If not done, continue read)
 
+
+Prepare
+How about google "sliding window maximum",
+and make sure you understand 239. Sliding Window Maximum
+Done. (If not done, continue read)
+
+
+Explanation
+Update res[i],
+where res[i] means the maximum result you can get if the last element is A[i].
+
+I directly modify on the input A,
+if you don't like it,
+use a copy of A
+
+Keep a decreasing deque q,
+deque[0] is the maximum result in the last element of result.
+
+If deque[0] > 0. we add it to A[i]
+
+In the end, we return the maximum res.
+
+
+Complexity
+Because all element are pushed and popped at most once.
+Time O(N)
+
+Because at most O(K) elements in the deque.
+Space O(K)
+
+
+More
+Deque can be widely used in all bfs problems.
+For more complecated usage,
+I recall this problem
+862. Shortest Subarray with Sum at Least K
+
+ Transition: A[i] = max(0, A[i - k], A[i - k + 1], .., A[i - 1]) + A[i]. (@lee215 modifies the input A directly)
+        Translated into a traditional dp: dp[i] = max(0, dp[i - k], dp[i - k + 1], .., dp[i -1]) + A[i]
+        dp[i] is the max sum we can have from A[:i] when A[i] has been chosen.
+        """
+        # `deque` stores dp[i - k], dp[i-k+1], .., dp[i - 1] whose values are larger than 0 in a decreasing order
+        # Note that the length of `deque` is not necessarily `k`. The values smaller than dp[i-1] will be discarded.
+ 1. We always want to retrieve the max of (0, dp[i - k], dp[i-k+1], .., dp[i - 1]) from `deque`
+            # 2. We expect dp[i] to be added to `deque` so that we can compute dp[i + 1] in the next iteration
+            # 3. So, if dp[i] is larger than some old values, we can discard them safely.
+            # 4. As a result, the length of `deque` is not necessarily `k`
+
+we do not need the value of A[i - k] when computing dp[i+1] in the next iteration, because `j - i <= k` has to be satisfied.
+*/
 class Solution {
 public:
-    int constrainedSubsetSum(vector<int>& nums, int k) {
-
+    int constrainedSubsetSum(vector<int>& A, int k) {
+        deque<int> q;
+        int res = A[0];
+        for (int i = 0; i < A.size(); ++i) {
+            A[i] += q.size() ? q.front() : 0;
+            res = max(res, A[i]);
+            while (q.size() && A[i] > q.back())
+                q.pop_back();
+            if (A[i] > 0)
+                q.push_back(A[i]);
+            if (i >= k && q.size() && q.front() == A[i - k])
+                q.pop_front();
+        }
+        return res;
     }
 };
