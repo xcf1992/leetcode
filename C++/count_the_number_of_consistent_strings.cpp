80d3f721881610554242ccd0cd764b834329994d
chenfu.xie <chenfu.xie@bytedance.com>
2020-12-24 10:20:57 -0800

    1684. Count the Number of Consistent Strings

---
+++ C++/count_the_number_of_consistent_strings.cpp
@@ -0,0 +1,70 @@
+/*
+1684. Count the Number of Consistent Strings
+https://leetcode.com/problems/count-the-number-of-consistent-strings/
+
+You are given a string allowed consisting of distinct characters
+and an array of strings words.
+A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+Example 1:
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+
+Example 2:
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+Explanation: All strings are consistent.
+
+Example 3:
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+
+Constraints:
+1 <= words.length <= 104
+1 <= allowed.length <= 26
+1 <= words[i].length <= 10
+The characters in allowed are distinct.
+words[i] and allowed contain only lowercase English letters.
+*/
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        vector<int> letter(26, 0);
+        for (char c : allowed) {
+            letter[c - 'a'] = 1;
+        }
+
+        int result = 0;
+        for (string& w : words) {
+            bool allAllowed = 1;
+            for (char c : w) {
+                if (letter[c - 'a'] == 0) {
+                    allAllowed = 0;
+                    break;
+                }
+            }
+            result += allAllowed;
+        }
+        return result;
+    }
+};
