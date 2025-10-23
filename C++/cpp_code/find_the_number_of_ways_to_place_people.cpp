c45d8c7247f925a10cfcd0f81887581e7a82ee61
chenfu.xie <chenfu.xie@bytedance.com>
2025-10-22 21:01:13 -0700

    3025. Find the Number of Ways to Place People I

---
+++ C++/cpp_code/find_the_number_of_ways_to_place_people.cpp
@@ -0,0 +1,96 @@
+/*
+https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/description/
+3025. Find the Number of Ways to Place People I
+
+You are given a 2D array points of size n x 2 representing integer coordinates of some points on a 2D plane, where points[i] = [xi, yi].
+
+Count the number of pairs of points (A, B), where
+
+A is on the upper left side of B, and
+there are no other points in the rectangle (or line) they make (including the border), except for the points A and B.
+Return the count.
+
+
+
+Example 1:
+
+Input: points = [[1,1],[2,2],[3,3]]
+
+Output: 0
+
+Explanation:
+
+
+
+There is no way to choose A and B such that A is on the upper left side of B.
+
+Example 2:
+
+Input: points = [[6,2],[4,4],[2,6]]
+
+Output: 2
+
+Explanation:
+
+
+
+The left one is the pair (points[1], points[0]), where points[1] is on the upper left side of points[0] and the rectangle is empty.
+The middle one is the pair (points[2], points[1]), same as the left one it is a valid pair.
+The right one is the pair (points[2], points[0]), where points[2] is on the upper left side of points[0], but points[1] is inside the rectangle so it's not a valid pair.
+Example 3:
+
+Input: points = [[3,1],[1,3],[1,1]]
+
+Output: 2
+
+Explanation:
+
+
+
+The left one is the pair (points[2], points[0]), where points[2] is on the upper left side of points[0] and there are no other points on the line they form. Note that it is a valid state when the two points form a line.
+The middle one is the pair (points[1], points[2]), it is a valid pair same as the left one.
+The right one is the pair (points[1], points[0]), it is not a valid pair as points[2] is on the border of the rectangle.
+
+
+Constraints:
+
+2 <= n <= 50
+points[i].length == 2
+0 <= points[i][0], points[i][1] <= 50
+All points[i] are distinct.
+*/
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b) {
+            return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
+        });
+
+        int n = points.size();
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int cur_min = -1;
+            for (int j = i + 1; j < n; j++) {
+                if (points[j][1] > cur_min && points[j][1] <= points[i][1]) {
+                    res += 1;
+                    cur_min = points[j][1];
+                }
+            }
+        }
+        return res;
+    }
+};
