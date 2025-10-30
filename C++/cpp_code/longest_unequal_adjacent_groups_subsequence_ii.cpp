380339e2d03dfb8eb75279677bd8124df406c7b1
chenfu.xie <chenfu.xie@bytedance.com>
2025-10-30 16:12:35 -0700

    2901. Longest Unequal Adjacent Groups Subsequence II

--- C++/cpp_code/longest_unequal_adjacent_groups_subsequence_ii.cpp
+++ C++/cpp_code/longest_unequal_adjacent_groups_subsequence_ii.cpp
@@ -1,27 +1,66 @@
 /*
-1086. High Five
-Given a list of scores of different students,
-return the average score of each student's top five scores in the order of each student's id.
+https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-ii/
+2901. Longest Unequal Adjacent Groups Subsequence II
 
-Each entry items[i] has items[i][0] the student's id, and items[i][1] the student's score.
-The average score is calculated using integer division.
+You are given a string array words, and an array groups, both arrays having length n.
+
+The hamming distance between two strings of equal length is the number of positions at which the corresponding characters are different.
+
+You need to select the longest subsequence from an array of indices [0, 1, ..., n - 1], such that for the subsequence denoted as [i0, i1, ..., ik-1] having length k, the following holds:
+
+For adjacent indices in the subsequence, their corresponding groups are unequal, i.e., groups[ij] != groups[ij+1], for each j where 0 < j + 1 < k.
+words[ij] and words[ij+1] are equal in length, and the hamming distance between them is 1, where 0 < j + 1 < k, for all indices in the subsequence.
+Return a string array containing the words corresponding to the indices (in order) in the selected subsequence. If there are multiple answers, return any of them.
+
+Note: strings in words may be unequal in length.
+
+
 
 Example 1:
 
-Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
-Output: [[1,87],[2,88]]
-Explanation:
-The average of the student with id = 1 is 87.
-The average of the student with id = 2 is 88.6. But with integer division their average converts to 88.
+Input: words = ["bab","dab","cab"], groups = [1,2,2]
+
+Output: ["bab","cab"]
+
+Explanation: A subsequence that can be selected is [0,2].
+
+groups[0] != groups[2]
+words[0].length == words[2].length, and the hamming distance between them is 1.
+So, a valid answer is [words[0],words[2]] = ["bab","cab"].
+
+Another subsequence that can be selected is [0,1].
+
+groups[0] != groups[1]
+words[0].length == words[1].length, and the hamming distance between them is 1.
+So, another valid answer is [words[0],words[1]] = ["bab","dab"].
+
+It can be shown that the length of the longest subsequence of indices that satisfies the conditions is 2.
+
+Example 2:
+
+Input: words = ["a","b","c","d"], groups = [1,2,3,4]
+
+Output: ["a","b","c","d"]
+
+Explanation: We can select the subsequence [0,1,2,3].
+
+It satisfies both conditions.
+
+Hence, the answer is [words[0],words[1],words[2],words[3]] = ["a","b","c","d"].
+
+It has the longest length among all subsequences of indices that satisfy the conditions.
+
+Hence, it is the only answer.
 
 
-Note:
 
-1 <= items.length <= 1000
-items[i].length == 2
-The IDs of the students is between 1 to 1000
-The score of the students is between 1 to 100
-For each student, there are at least 5 scores
+Constraints:
+
+1 <= n == words.length == groups.length <= 1000
+1 <= words[i].length <= 10
+1 <= groups[i] <= n
+words consists of distinct strings.
+words[i] consists of lowercase English letters.
 */
 #include <iostream>
 #include <string>
@@ -39,30 +78,59 @@ For each student, there are at least 5 scores
 using namespace std;
 
 class Solution {
-public:
-    vector<vector<int>> highFive(vector<vector<int>>& items) {
-        map<int, vector<int>> count;
-        for (vector<int>& item : items) {
-            int id = item[0];
-            int score = item[1];
-            if (count.find(id) == count.end()) {
-                count[id] = vector<int>(101, 0);
-            }
-            count[id][score] += 1;
+private:
+    bool adjacent(vector<string>& words, vector<int>& groups, int i, int j) {
+        if (groups[i] == groups[j]) {
+            return false;
         }
 
-        vector<vector<int>> result;
-        for (auto& it : count) {
-            int id = it.first;
-            vector<int> scores = it.second;
-            int number = 0;
-            int sum = 0;
-            for (int i = 100; i >= 1 and number < 5; --i) if (scores[i] != 0) {
-                sum += i * min(5 - number, scores[i]);
-                number += min(5 - number, scores[i]);
-            }
-            result.push_back({id, sum / 5});
+        if (words[i].size() != words[j].size()) {
+            return false;
         }
+
+        int distance = 0;
+        for (int idx = 0; idx < words[i].size(); idx++) {
+            if (words[i][idx] != words[j][idx]) {
+                distance += 1;
+            }
+        }
+        return distance == 1;
+    }
+public:
+    vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        int n = words.size();
+        if (n <= 1) {
+            return words;
+        }
+
+        vector<int> dp(n, 1);
+        int max_len = 1;
+        vector<int> parent(n, -1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (!adjacent(words, groups, i, j)) {
+                    continue;
+                }
+
+                if (dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    parent[i] = j;
+                }
+            }
+            max_len = max(max_len, dp[i]);
+        }
+
+        vector<string> result;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == max_len) {
+                while (i != -1) {
+                    result.emplace_back(words[i]);
+                    i = parent[i];
+                }
+                break;
+            }
+        }
+        reverse(result.begin(), result.end());
         return result;
     }
 };
