5feb6868e8e34e0a02615ca2a1e05842eb56a70e
chenfu.xie <chenfu.xie@bytedance.com>
2025-10-30 15:43:31 -0700

    2900. Longest Unequal Adjacent Groups Subsequence I

---
+++ C++/cpp_code/longest_unequal_adjacent_groups_subsequence_i.cpp
@@ -0,0 +1,74 @@
+/*
+https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i/description/
+2900. Longest Unequal Adjacent Groups Subsequence I
+
+You are given a string array words and a binary array groups both of length n.
+
+A subsequence of words is alternating if for any two consecutive strings in the sequence, their corresponding elements at the same indices in groups are different (that is, there cannot be consecutive 0 or 1).
+
+Your task is to select the longest alternating subsequence from words.
+
+Return the selected subsequence. If there are multiple answers, return any of them.
+
+Note: The elements in words are distinct.
+
+
+
+Example 1:
+
+Input: words = ["e","a","b"], groups = [0,0,1]
+
+Output: ["e","b"]
+
+Explanation: A subsequence that can be selected is ["e","b"] because groups[0] != groups[2]. Another subsequence that can be selected is ["a","b"] because groups[1] != groups[2]. It can be demonstrated that the length of the longest subsequence of indices that satisfies the condition is 2.
+
+Example 2:
+
+Input: words = ["a","b","c","d"], groups = [1,0,1,1]
+
+Output: ["a","b","c"]
+
+Explanation: A subsequence that can be selected is ["a","b","c"] because groups[0] != groups[1] and groups[1] != groups[2]. Another subsequence that can be selected is ["a","b","d"] because groups[0] != groups[1] and groups[1] != groups[3]. It can be shown that the length of the longest subsequence of indices that satisfies the condition is 3.
+
+
+
+Constraints:
+
+1 <= n == words.length == groups.length <= 100
+1 <= words[i].length <= 10
+groups[i] is either 0 or 1.
+words consists of distinct strings.
+words[i] consists of lowercase English letters.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        vector<int> ans;
+        for (int i = 0; i < words.size(); i++) {
+            if (ans.empty() || groups[ans.back()] != groups[i]) {
+                ans.push_back(i);
+            }
+        }
+
+        vector<string> res;
+        for (int i : ans) {
+            res.push_back(words[i]);
+        }
+        return res;
+    }
+};
---
+++ C++/cpp_code/longest_unequal_adjacent_groups_subsequence_ii.cpp
@@ -0,0 +1,68 @@
+/*
+1086. High Five
+Given a list of scores of different students,
+return the average score of each student's top five scores in the order of each student's id.
+
+Each entry items[i] has items[i][0] the student's id, and items[i][1] the student's score.
+The average score is calculated using integer division.
+
+Example 1:
+
+Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+Output: [[1,87],[2,88]]
+Explanation:
+The average of the student with id = 1 is 87.
+The average of the student with id = 2 is 88.6. But with integer division their average converts to 88.
+
+
+Note:
+
+1 <= items.length <= 1000
+items[i].length == 2
+The IDs of the students is between 1 to 1000
+The score of the students is between 1 to 100
+For each student, there are at least 5 scores
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> highFive(vector<vector<int>>& items) {
+        map<int, vector<int>> count;
+        for (vector<int>& item : items) {
+            int id = item[0];
+            int score = item[1];
+            if (count.find(id) == count.end()) {
+                count[id] = vector<int>(101, 0);
+            }
+            count[id][score] += 1;
+        }
+
+        vector<vector<int>> result;
+        for (auto& it : count) {
+            int id = it.first;
+            vector<int> scores = it.second;
+            int number = 0;
+            int sum = 0;
+            for (int i = 100; i >= 1 and number < 5; --i) if (scores[i] != 0) {
+                sum += i * min(5 - number, scores[i]);
+                number += min(5 - number, scores[i]);
+            }
+            result.push_back({id, sum / 5});
+        }
+        return result;
+    }
+};
