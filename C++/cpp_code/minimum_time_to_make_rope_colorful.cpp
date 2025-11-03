2dbf7c3955f4745a8d491efcce5dd45d335db32c
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-02 21:21:38 -0800

    1578. Minimum Time to Make Rope Colorful

---
+++ C++/cpp_code/minimum_time_to_make_rope_colorful.cpp
@@ -0,0 +1,78 @@
+/*
+https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
+1578. Minimum Time to Make Rope Colorful
+
+Alice has n balloons arranged on a rope. You are given a 0-indexed string colors where colors[i] is the color of the ith balloon.
+
+Alice wants the rope to be colorful. She does not want two consecutive balloons to be of the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful. You are given a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that Bob needs to remove the ith balloon from the rope.
+
+Return the minimum time Bob needs to make the rope colorful.
+
+
+
+Example 1:
+
+
+Input: colors = "abaac", neededTime = [1,2,3,4,5]
+Output: 3
+Explanation: In the above image, 'a' is blue, 'b' is red, and 'c' is green.
+Bob can remove the blue balloon at index 2. This takes 3 seconds.
+There are no longer two consecutive balloons of the same color. Total time = 3.
+Example 2:
+
+
+Input: colors = "abc", neededTime = [1,2,3]
+Output: 0
+Explanation: The rope is already colorful. Bob does not need to remove any balloons from the rope.
+Example 3:
+
+
+Input: colors = "aabaa", neededTime = [1,2,3,4,1]
+Output: 2
+Explanation: Bob will remove the balloons at indices 0 and 4. Each balloons takes 1 second to remove.
+There are no longer two consecutive balloons of the same color. Total time = 1 + 1 = 2.
+
+
+Constraints:
+
+n == colors.length == neededTime.length
+1 <= n <= 105
+1 <= neededTime[i] <= 104
+colors contains only lowercase English letters.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    int minCost(string colors, vector<int> &neededTime) {
+        int n = colors.size();
+        if (n <= 1) {
+            return 0;
+        }
+
+        int total_time = 0;
+        int max_time = 0;
+        int i = 0;
+        while (i < n) {
+            total_time += neededTime[i];
+            int cur_max = neededTime[i];
+            int j = i + 1;
+            for (; j < n; j++) {
+                if (colors[i] != colors[j]) {
+                    break;
+                }
+                total_time += neededTime[j];
+                cur_max = max(neededTime[j], cur_max);
+            }
+            max_time += cur_max;
+            i = j;
+        }
+        return total_time == max_time ? 0 : total_time - max_time;
+    }
+};
