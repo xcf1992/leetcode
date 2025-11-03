687551595eba2d33b3f134b72536a3b2c8567bd5
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-02 21:44:05 -0800

    2929. Distribute Candies Among Children II

---
+++ C++/cpp_code/distribute_candies_among_children_ii.cpp
@@ -0,0 +1,55 @@
+/*
+https://leetcode.com/problems/distribute-candies-among-children-ii/description/
+2929. Distribute Candies Among Children II
+
+You are given two positive integers n and limit.
+
+Return the total number of ways to distribute n candies among 3 children such that no child gets more than limit candies.
+
+
+
+Example 1:
+
+Input: n = 5, limit = 2
+Output: 3
+Explanation: There are 3 ways to distribute 5 candies such that no child gets more than 2 candies: (1, 2, 2), (2, 1, 2) and (2, 2, 1).
+Example 2:
+
+Input: n = 3, limit = 3
+Output: 10
+Explanation: There are 10 ways to distribute 3 candies such that no child gets more than 3 candies: (0, 0, 3), (0, 1, 2), (0, 2, 1), (0, 3, 0), (1, 0, 2), (1, 1, 1), (1, 2, 0), (2, 0, 1), (2, 1, 0) and (3, 0, 0).
+
+
+Constraints:
+
+1 <= n <= 106
+1 <= limit <= 106
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        long long ans = 0;
+        for (int i = 0; i <= min(limit, n); i++) {
+            if (n - i > 2 * limit) {
+                continue;
+            }
+            ans += min(n - i, limit) - max(0, n - i - limit) + 1;
+        }
+        return ans;
+    }
+};
