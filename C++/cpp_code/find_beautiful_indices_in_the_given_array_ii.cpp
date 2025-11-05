9495f05e20e6ef24ee0164084c97106c95ceb53d
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-05 12:39:28 -0800

    3008. Find Beautiful Indices in the Given Array II

---
+++ C++/cpp_code/find_beautiful_indices_in_the_given_array_ii.cpp
@@ -0,0 +1,102 @@
+/*
+https://leetcode.com/problems/find-beautiful-indices-in-the-given-array-ii/description
+3008. Find Beautiful Indices in the Given Array I
+
+You are given a 0-indexed string s, a string a, a string b, and an integer k.
+
+An index i is beautiful if:
+
+0 <= i <= s.length - a.length
+s[i..(i + a.length - 1)] == a
+There exists an index j such that:
+0 <= j <= s.length - b.length
+s[j..(j + b.length - 1)] == b
+|j - i| <= k
+Return the array that contains beautiful indices in sorted order from smallest to largest.
+
+
+
+Example 1:
+
+Input: s = "isawsquirrelnearmysquirrelhouseohmy", a = "my", b = "squirrel", k = 15
+Output: [16,33]
+Explanation: There are 2 beautiful indices: [16,33].
+- The index 16 is beautiful as s[16..17] == "my" and there exists an index 4 with s[4..11] == "squirrel" and |16 - 4| <= 15.
+- The index 33 is beautiful as s[33..34] == "my" and there exists an index 18 with s[18..25] == "squirrel" and |33 - 18| <= 15.
+Thus we return [16,33] as the result.
+Example 2:
+
+Input: s = "abcd", a = "a", b = "a", k = 4
+Output: [0]
+Explanation: There is 1 beautiful index: [0].
+- The index 0 is beautiful as s[0..0] == "a" and there exists an index 0 with s[0..0] == "a" and |0 - 0| <= 4.
+Thus we return [0] as the result.
+
+
+Constraints:
+
+1 <= k <= s.length <= 105
+1 <= a.length, b.length <= 10
+s, a, and b contain only lowercase English letters.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    void kmp_matching(string &str, string &pattern, vector<int> &v) {
+        string t = pattern + "@" + str;
+        vector<int> lps(1, 0);
+        for (int i = 1; i < t.size(); ++i) {
+            int len = lps[i - 1];
+            while (len > 0 && t[len] != t[i]) {
+                len = lps[len - 1];
+            }
+            lps.push_back((t[len] == t[i]) ? len + 1 : 0);
+        }
+
+        for (int i = 0; i < lps.size(); ++i) {
+            if (lps[i] == pattern.size()) {
+                v.push_back(i - 2 * pattern.size());
+            }
+        }
+    }
+
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        vector<int> index_a, index_b;
+        kmp_matching(s, a, index_a);
+        kmp_matching(s, b, index_b);
+
+        vector<int> rst;
+        int i = 0;
+        int j = 0;
+        while (i < index_a.size() && j < index_b.size()) {
+            int idx_a = index_a[i];
+            int idx_b = index_b[j];
+            if (abs(idx_a - idx_b) <= k) {
+                rst.push_back(idx_a);
+                i += 1;
+                continue;
+            }
+
+            if (idx_a < idx_b) {
+                i += 1;
+            } else {
+                j += 1;
+            }
+        }
+        return rst;
+    }
+};
