86fa73a34875807cb87b9a1968506fc845116647
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-04 17:56:43 -0800

    2325. Decode the Message

---
+++ C++/cpp_code/decode_the_message.cpp
@@ -0,0 +1,77 @@
+/*
+https://leetcode.com/problems/decode-the-message/description/
+2325. Decode the Message
+
+You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
+
+Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
+Align the substitution table with the regular English alphabet.
+Each letter in message is then substituted using the table.
+Spaces ' ' are transformed to themselves.
+For example, given key = "happy boy" (actual key would have at least one instance of each letter in the alphabet), we have the partial substitution table of ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
+Return the decoded message.
+
+
+
+Example 1:
+
+
+Input: key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv"
+Output: "this is a secret"
+Explanation: The diagram above shows the substitution table.
+It is obtained by taking the first appearance of each letter in "the quick brown fox jumps over the lazy dog".
+Example 2:
+
+
+Input: key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
+Output: "the five boxing wizards jump quickly"
+Explanation: The diagram above shows the substitution table.
+It is obtained by taking the first appearance of each letter in "eljuxhpwnyrdgtqkviszcfmabo".
+
+
+Constraints:
+
+26 <= key.length <= 2000
+key consists of lowercase English letters and ' '.
+key contains every letter in the English alphabet ('a' to 'z') at least once.
+1 <= message.length <= 2000
+message consists of lowercase English letters and ' '.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        vector<int> decode(26, -1);
+        int ori = 0;
+        for (char c : key) {
+            if (c == ' ' || decode[c - 'a'] != -1) {
+                continue;
+            }
+
+            decode[c - 'a'] = ori;
+            ori += 1;
+        }
+
+        string rst = message;
+        for (int i = 0; i < message.size(); i++) {
+            if (message[i] != ' ') {
+                rst[i] = 'a' + decode[message[i] - 'a'];
+            }
+        }
+        return rst;
+    }
+};
