fe83cf1de22c32554aa7883eeddc744628bb1845
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-15 20:58:41 -0800

    2438. Range Product Queries of Powers

---
+++ C++/cpp_code/range_product_queriest_of_powers.cpp
@@ -0,0 +1,86 @@
+/*
+https://leetcode.com/problems/range-product-queries-of-powers/description/
+2438. Range Product Queries of Powers
+
+Given a positive integer n, there exists a 0-indexed array called powers, composed of the minimum number of powers of 2
+that sum to n. The array is sorted in non-decreasing order, and there is only one way to form the array.
+
+You are also given a 0-indexed 2D integer array queries, where queries[i] = [lefti, righti]. Each queries[i] represents
+a query where you have to find the product of all powers[j] with lefti <= j <= righti.
+
+Return an array answers, equal in length to queries, where answers[i] is the answer to the ith query. Since the answer
+to the ith query may be too large, each answers[i] should be returned modulo 109 + 7.
+
+
+
+Example 1:
+
+Input: n = 15, queries = [[0,1],[2,2],[0,3]]
+Output: [2,4,64]
+Explanation:
+For n = 15, powers = [1,2,4,8]. It can be shown that powers cannot be a smaller size.
+Answer to 1st query: powers[0] * powers[1] = 1 * 2 = 2.
+Answer to 2nd query: powers[2] = 4.
+Answer to 3rd query: powers[0] * powers[1] * powers[2] * powers[3] = 1 * 2 * 4 * 8 = 64.
+Each answer modulo 109 + 7 yields the same answer, so [2,4,64] is returned.
+Example 2:
+
+Input: n = 2, queries = [[0,0]]
+Output: [2]
+Explanation:
+For n = 2, powers = [2].
+The answer to the only query is powers[0] = 2. The answer modulo 109 + 7 is the same, so [2] is returned.
+
+
+Constraints:
+
+1 <= n <= 109
+1 <= queries.length <= 105
+0 <= starti <= endi < powers.length
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> bins;
+        int rep = 1;
+        while (n) {
+            if (n % 2 == 1) {
+                bins.push_back(rep);
+            }
+            n /= 2;
+            rep *= 2;
+        }
+
+        int m = bins.size();
+        vector<vector<int>> results(m, vector<int>(m));
+        for (int i = 0; i < m; ++i) {
+            int cur = 1;
+            for (int j = i; j < m; ++j) {
+                cur = static_cast<long long>(cur) * bins[j] % mod;
+                results[i][j] = cur;
+            }
+        }
+
+        vector<int> ans;
+        for (const auto& query : queries) {
+            ans.push_back(results[query[0]][query[1]]);
+        }
+        return ans;
+    }
+
+private:
+    static constexpr int mod = 1000000007;
+};
