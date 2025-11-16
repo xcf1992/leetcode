14fd395c00a244a2ce95461d52be8a4942ef8ddf
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-15 20:54:49 -0800

    2016. Maximum Difference Between Increasing Elements

---
+++ C++/cpp_code/maximum_difference_between_increasing_elements.cpp
@@ -0,0 +1,66 @@
+/*
+https://leetcode.com/problems/maximum-difference-between-increasing-elements/description/
+2016. Maximum Difference Between Increasing Elements
+
+Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] -
+nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+
+Return the maximum difference. If no such i and j exists, return -1.
+
+
+
+Example 1:
+
+Input: nums = [7,1,5,4]
+Output: 4
+Explanation:
+The maximum difference occurs with i = 1 and j = 2, nums[j] - nums[i] = 5 - 1 = 4.
+Note that with i = 1 and j = 0, the difference nums[j] - nums[i] = 7 - 1 = 6, but i > j, so it is not valid.
+Example 2:
+
+Input: nums = [9,4,3,2]
+Output: -1
+Explanation:
+There is no i and j such that i < j and nums[i] < nums[j].
+Example 3:
+
+Input: nums = [1,5,2,10]
+Output: 9
+Explanation:
+The maximum difference occurs with i = 0 and j = 3, nums[j] - nums[i] = 10 - 1 = 9.
+
+
+Constraints:
+
+n == nums.length
+2 <= n <= 1000
+1 <= nums[i] <= 109
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int n = nums.size();
+        int rst = -1;
+        int max_val = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (max_val > nums[i]) {
+                rst = max(rst, max_val - nums[i]);
+            }
+            max_val = max(max_val, nums[i]);
+        }
+        return rst;
+    }
+};
