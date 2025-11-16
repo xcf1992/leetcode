53fd2f274f8ec30681c12b9a60f9b745c166e4d6
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-16 15:39:42 -0800

    2749. Minimum Operations to Make the Integer Zero

---
+++ C++/cpp_code/minimum_operations_to_make_the_integer_zero.cpp
@@ -0,0 +1,65 @@
+/*
+https://leetcode.com/problems/minimum-operations-to-make-the-integer-zero/description/
+2749. Minimum Operations to Make the Integer Zero
+
+You are given two integers num1 and num2.
+
+In one operation, you can choose integer i in the range [0, 60] and subtract 2i + num2 from num1.
+
+Return the integer denoting the minimum number of operations needed to make num1 equal to 0.
+
+If it is impossible to make num1 equal to 0, return -1.
+
+
+
+Example 1:
+
+Input: num1 = 3, num2 = -2
+Output: 3
+Explanation: We can make 3 equal to 0 with the following operations:
+- We choose i = 2 and subtract 22 + (-2) from 3, 3 - (4 + (-2)) = 1.
+- We choose i = 2 and subtract 22 + (-2) from 1, 1 - (4 + (-2)) = -1.
+- We choose i = 0 and subtract 20 + (-2) from -1, (-1) - (1 + (-2)) = 0.
+It can be proven, that 3 is the minimum number of operations that we need to perform.
+Example 2:
+
+Input: num1 = 5, num2 = 7
+Output: -1
+Explanation: It can be proven, that it is impossible to make 5 equal to 0 with the given operation.
+
+
+Constraints:
+
+1 <= num1 <= 109
+-109 <= num2 <= 109
+*/
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        for (int k = 1; k <= 60; k++) {
+            long long x = num1 - 1LL * num2 * k;
+            if (x < k) {
+                return -1;
+            }
+            if (k >= __builtin_popcountll(x)) {
+                return k;
+            }
+        }
+        return -1;
+    }
+};
