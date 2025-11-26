81c3262cbde4e2c536dc1dfc8a838400b3dbe696
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-25 20:44:17 -0800

    2939. Maximum Xor Product

---
+++ C++/cpp_code/maximum_xor_product.cpp
@@ -0,0 +1,74 @@
+/*
+https://leetcode.com/problems/maximum-xor-product/description/
+2939. Maximum Xor Product
+
+Given three integers a, b, and n, return the maximum value of (a XOR x) * (b XOR x) where 0 <= x < 2n.
+
+Since the answer may be too large, return it modulo 109 + 7.
+
+Note that XOR is the bitwise XOR operation.
+
+
+
+Example 1:
+
+Input: a = 12, b = 5, n = 4
+Output: 98
+Explanation: For x = 2, (a XOR x) = 14 and (b XOR x) = 7. Hence, (a XOR x) * (b XOR x) = 98.
+It can be shown that 98 is the maximum value of (a XOR x) * (b XOR x) for all 0 <= x < 2n.
+Example 2:
+
+Input: a = 6, b = 7 , n = 5
+Output: 930
+Explanation: For x = 25, (a XOR x) = 31 and (b XOR x) = 30. Hence, (a XOR x) * (b XOR x) = 930.
+It can be shown that 930 is the maximum value of (a XOR x) * (b XOR x) for all 0 <= x < 2n.
+Example 3:
+
+Input: a = 1, b = 6, n = 3
+Output: 12
+Explanation: For x = 5, (a XOR x) = 4 and (b XOR x) = 3. Hence, (a XOR x) * (b XOR x) = 12.
+It can be shown that 12 is the maximum value of (a XOR x) * (b XOR x) for all 0 <= x < 2n.
+
+
+Constraints:
+
+0 <= a, b < 250
+0 <= n <= 50
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    int maximumXorProduct(long long a, long long b, int n) {
+        long long mod = 1e9 + 7;
+        long long rst = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            long long bit_a = (a >> i) & 1;
+            long long bit_b = (b >> i) & 1;
+
+            if (bit_a == bit_b) {
+                rst |= (1 ^ bit_a) << i;
+                continue;
+            }
+
+            if ((rst ^ a) > (rst ^ b)) {
+                rst |= (1 ^ bit_b) << i;
+            } else {
+                rst |= (1 ^ bit_a) << i;
+            }
+        }
+
+        return (((rst ^ a) % mod) * ((rst ^ b) % mod)) % mod;
+    }
+};
