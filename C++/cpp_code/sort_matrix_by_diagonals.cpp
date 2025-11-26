1bdca90479e79793c5e8b5cd3cc6166a4e4cdab3
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-25 20:58:04 -0800

    3446. Sort Matrix by Diagonals

---
+++ C++/cpp_code/sort_matrix_by_diagonals.cpp
@@ -0,0 +1,104 @@
+/*
+https://leetcode.com/problems/sort-matrix-by-diagonals/description/
+3446. Sort Matrix by Diagonals
+
+You are given an n x n square matrix of integers grid. Return the matrix such that:
+
+The diagonals in the bottom-left triangle (including the middle diagonal) are sorted in non-increasing order.
+The diagonals in the top-right triangle are sorted in non-decreasing order.
+
+
+Example 1:
+
+Input: grid = [[1,7,3],[9,8,2],[4,5,6]]
+
+Output: [[8,2,3],[9,6,7],[4,5,1]]
+
+Explanation:
+
+
+
+The diagonals with a black arrow (bottom-left triangle) should be sorted in non-increasing order:
+
+[1, 8, 6] becomes [8, 6, 1].
+[9, 5] and [4] remain unchanged.
+The diagonals with a blue arrow (top-right triangle) should be sorted in non-decreasing order:
+
+[7, 2] becomes [2, 7].
+[3] remains unchanged.
+Example 2:
+
+Input: grid = [[0,1],[1,2]]
+
+Output: [[2,1],[1,0]]
+
+Explanation:
+
+
+
+The diagonals with a black arrow must be non-increasing, so [0, 2] is changed to [2, 0]. The other diagonals are already
+in the correct order.
+
+Example 3:
+
+Input: grid = [[1]]
+
+Output: [[1]]
+
+Explanation:
+
+Diagonals with exactly one element are already in order, so no changes are needed.
+
+
+
+Constraints:
+
+grid.length == grid[i].length == n
+1 <= n <= 10
+-105 <= grid[i][j] <= 105
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        unordered_map<int, priority_queue<int>> max_heaps;
+        unordered_map<int, priority_queue<int, vector<int>, std::greater<int>>> min_heaps;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int key = i - j;
+                if (key < 0) {
+                    min_heaps[key].push(grid[i][j]);
+                } else {
+                    max_heaps[key].push(grid[i][j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int key = i - j;
+                if (key < 0) {
+                    grid[i][j] = min_heaps[key].top();
+                    min_heaps[key].pop();
+                } else {
+                    grid[i][j] = max_heaps[key].top();
+                    max_heaps[key].pop();
+                }
+            }
+        }
+        return grid;
+    }
+};
