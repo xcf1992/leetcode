eab9080f302d5fc1ef505b0e6c77ba1af4b5c765
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-25 20:49:38 -0800

    3350. Adjacent Increasing Subarrays Detection II

---
+++ C++/cpp_code/adjacent_increasing_subarrays_detection_ii.cpp
@@ -0,0 +1,82 @@
+/*
+https://leetcode.com/problems/adjacent-increasing-subarrays-detection-ii/description/
+3350. Adjacent Increasing Subarrays Detection II
+
+Given an array nums of n integers, your task is to find the maximum value of k for which there exist two adjacent
+subarrays of length k each, such that both subarrays are strictly increasing. Specifically, check if there are two
+subarrays of length k starting at indices a and b (a < b), where:
+
+Both subarrays nums[a..a + k - 1] and nums[b..b + k - 1] are strictly increasing.
+The subarrays must be adjacent, meaning b = a + k.
+Return the maximum possible value of k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+
+Example 1:
+
+Input: nums = [2,5,7,8,9,2,3,4,3,1]
+
+Output: 3
+
+Explanation:
+
+The subarray starting at index 2 is [7, 8, 9], which is strictly increasing.
+The subarray starting at index 5 is [2, 3, 4], which is also strictly increasing.
+These two subarrays are adjacent, and 3 is the maximum possible value of k for which two such adjacent strictly
+increasing subarrays exist. Example 2:
+
+Input: nums = [1,2,3,4,4,4,4,5,6,7]
+
+Output: 2
+
+Explanation:
+
+The subarray starting at index 0 is [1, 2], which is strictly increasing.
+The subarray starting at index 2 is [3, 4], which is also strictly increasing.
+These two subarrays are adjacent, and 2 is the maximum possible value of k for which two such adjacent strictly
+increasing subarrays exist.
+
+
+Constraints:
+
+2 <= nums.length <= 2 * 105
+-109 <= nums[i] <= 109
+*/
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int increasing_len = 1;
+        int prev_increasing_len = 0;
+        int rst = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                increasing_len += 1;
+            } else {
+                prev_increasing_len = increasing_len;
+                increasing_len = 1;
+            }
+
+            rst = max(rst, increasing_len / 2);
+            rst = max(rst, min(increasing_len, prev_increasing_len));
+        }
+        return rst;
+    }
+};
