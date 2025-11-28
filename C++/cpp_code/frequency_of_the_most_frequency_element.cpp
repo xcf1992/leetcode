5298215dc993f4be81f8a9314fd0fbc6d606f40c
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-28 14:54:57 -0800

    2294. Partition Array Such That Maximum Difference Is K

---
+++ C++/cpp_code/frequency_of_the_most_frequency_element.cpp
@@ -0,0 +1,73 @@
+/*
+https://leetcode.com/problems/frequency-of-the-most-frequent-element/description/
+1838. Frequency of the Most Frequent Element
+https://leetcode.com/problems/frequency-of-the-most-frequent-element/description
+The frequency of an element is the number of times it occurs in an array.
+
+You are given an integer array nums and an integer k. In one operation, you can choose an index of nums and increment
+the element at that index by 1.
+
+Return the maximum possible frequency of an element after performing at most k operations.
+
+
+
+Example 1:
+
+Input: nums = [1,2,4], k = 5
+Output: 3
+Explanation: Increment the first element three times and the second element two times to make nums = [4,4,4].
+4 has a frequency of 3.
+Example 2:
+
+Input: nums = [1,4,8,13], k = 5
+Output: 2
+Explanation: There are multiple optimal solutions:
+- Increment the first element three times to make nums = [4,4,8,13]. 4 has a frequency of 2.
+- Increment the second element four times to make nums = [1,8,8,13]. 8 has a frequency of 2.
+- Increment the third element five times to make nums = [1,4,13,13]. 13 has a frequency of 2.
+Example 3:
+
+Input: nums = [3,9,6], k = 2
+Output: 1
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
+1 <= k <= 105
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(begin(nums), end(nums));
+        long left = 0;
+        long right = 0;
+        long n = nums.size();
+        long sum = 0;
+        long rst = 0;
+        while (right < n) {
+            sum += nums[right];
+            while ((right - left + 1) * nums[right] - sum > k) {
+                sum -= nums[left];
+                left += 1;
+            }
+            rst = max(rst, right - left + 1);
+            right += 1;
+        }
+        return rst;
+    }
+};
