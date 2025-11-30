42932fb6ecccb1afc1ea3ce4b05bfa9931d5b9ae
chenfu.xie <chenfu.xie@bytedance.com>
2025-11-29 21:25:25 -0800

    3234. Count the Number of Substrings With Dominant Ones

--- C++/cpp_code/count_the_number_of_substrings_with_dominant_ones.cpp
+++ C++/cpp_code/count_the_number_of_substrings_with_dominant_ones.cpp
@@ -63,32 +63,35 @@ using namespace std;
 /*
 https://leetcode.com/problems/count-the-number-of-substrings-with-dominant-ones/solutions/7338477/count-the-number-of-substrings-with-domi-jqas/?envType=daily-question&envId=2025-11-15
  */
+
 class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n = s.size();
-        vector<int> pre(n + 1);
-        pre[0] = -1;
+        vector<int> pre_zero(n + 1);
+        pre_zero[0] = -1;
         for (int i = 0; i < n; i++) {
             if (i == 0 || (i > 0 && s[i - 1] == '0')) {
-                pre[i + 1] = i;
+                pre_zero[i + 1] = i;
             } else {
-                pre[i + 1] = pre[i];
+                pre_zero[i + 1] = pre_zero[i];
             }
         }
-        int res = 0;
-        for (int i = 1; i <= n; i++) {
-            int cnt0 = s[i - 1] == '0';
-            int j = i;
-            while (j > 0 && cnt0 * cnt0 <= n) {
-                int cnt1 = (i - pre[j]) - cnt0;
+
+        int rst = 0;
+        for (int end = 1; end <= n; end++) {
+            int cnt0 = s[end - 1] == '0';
+            int cur_pos = end;
+            while (cur_pos > 0 && cnt0 * cnt0 <= n) {
+                int cnt1 = (end - pre_zero[cur_pos]) - cnt0;
                 if (cnt0 * cnt0 <= cnt1) {
-                    res += min(j - pre[j], cnt1 - cnt0 * cnt0 + 1);
+                    // since not all new sub are valid, we need to add enough 1 to make it valid
+                    rst += min(cur_pos - pre_zero[cur_pos], cnt1 - cnt0 * cnt0 + 1);
                 }
-                j = pre[j];
+                cur_pos = pre_zero[cur_pos];
                 cnt0++;
             }
         }
-        return res;
+        return rst;
     }
 };
