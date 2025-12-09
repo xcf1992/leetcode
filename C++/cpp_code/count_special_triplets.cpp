f49fed6d5c2061e3b544e24f4cbcb5c762f10459
chenfu.xie <chenfu.xie@bytedance.com>
2025-12-09 15:51:10 -0800

    3583. Count Special Triplets

---
+++ C++/cpp_code/count_special_triplets.cpp
@@ -0,0 +1,103 @@
+/*
+https://leetcode.com/problems/count-special-triplets/description/?envType=daily-question&envId=2025-12-09
+3583. Count Special Triplets
+
+You are given an integer array nums.
+
+A special triplet is defined as a triplet of indices (i, j, k) such that:
+
+0 <= i < j < k < n, where n = nums.length
+nums[i] == nums[j] * 2
+nums[k] == nums[j] * 2
+Return the total number of special triplets in the array.
+
+Since the answer may be large, return it modulo 109 + 7.
+
+
+
+Example 1:
+
+Input: nums = [6,3,6]
+
+Output: 1
+
+Explanation:
+
+The only special triplet is (i, j, k) = (0, 1, 2), where:
+
+nums[0] = 6, nums[1] = 3, nums[2] = 6
+nums[0] = nums[1] * 2 = 3 * 2 = 6
+nums[2] = nums[1] * 2 = 3 * 2 = 6
+Example 2:
+
+Input: nums = [0,1,0,0]
+
+Output: 1
+
+Explanation:
+
+The only special triplet is (i, j, k) = (0, 2, 3), where:
+
+nums[0] = 0, nums[2] = 0, nums[3] = 0
+nums[0] = nums[2] * 2 = 0 * 2 = 0
+nums[3] = nums[2] * 2 = 0 * 2 = 0
+Example 3:
+
+Input: nums = [8,4,2,8,4]
+
+Output: 2
+
+Explanation:
+
+There are exactly two special triplets:
+
+(i, j, k) = (0, 1, 3)
+nums[0] = 8, nums[1] = 4, nums[3] = 8
+nums[0] = nums[1] * 2 = 4 * 2 = 8
+nums[3] = nums[1] * 2 = 4 * 2 = 8
+(i, j, k) = (1, 2, 4)
+nums[1] = 4, nums[2] = 2, nums[4] = 4
+nums[1] = nums[2] * 2 = 2 * 2 = 4
+nums[4] = nums[2] * 2 = 2 * 2 = 4
+
+
+Constraints:
+
+3 <= n == nums.length <= 105
+0 <= nums[i] <= 105
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <climits>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        unordered_map<int, int> freq_next;
+        for (const int& num : nums) {
+            freq_next[num] += 1;
+        }
+
+        unordered_map<int, int> freq_prev;
+        long long rst = 0;
+        int mod = 1e9 + 7;
+        for (const int& num : nums) {
+            freq_next[num] -= 1;
+            int tgt_val = num * 2;
+            if (freq_next.find(tgt_val) != freq_next.end() && freq_prev.find(tgt_val) != freq_prev.end()) {
+                rst = (rst + 1LL * freq_next[tgt_val] * freq_prev[tgt_val] % mod) % mod;
+            }
+            freq_prev[num] += 1;
+        }
+        return rst;
+    }
+};
