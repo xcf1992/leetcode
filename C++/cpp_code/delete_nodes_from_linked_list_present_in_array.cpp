a5be4f036c088becd699087e06effe6ef41056d4
chenfu.xie <chenfu.xie@bytedance.com>
2025-12-10 14:45:28 -0800

    3217. Delete Nodes From Linked List Present in Array

---
+++ C++/cpp_code/delete_nodes_from_linked_list_present_in_array.cpp
@@ -0,0 +1,106 @@
+/*
+https://leetcode.com/problems/delete-nodes-from-linked-list-present-in-array/description/
+3217. Delete Nodes From Linked List Present in Array
+
+You are given an array of integers nums and the head of a linked list. Return the head of the modified linked list after
+removing all nodes from the linked list that have a value that exists in nums.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3], head = [1,2,3,4,5]
+
+Output: [4,5]
+
+Explanation:
+
+
+
+Remove the nodes with values 1, 2, and 3.
+
+Example 2:
+
+Input: nums = [1], head = [1,2,1,2,1,2]
+
+Output: [2,2,2]
+
+Explanation:
+
+
+
+Remove the nodes with value 1.
+
+Example 3:
+
+Input: nums = [5], head = [1,2,3,4]
+
+Output: [1,2,3,4]
+
+Explanation:
+
+
+
+No node has value 5.
+
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
+All elements in nums are unique.
+The number of nodes in the given list is in the range [1, 105].
+1 <= Node.val <= 105
+The input is generated such that there is at least one node in the linked list that has a value not present in nums.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <map>
+#include <climits>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {
+    }
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+    ListNode(int x, ListNode* next) : val(x), next(next) {
+    }
+};
+
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> existed(nums.begin(), nums.end());
+        ListNode* pre = new ListNode();
+        pre->next = head;
+        ListNode* cur = pre;
+        ListNode* nxt = head;
+        while (nxt != nullptr) {
+            if (existed.find(nxt->val) != existed.end()) {
+                nxt = nxt->next;
+                continue;
+            }
+
+            cur -> next = nxt;
+            nxt = nxt -> next;
+            cur = cur -> next;
+            cur -> next = nullptr;
+        }
+        return pre->next;
+    }
+};
