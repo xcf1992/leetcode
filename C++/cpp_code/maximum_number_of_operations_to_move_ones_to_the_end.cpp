6736ae85bde9ca5577d0446e9ac5846ab55970a4
chenfu.xie <chenfu.xie@bytedance.com>
2025-12-11 10:49:03 -0800

    3228. Maximum Number of Operations to Move Ones to the End

---
+++ C++/cpp_code/maximum_number_of_operations_to_move_ones_to_the_end.cpp
@@ -0,0 +1,81 @@
+/*
+https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/description/
+3228. Maximum Number of Operations to Move Ones to the End
+
+You are given a binary string s.
+
+You can perform the following operation on the string any number of times:
+
+Choose any index i from the string where i + 1 < s.length such that s[i] == '1' and s[i + 1] == '0'.
+Move the character s[i] to the right until it reaches the end of the string or another '1'. For example, for s =
+"010010", if we choose i = 1, the resulting string will be s = "000110". Return the maximum number of operations that
+you can perform.
+
+
+
+Example 1:
+
+Input: s = "1001101"
+
+Output: 4
+
+Explanation:
+
+We can perform the following operations:
+
+Choose index i = 0. The resulting string is s = "0011101".
+Choose index i = 4. The resulting string is s = "0011011".
+Choose index i = 3. The resulting string is s = "0010111".
+Choose index i = 2. The resulting string is s = "0001111".
+Example 2:
+
+Input: s = "00111"
+
+Output: 0
+
+
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either '0' or '1'.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxOperations(string s) {
+        int n = s.size();
+        int rst = 0;
+        int idx = 0;
+        int one_cnt = 0;
+        while (idx < n) {
+            if (s[idx] == '0') {
+                idx += 1;
+                continue;
+            }
+
+            int end = idx;
+            while (end < n && s[end] == '1') {
+                one_cnt += 1;
+                end += 1;
+            }
+
+            if (end < n) {
+                rst += one_cnt;
+            }
+            idx = end;
+        }
+        return rst;
+    }
+};
