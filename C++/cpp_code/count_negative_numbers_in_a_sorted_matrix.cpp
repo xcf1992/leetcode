6d921fe3d903d31668498cf02adb860882ad6aea
chenfu.xie <chenfu.xie@bytedance.com>
2025-12-27 19:45:39 -0800

    1351. Count Negative Numbers in a Sorted Matrix

--- C++/cpp_code/count_negative_numbers_in_a_sorted_matrix.cpp
+++ C++/cpp_code/count_negative_numbers_in_a_sorted_matrix.cpp
@@ -47,6 +47,19 @@ using namespace std;
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
-
+        int m = grid.size();
+        int n = grid[0].size();
+        int row = m - 1;
+        int col = 0;
+        int rst = 0;
+        while (row >= 0 && col < n) {
+            if (grid[row][col] >= 0) {
+                col += 1;
+            } else {
+                rst += n - col;
+                row -= 1;
+            }
+        }
+        return rst;
     }
 };
--- C++/cpp_code/search_a_2d_matrix_II.cpp
+++ C++/cpp_code/search_a_2d_matrix_II.cpp
@@ -35,11 +35,7 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
-        if (m == 0) {
-            return false;
-        }
         int n = matrix[0].size();
-
         int row = 0;
         int col = n - 1;
         while (row < m and col >= 0) {
