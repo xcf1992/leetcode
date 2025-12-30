0a4e958111449b2e14a6f8febfad261d90b0b09f
chenfu.xie <chenfu.xie@bytedance.com>
2025-12-30 11:59:58 -0800

    2415. Reverse Odd Levels of Binary Tree

--- C++/cpp_code/magic_squares_in_grid.cpp
+++ C++/cpp_code/magic_squares_in_grid.cpp
@@ -1,37 +1,38 @@
 /*
- 840. Magic Squares In Grid
- A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9
- such that each row, column, and both diagonals all have the same sum.
+https://leetcode.com/problems/magic-squares-in-grid/description/
+840. Magic Squares In Grid
+A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9 such that each row, column, and both
+diagonals all have the same sum.
 
- Given an grid of integers, how many 3 x 3 "magic square" subgrids are there?
- (Each subgrid is contiguous).
+Given a row x col grid of integers, how many 3 x 3 magic square subgrids are there?
 
- Example 1:
+Note: while a magic square can only contain numbers from 1 to 9, grid may contain numbers up to 15.
 
- Input:
- [
-    [4,3,8,4],
-    [9,5,1,9],
-    [2,7,6,2]
- ]
- Output: 1
- Explanation:
- The following subgrid is a 3 x 3 magic square:
- 438
- 951
- 276
 
- while this one is not:
- 384
- 519
- 762
 
- In total, there is only one magic square inside the given grid.
- Note:
+Example 1:
 
- 1 <= grid.length <= 10
- 1 <= grid[0].length <= 10
- 0 <= grid[i][j] <= 15
+
+Input: grid = [[4,3,8,4],[9,5,1,9],[2,7,6,2]]
+Output: 1
+Explanation:
+The following subgrid is a 3 x 3 magic square:
+
+while this one is not:
+
+In total, there is only one magic square inside the given grid.
+Example 2:
+
+Input: grid = [[8]]
+Output: 0
+
+
+Constraints:
+
+row == grid.length
+col == grid[i].length
+1 <= row, col <= 10
+0 <= grid[i][j] <= 15
 */
 #include <iostream>
 #include <sstream>
