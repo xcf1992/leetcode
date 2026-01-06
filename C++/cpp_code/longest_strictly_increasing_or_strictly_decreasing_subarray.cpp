7653e0b89423645d6de435d65d5d95fc48aa44e5
chenfu.xie <chenfu.xie@bytedance.com>
2026-01-05 20:25:27 -0800

    3105. Longest Strictly Increasing or Strictly Decreasing Subarray

---
+++ C++/cpp_code/longest_strictly_increasing_or_strictly_decreasing_subarray.cpp
@@ -0,0 +1,92 @@
+/*
+https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/description/
+3105. Longest Strictly Increasing or Strictly Decreasing Subarray
+
+You are given an array of integers nums. Return the length of the longest subarray of nums which is either strictly
+increasing or strictly decreasing.
+
+
+
+Example 1:
+
+Input: nums = [1,4,3,3,2]
+
+Output: 2
+
+Explanation:
+
+The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+
+The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+
+Hence, we return 2.
+
+Example 2:
+
+Input: nums = [3,3,3,3]
+
+Output: 1
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+
+Hence, we return 1.
+
+Example 3:
+
+Input: nums = [3,2,1]
+
+Output: 3
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [2], and [1].
+
+The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
+
+Hence, we return 3.
+
+
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int rst = 1;
+        int n = nums.size();
+        int increasing_len = 1;
+        int decreasing_len = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                increasing_len += 1;
+            } else {
+                increasing_len = 1;
+            }
+            rst = max(rst, increasing_len);
+
+            int j = n - 1 - i;
+            if (nums[j] > nums[j + 1]) {
+                decreasing_len += 1;
+            } else {
+                decreasing_len = 1;
+            }
+            rst = max(rst, decreasing_len);
+        }
+        return rst;
+    }
+};
