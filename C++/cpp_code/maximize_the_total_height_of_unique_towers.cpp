62baa0a010e47d90c0c76fc37f5be6afb6a72123
chenfu.xie <chenfu.xie@bytedance.com>
2026-01-17 17:39:05 -0800

    3301. Maximize the Total Height of Unique Towers

---
+++ C++/cpp_code/maximize_the_total_height_of_unique_towers.cpp
@@ -0,0 +1,82 @@
+/*
+https://leetcode.com/problems/maximize-the-total-height-of-unique-towers/description/
+3301. Maximize the Total Height of Unique Towers
+
+You are given an array maximumHeight, where maximumHeight[i] denotes the maximum height the ith tower can be assigned.
+
+Your task is to assign a height to each tower so that:
+
+The height of the ith tower is a positive integer and does not exceed maximumHeight[i].
+No two towers have the same height.
+Return the maximum possible total sum of the tower heights. If it's not possible to assign heights, return -1.
+
+
+
+Example 1:
+
+Input: maximumHeight = [2,3,4,3]
+
+Output: 10
+
+Explanation:
+
+We can assign heights in the following way: [1, 2, 4, 3].
+
+Example 2:
+
+Input: maximumHeight = [15,10]
+
+Output: 25
+
+Explanation:
+
+We can assign heights in the following way: [15, 10].
+
+Example 3:
+
+Input: maximumHeight = [2,2,1]
+
+Output: -1
+
+Explanation:
+
+It's impossible to assign positive heights to each index so that no two towers have the same height.
+
+
+
+Constraints:
+
+1 <= maximumHeight.length <= 105
+1 <= maximumHeight[i] <= 109
+*/
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    long long maximumTotalSum(vector<int>& maximumHeight) {
+        sort(maximumHeight.rbegin(), maximumHeight.rend());
+        long long rst = 0;
+        int pre = INT_MAX;
+        for (int height : maximumHeight) {
+            int cur = min(pre - 1, height);
+            if (cur == 0) {
+                return -1;
+            }
+            rst += cur;
+            pre = cur;
+        }
+        return rst;
+    }
+};
