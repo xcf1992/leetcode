08a56e07141194cd152a3b3114aa14f5b74b60bf
chenfu.xie <chenfu.xie@bytedance.com>
2026-01-18 16:36:12 -0800

    1974. Minimum Time to Type Word Using Special Typewriter

---
+++ C++/cpp_code/minimum_time_to_type_word_using_special_typewriter.cpp
@@ -0,0 +1,88 @@
+/*
+https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/description/
+1974. Minimum Time to Type Word Using Special Typewriter
+
+There is a special typewriter with lowercase English letters 'a' to 'z' arranged in a circle with a pointer. A character
+can only be typed if the pointer is pointing to that character. The pointer is initially pointing to the character 'a'.
+
+
+Each second, you may perform one of the following operations:
+
+Move the pointer one character counterclockwise or clockwise.
+Type the character the pointer is currently on.
+Given a string word, return the minimum number of seconds to type out the characters in word.
+
+
+
+Example 1:
+
+Input: word = "abc"
+Output: 5
+Explanation:
+The characters are printed as follows:
+- Type the character 'a' in 1 second since the pointer is initially on 'a'.
+- Move the pointer clockwise to 'b' in 1 second.
+- Type the character 'b' in 1 second.
+- Move the pointer clockwise to 'c' in 1 second.
+- Type the character 'c' in 1 second.
+Example 2:
+
+Input: word = "bza"
+Output: 7
+Explanation:
+The characters are printed as follows:
+- Move the pointer clockwise to 'b' in 1 second.
+- Type the character 'b' in 1 second.
+- Move the pointer counterclockwise to 'z' in 2 seconds.
+- Type the character 'z' in 1 second.
+- Move the pointer clockwise to 'a' in 1 second.
+- Type the character 'a' in 1 second.
+Example 3:
+
+Input: word = "zjpc"
+Output: 34
+Explanation:
+The characters are printed as follows:
+- Move the pointer counterclockwise to 'z' in 1 second.
+- Type the character 'z' in 1 second.
+- Move the pointer clockwise to 'j' in 10 seconds.
+- Type the character 'j' in 1 second.
+- Move the pointer clockwise to 'p' in 6 seconds.
+- Type the character 'p' in 1 second.
+- Move the pointer counterclockwise to 'c' in 13 seconds.
+- Type the character 'c' in 1 second.
+
+
+Constraints:
+
+1 <= word.length <= 100
+word consists of lowercase English letters.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <climits>
+#include <map>
+#include <set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int minTimeToType(string word) {
+        int rst = 0;
+        char prev = 'a';
+        for (char c : word) {
+            rst += 1 + min(abs(c - prev), 26 - abs(c - prev));
+            prev = c;
+        }
+        return rst;
+    }
+};
