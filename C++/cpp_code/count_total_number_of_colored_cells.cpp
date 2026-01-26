5a229f32d11c807d8322071a9a3d1dbbb5cca1ba
chenfu.xie <chenfu.xie@bytedance.com>
2026-01-25 21:11:48 -0800

    2579. Count Total Number of Colored Cells

---
+++ C++/cpp_code/count_total_number_of_colored_cells.cpp
@@ -0,0 +1,53 @@
+/*
+https://leetcode.com/problems/count-total-number-of-colored-cells/description/
+2579. Count Total Number of Colored Cells
+
+There exists an infinitely large two-dimensional grid of uncolored unit cells. You are given a positive integer n,
+indicating that you must do the following routine for n minutes:
+
+At the first minute, color any arbitrary unit cell blue.
+Every minute thereafter, color blue every uncolored cell that touches a blue cell.
+Below is a pictorial representation of the state of the grid after minutes 1, 2, and 3.
+
+
+Return the number of colored cells at the end of n minutes.
+
+
+
+Example 1:
+
+Input: n = 1
+Output: 1
+Explanation: After 1 minute, there is only 1 blue cell, so we return 1.
+Example 2:
+
+Input: n = 2
+Output: 5
+Explanation: After 2 minutes, there are 4 colored cells on the boundary and 1 in the center, so we return 5.
+
+
+Constraints:
+
+1 <= n <= 105
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long rst = 1;
+        long long diff = 4;
+        for (int i = 1; i < n; i++) {
+            rst += diff;
+            diff += 4;
+        }
+        // return 1 + 4LL * n * (n - 1) / 2;
+        return rst;
+    }
+};
